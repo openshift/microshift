04aafabe6642f1760a877b1395bbdaccb07a425e
rootfs <hchen@redhat.com>
2021-05-18 08:55:58 -0400

    add openshift-sdn manifests

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -31,6 +31,7 @@ const (
 	VersionOperator        = "4.5.0-0.okd-2020-10-15-235428"
 	ImageCoreDNS           = "quay.io/openshift/okd-content@sha256:cf916e742d1a01f5632b42e1cdc7d918385eb85408faf32d3c92796f9faa6234"
 	ImageOC                = "quay.io/openshift/okd-content@sha256:e87ccf31c42554e2b62d5a441a68307043576dfc1c509b8e72fadc815591d3d2"
+	ImageSDN               = "quay.io/openshift/okd-content@sha256:71dbab00e9803acb3bcf859607d9d3ed445b6f3a063ecedd6b3ea02a7a8fdd80"
 	ImageKubeRbacProxy     = "quay.io/openshift/okd-content@sha256:1aa5bb03d0485ec2db2c7871a1eeaef83e9eabf7e9f1bc2c841cf1a759817c99"
 	ImageServiceCAOperator = "quay.io/openshift/okd-content@sha256:d5ab863a154efd4014b0e1d9f753705b97a3f3232bd600c0ed9bde71293c462e"
 )
