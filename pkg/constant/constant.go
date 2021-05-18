38ae4c1f4c937eab49393bec82c18f89eac4e39f
rootfs <hchen@redhat.com>
2021-05-18 08:50:35 -0400

    add components

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -25,4 +25,12 @@ const (
 	ServiceCIDR                         = "10.43.0.0/16"
 	ClusterDNS                          = "10.43.0.10"
 	DomainName                          = "ushift.testing"
+
+	// from https://github.com/openshift/okd/releases/tag/4.5.0-0.okd-2020-10-15-235428
+	ImageDNSOperator       = "quay.io/openshift/okd-content@sha256:04ed5d9d6fb14c0005c7074b659d6587c117da0e7e3b98f506f6cf3440c45358"
+	VersionOperator        = "4.5.0-0.okd-2020-10-15-235428"
+	ImageCoreDNS           = "quay.io/openshift/okd-content@sha256:cf916e742d1a01f5632b42e1cdc7d918385eb85408faf32d3c92796f9faa6234"
+	ImageOC                = "quay.io/openshift/okd-content@sha256:e87ccf31c42554e2b62d5a441a68307043576dfc1c509b8e72fadc815591d3d2"
+	ImageKubeRbacProxy     = "quay.io/openshift/okd-content@sha256:1aa5bb03d0485ec2db2c7871a1eeaef83e9eabf7e9f1bc2c841cf1a759817c99"
+	ImageServiceCAOperator = "quay.io/openshift/okd-content@sha256:d5ab863a154efd4014b0e1d9f753705b97a3f3232bd600c0ed9bde71293c462e"
 )
