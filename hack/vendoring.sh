28ec237d361b16b6b7d95aaa5a76c31125800bd0
Ricardo Noriega <rnoriega@redhat.com>
2021-05-21 14:27:07 +0200

    Remove working directory prints
    Signed-off-by: Ricardo Noriega <rnoriega@redhat.com>

--- hack/vendoring.sh
+++ hack/vendoring.sh
@@ -5,12 +5,9 @@ set -e
 echo "Removing vendor directory..."
 rm -rf vendor
 
-echo $PWD
-
 echo "Downloading libraries..."
 go mod vendor
 
-echo $PWD
 echo "Applying microshift patches to libraries..."
 for patch in `ls hack/patches/`; do git apply hack/patches/$patch; done
 
