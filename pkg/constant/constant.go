11019d08302bdc69a8d7870fa6ca4144929071db
rootfs <hchen@redhat.com>
2021-06-15 10:47:46 -0400

    update ocp container images

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -16,12 +16,10 @@ limitations under the License.
 package constant
 
 const (
-	// from https://github.com/openshift/okd/releases/tag/4.5.0-0.okd-2020-10-15-235428
-	ImageDNSOperator       = "quay.io/openshift/okd-content@sha256:04ed5d9d6fb14c0005c7074b659d6587c117da0e7e3b98f506f6cf3440c45358"
-	VersionOperator        = "4.5.0-0.okd-2020-10-15-235428"
-	ImageCoreDNS           = "quay.io/openshift/okd-content@sha256:cf916e742d1a01f5632b42e1cdc7d918385eb85408faf32d3c92796f9faa6234"
-	ImageOC                = "quay.io/openshift/okd-content@sha256:e87ccf31c42554e2b62d5a441a68307043576dfc1c509b8e72fadc815591d3d2"
-	ImageSDN               = "quay.io/openshift/okd-content@sha256:71dbab00e9803acb3bcf859607d9d3ed445b6f3a063ecedd6b3ea02a7a8fdd80"
-	ImageKubeRbacProxy     = "quay.io/openshift/okd-content@sha256:1aa5bb03d0485ec2db2c7871a1eeaef83e9eabf7e9f1bc2c841cf1a759817c99"
-	ImageServiceCAOperator = "quay.io/openshift/okd-content@sha256:d5ab863a154efd4014b0e1d9f753705b97a3f3232bd600c0ed9bde71293c462e"
+	// from https://github.com/openshift/okd/releases/tag/4.7.0-0.okd-2021-06-13-090745
+	ImageDNSOperator       = "quay.io/openshift/okd-content@sha256:f08a109aeb8eb5e4cc81d075f5efe0bbf1ea25a5a67ff533d55b88723df5916a"
+	ImageCoreDNS           = "quay.io/openshift/okd-content@sha256:a51b153b85c92c76017eee22582630b3ba6585ee839cf9b23a18f8598c326254"
+	ImageOC                = "quay.io/openshift/okd-content@sha256:2e78fd9e4343d88b9aa69a33eae2d3a0737a25a795dd2be0a04367ac743e09f2"
+	ImageKubeRbacProxy     = "quay.io/openshift/okd-content@sha256:fcce680899a37d6bdc621a58b6da0587d01cbb49a2d7b713e0d606dffc9f685a"
+	ImageServiceCAOperator = "quay.io/openshift/okd-content@sha256:9f32b0d4b6a08f8f40ff32772caa6ad152e8a045cfbf34b857b02d4cfa4e3fc7"
 )
