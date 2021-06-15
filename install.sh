84a6921b0873e2a1da8c0ec12330ac4250d1da0b
Ricardo Noriega <rnoriega@redhat.com>
2021-06-15 13:54:53 +0200

    Fix nits from review and configure kubeconfig
    Signed-off-by: Ricardo Noriega <rnoriega@redhat.com>

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ set -e
 #
 # - ROLES="controlplane,node"
 #   Option by default, but controlplane role can be run standalone.
+ROLES="controlplane,node"
+VERSION=v0.2
 
 # Function to get Linux distribution
 get_distro() {
@@ -21,18 +23,20 @@ get_arch() {
 
 # If RHEL, use subscription manager to register
 register_subs() {
-    subscription-manager register --auto-attach
-    subscription-manager repos --enable=rhocp-4.7-for-rhel-8-x86_64-rpms
+    sudo subscription-manager register --auto-attach
+    sudo subscription-manager repos --enable=rhocp-4.7-for-rhel-8-x86_64-rpms
 }
 
-# Apply SElinux policies (Permissive mode at the moment)
+# Apply SElinux policies
 apply_selinux_policy() {
-    setenforce 0
+    sudo semanage fcontext -a -t container_runtime_exec_t /usr/local/bin
+    sudo mkdir /var/lib/kubelet/
+    sudo chcon -R -t container_file_t /var/lib/kubelet/
 }
 
 # Install dependencies
 install_dependencies() {
-    dnf install -y \
+    sudo dnf install -y \
     policycoreutils-python-utils \
     conntrack \
     iptables-services
@@ -41,65 +45,67 @@ install_dependencies() {
 # Install CRI-O depending on the distro
 install_crio() {
     if [ "$DISTRO" == "fedora" ]; then
-        dnf module -y enable cri-o:1.20
-        dnf install -y cri-o cri-tools
+        sudo dnf module -y enable cri-o:1.20
+        sudo dnf install -y cri-o cri-tools
     fi
     if [ "$DISTRO" == "rhel" ]; then
-        dnf install cri-o cri-tools -y
+        sudo dnf install cri-o cri-tools -y
     else [ "$DISTRO" == "centos" ]
-        VERSION=1.20
+        CRIOVERSION=1.20
         OS=CentOS_8_Stream
-        curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
-        curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
-        dnf install -y cri-o cri-tools
+        sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
+        sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$CRIOVERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIOVERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$CRIOVERSION.repo
+        sudo dnf install -y cri-o cri-tools
     fi
 }
 
 
 # CRI-O config to match Microshift networking values
 crio_conf() {
-    sed -i 's/10.85.0.0\/16/10.42.0.0\/24/' /etc/cni/net.d/100-crio-bridge.conf
+    sudo sed -i 's/10.85.0.0\/16/10.42.0.0\/24/' /etc/cni/net.d/100-crio-bridge.conf
 
      if [ "$DISTRO" == "rhel" ]; then
-        sed -i 's/\/usr\/libexec\/crio\/conmon/\/usr\/bin\/conmon/' /etc/crio/crio.conf 
+        sudo sed -i 's|/usr/libexec/crio/conmon|/usr/bin/conmon|' /etc/crio/crio.conf 
      fi
 }
 
 # Start CRI-O
 verify_crio() {
-    systemctl enable crio
-    systemctl start crio
+    sudo systemctl enable crio
+    sudo systemctl restart crio
 
 }
 
 # Download and install kubectl
 get_kubectl() {
     curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-    chmod +x ./kubectl
-    mv ./kubectl /usr/bin/kubectl
+    sudo chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
 
 }
 
 # Download and install microshift
 get_microshift() {
     if [ $ARCH = "x86_64" ]; then
-        curl -O https://github.com/redhat-et/microshift/releases/download/v0.2/microshift-linux-amd64 -o microshift
-        curl https://github.com/redhat-et/microshift/releases/download/v0.2/release.sha256 
+        curl -L https://github.com/redhat-et/microshift/releases/download/$VERSION/microshift-linux-amd64 -o microshift
+        curl -L https://github.com/redhat-et/microshift/releases/download/$VERSION/release.sha256 -o release.sha256
     fi
 
-    SHA=$(sha256sum microshift)
-    if [[ $SHA != $(cat release.sha256) ]]; then echo "SHA256 checksum failed" && exit 1; fi
+    SHA=$(sha256sum microshift | awk '{print $1}')
+    if [[ $SHA != $(cat release.sha256 | awk '{print $1}') ]]; then echo "SHA256 checksum failed" && exit 1; fi
 
-    chmod +x microshift
-    mv microshift /usr/bin/
+    sudo chmod +x microshift
+    sudo mv microshift /usr/local/bin/
 
-    cat << EOF > /usr/lib/systemd/system/microshift.service
+    apply_selinux_policy
+
+    cat << EOF | sudo tee -a /usr/lib/systemd/system/microshift.service
 [Unit]
 Description=Microshift
 
 [Service]
-WorkingDirectory=/usr/bin/
-ExecStart=microshift run
+WorkingDirectory=/usr/local/bin/
+ExecStart=microshift run --roles="$ROLES"
 Restart=always
 User=root
 
@@ -107,15 +113,21 @@ User=root
 WantedBy=multi-user.target
 EOF
 
-    systemctl enable microshift.service --now
+    sudo systemctl enable microshift.service --now
 
+}
+
+# Locate kubeadmin configuration to default kubeconfig location
+prepare_kubeconfig() {
+    mkdir $HOME/.kube
+    sudo cp /var/lib/microshift/resources/kubeadmin/kubeconfig $HOME/.kube/config
+    sudo chown $USER:$USER $HOME/.kube/config
 }
 
 
 # Script execution
 get_distro
 get_arch
-apply_selinux_policy
 if [ $DISTRO = "rhel" ]; then
     register_subs
 fi
@@ -124,6 +136,12 @@ install_crio
 crio_conf
 verify_crio
 get_kubectl
+get_microshift
 
+until sudo test -f /var/lib/microshift/resources/kubeadmin/kubeconfig
+do
+     sleep 2
+done
+prepare_kubeconfig
 
 
