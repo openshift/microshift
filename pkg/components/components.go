85ab00bf9714dd65f9ab9c30c7cb4d1d72117147
rootfs <hchen@redhat.com>
2021-07-14 11:43:10 -0400

    remove predefined components

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -2,31 +2,8 @@ package components
 
 import (
 	"github.com/openshift/microshift/pkg/config"
-	"github.com/sirupsen/logrus"
 )
 
 func StartComponents(cfg *config.MicroshiftConfig) error {
-	if err := startServiceCAController(cfg, cfg.DataDir+"/resources/kubeadmin/kubeconfig"); err != nil {
-		logrus.Warningf("failed to start service-ca controller: %v", err)
-		return err
-	}
-
-	if err := startHostpathProvisioner(cfg.DataDir + "/resources/kubeadmin/kubeconfig"); err != nil {
-		logrus.Warningf("failed to start hostpath provisioner: %v", err)
-		return err
-	}
-
-	if err := startIngressController(cfg, cfg.DataDir+"/resources/kubeadmin/kubeconfig"); err != nil {
-		logrus.Warningf("failed to start ingress router controller: %v", err)
-		return err
-	}
-	if err := startDNSController(cfg, cfg.DataDir+"/resources/kubeadmin/kubeconfig"); err != nil {
-		logrus.Warningf("failed to start DNS controller: %v", err)
-		return err
-	}
-	if err := startFlannel(cfg.DataDir + "/resources/kubeadmin/kubeconfig"); err != nil {
-		logrus.Warningf("failed to start Flannel: %v", err)
-		return err
-	}
 	return nil
 }
