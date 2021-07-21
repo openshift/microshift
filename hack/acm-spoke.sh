54fb240b295a9ff07fae5be239a1468540f6bcc1
jcope <jcope@redhat.com>
2021-07-21 14:50:47 -0500

    spleep

--- hack/acm-spoke.sh
+++ hack/acm-spoke.sh
@@ -6,5 +6,5 @@ CLUSTER_NAME=${1:?"expected the cluster name, required to find import data"}
 WORK_DIR="$HOME/.acm/$CLUSTER_NAME/spoke"
 
 oc apply -f "$WORK_DIR"/klusterlet-crd.yaml
-spleep 2
+sleep 2
 oc apply -f "$WORK_DIR"/import.yaml
