683df3f18cb2b51aa1523710ca414209d20e3757
Ryan Cook <rcook@redhat.com>
2021-08-05 14:07:12 -0400

    Disconnected (#2)
    * testing this way
    
    * ignore base dir
    
    * do i have these privs?
    
    * did not have perms
    
    * trimming
    
    * Delete release.yml

---
+++ hack/disconnected.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PODMAN_ROOT="/tmp/storage"
+rm -rf ${PODMAN_ROOT} || true
+mkdir -p ${PODMAN_ROOT}
+
+echo "Discovering images"
+CONSTANTS=`grep "Image" ./pkg/constant/constant.go  | uniq | awk -F= '{print $2}' | tr -d '"'  | tr -d ' '`
+BINDATA=`grep "image:" ./pkg/assets/apps/bindata.go  | uniq | grep -v { | awk -F: '{print $2":" $3}' | tr -d ' '`
+
+for i in ${CONSTSANTS} ${BINDATA}; do
+   podman --root ${PODMAN_ROOT} --runroot  ${PODMAN_ROOT}  pull  ${i}
+done
+
+sudo tar czf  microshift-images-amd64.tgz -C /tmp/ storage
