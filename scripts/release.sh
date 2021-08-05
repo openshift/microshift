5a7da16f547519f43c36132fd755f994f79fbc4f
Ryan Cook <rcook@redhat.com>
2021-08-05 18:00:36 -0400

    dest

--- scripts/release.sh
+++ scripts/release.sh
@@ -156,9 +156,9 @@ stage_release_image_binaries() {
 }
 
 pull_container_images() {
-  local data="$1"
+  local dest="$1"
   cd "$ROOT"
-  sh hack/disconnected.sh $data
+  sh hack/disconnected.sh $dest
 }
 
 build_container_images_artifacts() {
