4a64773297a89861f45eb5440bca2e659aac0d49
Ryan Cook <rcook@redhat.com>
2021-08-06 10:54:40 -0400

    set location

--- scripts/release.sh
+++ scripts/release.sh
@@ -169,8 +169,9 @@ push_container_image_artifacts() {
 }
 
 pull_container_images() {
+  local asset_dir="$1"
    cd "$ROOT"
-   sh hack/disconnected.sh
+   sh hack/disconnected.sh "$asset_dir"
 }
 
 podman_create_manifest(){
@@ -267,7 +268,7 @@ build_container_images_artifacts                                      || { git s
 STAGE_DIR=$(stage_release_image_binaries)                             || { git switch -; exit 1; }
 push_container_image_artifacts                                        || { git switch -; exit 1; }
 push_container_manifest                                               || { git switch -; exit 1; }
-pull_container_images                                                 || { git switch -; exit 1; }
+pull_container_images "$STAGE_DIR"                                    || { git switch -; exit 1; }
 UPLOAD_URL="$(git_create_release "$API_DATA" "$TOKEN")"               || { git switch -; exit 1; }
 git_post_artifacts "$STAGE_DIR" "$UPLOAD_URL" "$TOKEN"                || { git switch -; exit 1; }
 git switch -
