a2e493bbf2e577c29c8d0b6c6ab89b87fde766f1
Frank A. Zdarsky <fzdarsky@redhat.com>
2021-08-27 16:39:06 +0200

    Add release info for other supported archs
    Co-authored-by: Carlos de Paula <me@carlosedp.com>
    
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

--- pkg/release/release_amd64.go
+++ pkg/release/release_amd64.go
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package release
 
 var Base = "4.7.0-0.okd-2021-06-13-090745"
