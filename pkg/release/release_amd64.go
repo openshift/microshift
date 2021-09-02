00195386d1dd4162cd74d75f7b941c05aa8ae958
Frank A. Zdarsky <fzdarsky@redhat.com>
2021-09-02 19:33:05 +0200

    Rebase release metadata
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

--- pkg/release/release_amd64.go
+++ pkg/release/release_amd64.go
@@ -16,15 +16,15 @@ limitations under the License.
 
 package release
 
-var Base = "4.7.0-0.okd-2021-06-13-090745"
+var Base = "4.7.0-0.okd-2021-08-22-163618"
 
 var Image = map[string]string{
-	"cli":                           "quay.io/openshift/okd-content@sha256:b20d195c721cd3b6215e5716b5569cbabbe861559af7dce07b5f8f3d38e6d701", // for dns-node-resolver
+	"cli":                           "quay.io/openshift/okd-content@sha256:71c912b47f0b1ca79442d207bd3a5c1435c000412423fa89d2d12096d0420bd6",
 	"coredns":                       "quay.io/openshift/okd-content@sha256:a51b153b85c92c76017eee22582630b3ba6585ee839cf9b23a18f8598c326254",
-	"haproxy_router":                "quay.io/openshift/okd-content@sha256:d09ab1bfce0ec273183cbe822aa5c6bdceeaee1685753de27b0ab9c15b54d8c0",
+	"haproxy_router":                "quay.io/openshift/okd-content@sha256:5be59a445c7187bc7d9e0bf4971277b058b0786485597379969a5c7fdc136409",
 	"kube_flannel":                  "quay.io/coreos/flannel:v0.14.0",
 	"kube_rbac_proxy":               "quay.io/openshift/okd-content@sha256:fcce680899a37d6bdc621a58b6da0587d01cbb49a2d7b713e0d606dffc9f685a",
 	"kubevirt_hostpath_provisioner": "quay.io/kubevirt/hostpath-provisioner:v0.8.0",
 	"pause":                         "k8s.gcr.io/pause",
-	"service_ca_operator":           "quay.io/openshift/okd-content@sha256:9f32b0d4b6a08f8f40ff32772caa6ad152e8a045cfbf34b857b02d4cfa4e3fc7",
+	"service_ca_operator":           "quay.io/openshift/okd-content@sha256:ea4e95cf5345f6eba15721713f94790febe1e09aa464de30944302e88e8a92a4",
 }
