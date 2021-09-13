e52655d63c84a7c5e3a2732e39aefad5d4248f8b
rootfs <hchen@redhat.com>
2021-09-13 16:33:52 -0400

    build both cuda and ubi-init aio images

---
+++ hack/all-in-one/build-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TAG="${TAG:-quay.io/microshift/microshift-aio-$(date +%Y-%m-%d-%H-%M)}"
+for i in "registry.access.redhat.com/ubi8/ubi-init:8.4" "docker.io/nvidia/cuda:11.4.1-cudnn8-devel-ubi8"; do
+   echo "build microshift aio image using base image "${i}
+   tag=$(echo ${i} |awk -F"/" '{print $NF}'| sed -e 's/:/-/g')
+   echo ${tag}
+   podman build --build-arg IMAGE_NAME=${i} -t ${TAG}-${tag} .
+done
