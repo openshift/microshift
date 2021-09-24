42c9034a3f3c45d8de5a75e32d89a3f11a364643
Miguel Angel Ajo <majopela@redhat.com>
2021-09-24 19:13:28 +0200

    Continue support for RHEL7
    Fix the build-images and some issues with the fc28 iptables
    rpm installation.
    
    My version of the #304 in #309 was missing the containers storage.conf
    fix for rhel7.
    
    Co-Authored-By: Huamin Chen <hchen@redhat.com>
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

--- hack/all-in-one/build-images.sh
+++ hack/all-in-one/build-images.sh
@@ -6,9 +6,9 @@ for img in "registry.access.redhat.com/ubi8/ubi-init:8.4" "docker.io/nvidia/cuda
    tag=$(echo ${img} |awk -F"/" '{print $NF}'| sed -e 's/:/-/g')
    echo ${tag}
 
-   for iptables in "nft" "legacy-iptables"; do
-        iptables_tag=""
-        [ "${iptables}" == "legacy-iptables" ] && iptables_tag = "-legacy-iptables"
-        podman build --build-arg IMAGE_NAME=${img} --build-arg IPTABLES=${iptables} -t ${TAG}-${tag}${iptables_tag} .
+   for host in "rhel7" "rhel8"; do
+        host_tag=""
+        [ "${host}" == "rhel7" ] && host_tag="-rhel7"
+        podman build --build-arg IMAGE_NAME=${img} --build-arg HOST=${host} -t ${TAG}-${tag}${host_tag} .
    done
 done
