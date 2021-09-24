4e13eebfb9fe206f52edd1e3072ba1f72981c43f
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2021-09-24 17:57:38 +0200

    Legacy and nft-tables based images matrix (#309)
    This is an alternative to #304.
    
    Co-Authored-By: Huamin Chen <hchen@redhat.com>
    
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

--- hack/all-in-one/build-images.sh
+++ hack/all-in-one/build-images.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-TAG="${TAG:-quay.io/microshift/microshift-aio-$(date +%Y-%m-%d-%H-%M)}"
-for i in "registry.access.redhat.com/ubi8/ubi-init:8.4" "docker.io/nvidia/cuda:11.4.2-base-ubi8"; do
+TAG="${TAG:-quay.io/microshift/microshift-aio:$(date +%Y-%m-%d-%H-%M)}"
+for img in "registry.access.redhat.com/ubi8/ubi-init:8.4" "docker.io/nvidia/cuda:11.4.2-base-ubi8"; do
    echo "build microshift aio image using base image "${i}
-   tag=$(echo ${i} |awk -F"/" '{print $NF}'| sed -e 's/:/-/g')
+   tag=$(echo ${img} |awk -F"/" '{print $NF}'| sed -e 's/:/-/g')
    echo ${tag}
-   podman build --build-arg IMAGE_NAME=${i} -t ${TAG}-${tag} .
+
+   for iptables in "nft" "legacy-iptables"; do
+        iptables_tag=""
+        [ "${iptables}" == "legacy-iptables" ] && iptables_tag = "-legacy-iptables"
+        podman build --build-arg IMAGE_NAME=${img} --build-arg IPTABLES=${iptables} -t ${TAG}-${tag}${iptables_tag} .
+   done
 done
