3cef7771b92833c5e92dd58acfbc2f94fd3556ad
Jon Cope <jcope@redhat.com>
2021-10-28 13:33:17 -0500

    cleanup script should handled crio pods (#396)
    add retry loop to remove orphaned pods
    
    Co-authored-by: Miguel Angel Ajo Pelayo <miguelangel@ajo.es>
    
    Co-authored-by: Miguel Angel Ajo Pelayo <miguelangel@ajo.es>

--- hack/cleanup.sh
+++ hack/cleanup.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
-sudo crictl rm --all --force
-sudo crictl rmi --all --prune
+set -eu
 
-sudo pkill -9 conmon
-sudo pkill -9 pause
+# crictl redirect STDOUT.  When no objects (pod, image, container) are present, crictl dump the help menu instead.  This may be confusing to users.
+sudo bash -c '
+    echo "Removing crio pods"
+    until crictl rmp --all --force 1>/dev/null; do sleep 1; done
 
-sudo rm -rf /var/lib/microshift
+    echo "Removing crio containers"
+    crictl rm --all --force 1>/dev/null
+
+    echo "Removing crio images"
+    crictl rmi --all --prune 1>/dev/null
+
+    echo "Killing conmoni, pause processes"
+    pkill -9 conmon
+    pkill -9 pause
+
+    echo "Removing /var/lib/microshift"
+    rm -rf /var/lib/microshift
+
+    echo "Cleanup succeeded"
+'
