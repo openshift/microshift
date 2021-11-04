e9822fc44f8cbcabff87f9ea947a46b2bdfe604d
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2021-11-04 12:35:04 +0100

    Improve and pack the cleanup script in the rpms (#420)
    This will allow running `cleanup-all-microshift-data` which will
    ask for confirmation. This is something which should be useful
    during development phase and which could potentially be removed
    for a final product.
    
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

--- hack/cleanup.sh
+++ hack/cleanup.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-
 set -eu
+
+echo "DATA LOSS WARNING: Do you wish to stop and cleanup ALL MicroShift data AND cri-o container workloads?"
+select yn in "Yes" "No"; do
+    case "${yn}" in
+        Yes ) break ;;
+        * ) echo "aborting cleanup; " ; exit;;
+    esac
+done
 
 # crictl redirect STDOUT.  When no objects (pod, image, container) are present, crictl dump the help menu instead.  This may be confusing to users.
 sudo bash -c '
+    echo "Stopping microshift"
+    set +e
+    systemctl stop --now microshift 2>/dev/null
+    systemctl disable microshift 2>/dev/null
+    systemctl stop --now microshift-containerized 2>/dev/null
+    systemctl disable microshift-containerized 2>/dev/null
+    podman stop microshift 2>/dev/null
+    podman stop microshift-aio 2>/dev/null
+    set -e
+
     echo "Removing crio pods"
     until crictl rmp --all --force 1>/dev/null; do sleep 1; done
 
@@ -13,9 +30,11 @@ sudo bash -c '
     echo "Removing crio images"
     crictl rmi --all --prune 1>/dev/null
 
-    echo "Killing conmoni, pause processes"
+    echo "Killing conmon, pause processes"
     pkill -9 conmon
     pkill -9 pause
+
+
 
     echo "Removing /var/lib/microshift"
     rm -rf /var/lib/microshift
