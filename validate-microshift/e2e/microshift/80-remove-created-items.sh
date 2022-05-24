2bbc9df7a3cb7faf5145d7a726d660302089d959
jcope <jcope@redhat.com>
2022-05-24 16:44:15 -0500

    kuttl should be minimally used to ensure the cluster infra has deployed

--- validate-microshift/e2e/microshift/80-remove-created-items.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl delete ns guestbook
-kubectl delete application -n argocd guestbook
-kubectl delete ns uploader
-kubectl delete -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -n argocd || true
-kubectl delete ns argocd 
