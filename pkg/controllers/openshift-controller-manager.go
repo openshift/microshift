3bf34e6450d8b8291bc149a414b1688aff9c7256
Ricardo Noriega <rnoriega@redhat.com>
2022-05-30 16:48:04 +0200

    Replacing ocp by openshift for naming consistency
    Signed-off-by: Ricardo Noriega <rnoriega@redhat.com>

--- pkg/controllers/openshift-controller-manager.go
+++ pkg/controllers/openshift-controller-manager.go
@@ -51,7 +51,7 @@ func NewOpenShiftControllerManager(cfg *config.MicroshiftConfig) *OCPControllerM
 
 func (s *OCPControllerManager) Name() string { return componentOCM }
 func (s *OCPControllerManager) Dependencies() []string {
-	return []string{"kube-apiserver", "ocp-apiserver"}
+	return []string{"kube-apiserver", "openshift-apiserver"}
 }
 
 func (s *OCPControllerManager) configure(cfg *config.MicroshiftConfig) {
