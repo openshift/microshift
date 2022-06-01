66f387ec9cde05bb4eacde04c3033f3abac1df9c
Doug Hellmann <dhellmann@redhat.com>
2022-06-01 04:26:46 -0400

    image-builder: add missing dependencies (#707)
    yum-utils is needed for reposync
    
    selinux-policy-devel is needed for the RPM build

--- scripts/image-builder/configure.sh
+++ scripts/image-builder/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exo pipefail
 
-sudo dnf install -y git osbuild-composer composer-cli cockpit-composer bash-completion podman genisoimage createrepo syslinux
+sudo dnf install -y git osbuild-composer composer-cli cockpit-composer bash-completion podman genisoimage createrepo syslinux yum-utils selinux-policy-devel
 sudo systemctl enable osbuild-composer.socket --now
 sudo systemctl enable cockpit.socket --now
 sudo firewall-cmd -q --add-service=cockpit
