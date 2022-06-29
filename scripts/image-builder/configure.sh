0b102347e0ee943bb8e699d29fc02ee46b3bd57f
ggiguash@redhat.com <ggiguash@redhat.com>
2022-06-29 10:20:28 -0400

    Fix configure.sh to install EPEL repository for mock utility

--- scripts/image-builder/configure.sh
+++ scripts/image-builder/configure.sh
@@ -3,9 +3,13 @@ set -exo pipefail
 
 sudo dnf install -y git osbuild-composer composer-cli \
     cockpit-composer bash-completion podman genisoimage \
-    createrepo syslinux yum-utils selinux-policy-devel mock
+    createrepo syslinux yum-utils selinux-policy-devel
 sudo systemctl enable osbuild-composer.socket --now
 sudo systemctl enable cockpit.socket --now
 sudo firewall-cmd -q --add-service=cockpit
 sudo firewall-cmd -q --add-service=cockpit --permanent
+
+# The mock utility comes from the EPEL repository
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf install -y mock 
 sudo usermod -a -G mock $(whoami)
