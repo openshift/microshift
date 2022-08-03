fee2e391943d8d04f0ee3608af0c176635a7028d
ggiguash@redhat.com <ggiguash@redhat.com>
2022-08-03 09:23:51 -0400

    Streamline devenv configuration with OVN-K and TopoLVM drivers

--- hack/cleanup.sh
+++ hack/cleanup.sh
@@ -15,40 +15,30 @@ sudo bash -c "
     set +e
     systemctl stop --now microshift 2>/dev/null
     systemctl disable microshift 2>/dev/null
-    systemctl stop --now microshift-containerized 2>/dev/null
-    systemctl disable microshift-containerized 2>/dev/null
-    podman stop microshift 2>/dev/null
-    podman stop microshift-aio 2>/dev/null
+    pkill -9 microshift
 
     echo Removing non-OVN crio pods
-    
-    crictl pods | tail -n +2 | grep -vE openshift-ovn-kubernetes | awk '{print \$1}' | xargs crictl stopp
-    crictl pods | tail -n +2 | grep -vE openshift-ovn-kubernetes | awk '{print \$1}' | xargs crictl rmp
-    
+    NOVN_PODS=\$(crictl pods | tail -n +2 | grep -vE openshift-ovn-kubernetes | awk '{print \$1}')
+    [ ! -z \"\${NOVN_PODS}\" ] && crictl stopp \${NOVN_PODS}
+    [ ! -z \"\${NOVN_PODS}\" ] && crictl rmp   \${NOVN_PODS}
 
     echo Removing all crio pods
     until crictl rmp --all --force 1>/dev/null; do sleep 1; done
 
-    echo Removing crio containers
-    crictl rm --all --force 1>/dev/null
+    echo Removing crio container and image storage
+    crio wipe -f &>/dev/null
+    systemctl restart crio
 
-    echo Removing crio images
-    crictl rmi --all --prune 1>/dev/null
-
-    echo Killing conmon, pause processes
+    echo Killing conmon, pause and OVN processes
+    ovs-vsctl del-br br-int
     pkill -9 conmon
     pkill -9 pause
     pkill -9 ovn-controller
     pkill -9 ovn-northd
     pkill -9 ovsdb-server
 
-    echo Removing /var/lib/microshift
-    crio wipe -f
-    systemctl restart crio
-    echo Reverting microshift-ovs-init.service configuration
-    /usr/bin/configure-ovs.sh OpenShiftSDN
-    rm -rf /var/lib/microshift
-    rm -rf /var/lib/ovn
+    echo Removing MicroShift and OVN configuration
+    rm -rf /var/lib/{microshift,ovnk}
     rm -rf /var/run/ovn
 
     echo Cleanup succeeded
