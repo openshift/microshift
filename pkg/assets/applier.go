47055250ea52fd80bce8ce2231a7fbebfc26727a
Frank A. Zdarsky <fzdarsky@redhat.com>
2022-09-05 20:08:23 +0000

    Add generic asset templating
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

--- pkg/assets/applier.go
+++ pkg/assets/applier.go
@@ -12,7 +12,7 @@ var (
 
 var assetsEventRecorder events.Recorder = events.NewLoggingEventRecorder("microshift-assets")
 
-type RenderParams map[string]string
+type RenderParams map[string]interface{}
 
 type RenderFunc func([]byte, RenderParams) ([]byte, error)
 
--- pkg/components/render.go
+++ pkg/components/render.go
@@ -5,17 +5,48 @@ import (
 	"text/template"
 
 	"github.com/openshift/microshift/pkg/assets"
+	"github.com/openshift/microshift/pkg/config"
 	"github.com/openshift/microshift/pkg/release"
 )
 
+var templateFuncs = map[string]interface{}{}
+
+func renderParamsFromConfig(cfg *config.MicroshiftConfig, extra assets.RenderParams) assets.RenderParams {
+	params := map[string]interface{}{
+		"ReleaseImage":  release.Image,
+		"NodeName":      cfg.NodeName,
+		"NodeIP":        cfg.NodeIP,
+		"ClusterCIDR":   cfg.Cluster.ClusterCIDR,
+		"ServiceCIDR":   cfg.Cluster.ServiceCIDR,
+		"ClusterDNS":    cfg.Cluster.DNS,
+		"ClusterDomain": cfg.Cluster.Domain,
+	}
+	for k, v := range extra {
+		params[k] = v
+	}
+	return params
+}
+
+func renderTemplate(tb []byte, data assets.RenderParams) ([]byte, error) {
+	tmpl, err := template.New("").Funcs(templateFuncs).Parse(string(tb))
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, data); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
 func renderServiceCAController(b []byte, p assets.RenderParams) ([]byte, error) {
 	data := struct {
-		ReleaseImage           assets.RenderParams
+		ReleaseImage           map[string]string
 		CAConfigMap, TLSSecret string
 	}{
 		ReleaseImage: release.Image,
-		CAConfigMap:  p["ConfigMap"],
-		TLSSecret:    p["Secret"],
+		CAConfigMap:  p["ConfigMap"].(string),
+		TLSSecret:    p["Secret"].(string),
 	}
 	tpl := template.Must(template.New("sc").Parse(string(b)))
 	var byteBuff bytes.Buffer
@@ -28,11 +59,11 @@ func renderServiceCAController(b []byte, p assets.RenderParams) ([]byte, error) 
 
 func renderDNSService(b []byte, p assets.RenderParams) ([]byte, error) {
 	data := struct {
-		ReleaseImage assets.RenderParams
+		ReleaseImage map[string]string
 		ClusterIP    string
 	}{
 		ReleaseImage: release.Image,
-		ClusterIP:    p["ClusterDNS"],
+		ClusterIP:    p["ClusterDNS"].(string),
 	}
 	tpl := template.Must(template.New("svc").Parse(string(b)))
 	var byteBuff bytes.Buffer
@@ -45,17 +76,17 @@ func renderDNSService(b []byte, p assets.RenderParams) ([]byte, error) {
 
 func renderOVNKManifests(b []byte, p assets.RenderParams) ([]byte, error) {
 	data := struct {
-		ReleaseImage   assets.RenderParams
+		ReleaseImage   map[string]string
 		ClusterCIDR    string
 		ServiceCIDR    string
 		KubeconfigPath string
 		KubeconfigDir  string
 	}{
 		ReleaseImage:   release.Image,
-		ClusterCIDR:    p["ClusterCIDR"],
-		ServiceCIDR:    p["ServiceCIDR"],
-		KubeconfigPath: p["KubeconfigPath"],
-		KubeconfigDir:  p["KubeconfigDir"],
+		ClusterCIDR:    p["ClusterCIDR"].(string),
+		ServiceCIDR:    p["ServiceCIDR"].(string),
+		KubeconfigPath: p["KubeconfigPath"].(string),
+		KubeconfigDir:  p["KubeconfigDir"].(string),
 	}
 	tpl := template.Must(template.New("cm").Parse(string(b)))
 	var byteBuff bytes.Buffer
@@ -68,7 +99,7 @@ func renderOVNKManifests(b []byte, p assets.RenderParams) ([]byte, error) {
 
 func renderReleaseImage(b []byte, p assets.RenderParams) ([]byte, error) {
 	data := struct {
-		ReleaseImage assets.RenderParams
+		ReleaseImage map[string]string
 	}{
 		ReleaseImage: release.Image,
 	}
