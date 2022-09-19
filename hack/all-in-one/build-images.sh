3a53d650c8a2217c1fe6b2f0be7a9656b3222728
Emmanuel Kasper <emmanuel@libera.cc>
2022-09-19 13:48:18 +0200

    Set UBI version to latest UBI minor version
    This will allow to get the latest 8.y.z security fixes,
    and allow some consistency in our build process.
    
    We should bump 8.6 to 8.7 as soon as the 8.7 UBIs are released

--- hack/all-in-one/build-images.sh
+++ hack/all-in-one/build-images.sh
@@ -8,7 +8,7 @@ cp ../../packaging/images/microshift-aio/unit ../../packaging/images/microshift-
 
 ARCH=$(uname -m |sed -e "s/x86_64/amd64/" |sed -e "s/aarch64/arm64/")
 TAG="${TAG:-quay.io/microshift/microshift-aio:$(date +%Y-%m-%d-%H-%M)}"
-for img in "registry.access.redhat.com/ubi8/ubi-init:8.4" "docker.io/nvidia/cuda:11.4.2-base-ubi8"; do
+for img in "registry.access.redhat.com/ubi8/ubi-init:8.6" "docker.io/nvidia/cuda:11.4.2-base-ubi8"; do
    echo "build microshift aio image using base image ""${img}"
    tag=$(echo ${img} |awk -F"/" '{print $NF}'| sed -e 's/:/-/g')
    echo "${tag}"
