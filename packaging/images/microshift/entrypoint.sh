ce7680d336a41e68953000a2e0476f55800e69ae
ggiguash <ggiguash@redhat.com>
2022-09-20 10:38:44 +0000

    Remove container build of MicroShift

--- packaging/images/microshift/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p /etc/crio/crio.conf.d
-cp /root/crio.conf.d/microshift.conf /etc/crio/crio.conf.d/microshift.conf
-
-# switch to microshift process
-exec /usr/bin/microshift run
