9e5b8bd9d7e3249dc3aff38d367ef07284b118d8
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2022-10-19 14:48:36 +0200

    update component images

--- pkg/release/release_amd64.go
+++ pkg/release/release_amd64.go
@@ -21,10 +21,10 @@ package release
 
 func init() {
 	Image = map[string]string{
-		"cli":                       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:4d182d11a30e6c3c1420502bec5b1192c43c32977060c4def96ea160172f71e7",
-		"coredns":                   "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:e5f97df4705b6f3a222491197000b887d541e9f3a440a7456f94c82523193760",
-		"haproxy_router":            "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:72c751aa148bf498839e6f37b304e3265f85af1e00578e637332a13ed9545ece",
-		"kube_rbac_proxy":           "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:dd49360368f93bbe1a11b8d1ce6f0f98eeb0c9230d9801a2b08a714a92e1f655",
+		"cli":                       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:b0c1cdfddbcd419f31b0b854aab95d72f1e5317931935796f7cf9d84ec1f1c34",
+		"coredns":                   "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:12b1435f5f1b1f1f0930f6b5571818c3c6840e4dff769833e866844fc97458b3",
+		"haproxy_router":            "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:1751e269e76b2a92b9065aa18ac2ccf3237e87be405c4f1fd5e85bba1e52c130",
+		"kube_rbac_proxy":           "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:16a2195d4c6485f1051e0e59e7fab40e094f7fe0146f9b2562a744feab8f625c",
 		"odf_topolvm":               "registry.redhat.io/odf4/odf-topolvm-rhel8@sha256:362c41177d086fc7c8d4fa4ac3bbedb18b1902e950feead9219ea59d1ad0e7ad",
 		"openssl":                   "registry.access.redhat.com/ubi8/openssl@sha256:8b41865d30b7947de68a9c1747616bce4efab4f60f68f8b7016cd84d7708af6b",
 		"ose_csi_ext_provisioner":   "registry.redhat.io/openshift4/ose-csi-external-provisioner@sha256:4b7d8035055a867b14265495bd2787db608b9ff39ed4e6f65ff24488a2e488d2",
@@ -33,6 +33,6 @@ func init() {
 		"ose_csi_livenessprobe":     "registry.redhat.io/openshift4/ose-csi-livenessprobe@sha256:e4b0f6c89a12d26babdc2feae7d13d3f281ac4d38c24614c13c230b4a29ec56e",
 		"ovn_kubernetes_microshift": "quay.io/microshift/ovn-kubernetes-singlenode@sha256:e97d6035754fad1660b522b8afa4dea2502d5189c8490832e762ae2afb4cf142",
 		"pause":                     "k8s.gcr.io/pause:3.6",
-		"service_ca_operator":       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:afcc1f59015b394e6da7d73eba32de407807da45018e3c4ecc25e5741aaae2dd",
+		"service_ca_operator":       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:2a943123cf987185526ad781450264474ae5008b6b02cdce4c715754605da3c4",
 	}
 }
