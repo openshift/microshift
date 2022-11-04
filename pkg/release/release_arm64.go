de6273bbc91b385161c07c505f8ca01173c522e3
ci-robot <ci-robot@openshift.io>
2022-11-04 05:17:31 +0000

    update component images

--- pkg/release/release_arm64.go
+++ pkg/release/release_arm64.go
@@ -21,13 +21,13 @@ package release
 
 func init() {
 	Image = map[string]string{
-		"cli":                       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:50e9d4487877c3841f078cafac746fc0f7bf58ef2a1c7b6b1e2f354628760a79",
-		"coredns":                   "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:f7e9edbcaffe940b0f5989a29a80245b4015342f6f4aae42b24caf909e1f2c6f",
-		"haproxy_router":            "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:0ed84cfd46b75d0961c7ea98dcf78482758601997d1375d1a56fde8c3a43aefa",
-		"kube_rbac_proxy":           "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:72f9e579dc1395bb5754550847fac926ab5a87051949c52a3ea8c709dc23fd7d",
+		"cli":                       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:6aed4b901ff2456c803ff159b50021bd86c6473c0c65f76c39269fb330640d1b",
+		"coredns":                   "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:be116df9f46266e604f1028fbde3742e3d63fef7c6421fb5a2a3673ce4f971e7",
+		"haproxy_router":            "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:4f5f51a8ba723590e550bc2024eaea6554bb4b749ec4356229060d3bd92fc9eb",
+		"kube_rbac_proxy":           "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:51ce83de9a43dfe8731c532f4ed0402c70da8df1e056292190c2cb43a45232a7",
 		"openssl":                   "registry.access.redhat.com/ubi8/openssl@sha256:3f781a07e59d164eba065dba7d8e7661ab2494b21199c379b65b0ff514a1b8d0",
-		"ovn_kubernetes_microshift": "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:4678d1ccf812ef8bcbfd54cf59d5d01be05d1b08683c346e06c3e29d3f924824",
-		"pod":                       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:9f9b6f1aa2770195c726ef4186306813b3c3c038cc488ff05aa5873297de94d0",
-		"service_ca_operator":       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:989a43840f16af130f846df494e61e484d42ed679d311db79a6b698d2399b92e",
+		"ovn_kubernetes_microshift": "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:071a394e5d43dbe7cb2470fc570b4d6cd607b799614d02b0ecadfe28c2c00138",
+		"pod":                       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:f310397d0a61f0ffefbb5753cb72f271d483eac82f4dd676d592fe2ea8487a30",
+		"service_ca_operator":       "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:f41255221e5dafd5688684bc0446fedf4a7526b3a02e05ca787bd166449088f8",
 	}
 }
