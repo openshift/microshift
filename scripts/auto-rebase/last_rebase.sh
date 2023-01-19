62215e9aa423fa1347007bfc35b7539eefae8198
Doug Hellmann <dhellmann@redhat.com>
2023-01-19 11:32:27 -0500

    update last_rebase.sh

--- scripts/auto-rebase/last_rebase.sh
+++ scripts/auto-rebase/last_rebase.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-./scripts/auto-rebase/rebase.sh to "registry.ci.openshift.org/ocp/release:4.12.0-0.nightly-2023-01-03-161331" "registry.ci.openshift.org/ocp-arm64/release-arm64:4.12.0-0.nightly-arm64-2023-01-03-161334"
+./scripts/auto-rebase/rebase.sh to "quay.io/openshift-release-dev/ocp-release:4.12.0-x86_64" "quay.io/openshift-release-dev/ocp-release:4.12.0-aarch64"
