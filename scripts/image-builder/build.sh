f0af86156326271d2fe30bc36c1eac31efad47cb
Gregory Giguashvili <ggiguash@redhat.com>
2023-01-20 07:16:19 +0200

    OCP 4.12 dependencies should come from the rhocp-4.12 RPM channel

--- scripts/image-builder/build.sh
+++ scripts/image-builder/build.sh
@@ -241,9 +241,8 @@ createrepo microshift-local >/dev/null
 # Download openshift local RPM packages (noarch for python and selinux packages)
 rm -rf openshift-local 2>/dev/null || true
 reposync -n -a ${BUILD_ARCH} -a noarch --download-path openshift-local \
-    --repo=rhocp-4.12-el8-beta-${BUILD_ARCH}-rpms \
+    --repo=rhocp-4.12-for-rhel-8-${BUILD_ARCH}-rpms \
     --repo=fast-datapath-for-rhel-8-${BUILD_ARCH}-rpms >/dev/null
-#   --repo=rhocp-4.12-for-rhel-8-${BUILD_ARCH}-rpms \
 
 # Remove coreos packages to avoid conflicts
 find openshift-local -name \*coreos\* -exec rm -f {} \;
