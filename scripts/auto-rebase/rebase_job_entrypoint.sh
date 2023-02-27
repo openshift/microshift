3d46726a171e904f96876b50b07c472bc9f3d294
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-02-27 15:22:34 +0100

    change names of rebase image configmaps

--- scripts/auto-rebase/rebase_job_entrypoint.sh
+++ scripts/auto-rebase/rebase_job_entrypoint.sh
@@ -12,11 +12,11 @@ cp /secrets/ci-pull-secret/.dockercfg "$HOME/.pull-secret.json" || {
     echo "WARN: Could not copy registry secret file"
 }
 
-release_amd64="$(oc get configmap/release-release-images-latest -o yaml \
-    | yq '.data."release-images-latest.yaml"' \
+release_amd64="$(oc get configmap/release-release-images-nightly-amd64 -o yaml \
+    | yq '.data."release-images-nightly-amd64.yaml"' \
     | jq -r '.metadata.name')"
-release_arm64="$(oc get configmap/release-release-images-arm64-latest -o yaml \
-    | yq '.data."release-images-arm64-latest.yaml"' \
+release_arm64="$(oc get configmap/release-release-images-nightly-arm64 -o yaml \
+    | yq '.data."release-images-nightly-arm64.yaml"' \
     | jq -r '.metadata.name')"
 
 pullspec_release_amd64="registry.ci.openshift.org/ocp/release:${release_amd64}"
