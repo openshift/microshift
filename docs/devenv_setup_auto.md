fb30a452cc361b182d3a83ffe2c4f03cdb975f7b
Doug Hellmann <dhellmann@redhat.com>
2023-02-28 08:47:31 -0500

    NO-ISSUE: add tool for easier management of developer VMs
    Wrap the existing create script with one that also adds SSH keys and
    registers the RHEL subscription. Include a delete command and another
    to get the VM's IP.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- docs/devenv_setup_auto.md
+++ docs/devenv_setup_auto.md
@@ -6,7 +6,69 @@ This document describes how to automate the setup of MicroShift development envi
 
 It is recommended to review the above mentioned documentation sections before proceeding with the automation instructions in the current document.
 
-## Create Virtual Machine
+## Manage Virtual Machine
+
+Use the `manage-vm.sh` script on the hypervisor to manipulate the virtual machine where MicroShift will run.
+
+Running it without any arguments prints the usage message.
+```bash
+$ ./scripts/devenv-builder/manage-vm.sh
+```
+
+Use the `create` subcommand to create a new VM.
+
+Use the `delete` subcommand to remove a VM.
+
+Use the `ip` subcommand to find the IP address of a VM.
+
+If your host is configured with VM disk storage somewhere other than
+the default, set `MICROSHIFT_VMDISKDIR` to point to the location, for
+example "$HOME/VMs".
+
+Set `MICROSHIFT_SSH_KEY_FILE` to the path to an ssh public key to have
+that key added to the VM for easier login.
+
+## Configure Virtual Machine
+Log into the hypervisor host and run the following command to get the IP address of the development virtual machine and use it to remotely connect to the system.
+```bash
+sudo virsh domifaddr microshift-dev
+...
+...
+VMIPADDR=192.168.122.29
+```
+
+Configure SSH not to require a password when logging into the system.
+```bash
+ssh-copy-id -f microshift@${VMIPADDR}
+```
+
+Copy the configuration script and your OpenShift pull secret file to the virtual machine using `microshift:microshift` credentials.
+```bash
+scp scripts/devenv-builder/configure-vm.sh microshift@${VMIPADDR}:
+scp ~/.pull-secret.json microshift@${VMIPADDR}:
+```
+
+Log into the development virtual machine using `microshift:microshift` credentials and run the following command to configure MicroShift.
+```bash
+~/configure-vm.sh ~/.pull-secret.json
+```
+
+> When configuring a RHEL OS and the machine is not yet registered to the Red Hat subscription management service,
+> the script prompts for the user name and password to be used for subscribing.
+
+The configuration script should run unattended and finish with the following message.
+```
+The configuration phase completed. Run the following commands to:
+ - Wait until all MicroShift pods are running
+ - Clean up MicroShift service configuration
+
+watch sudo $(which oc) --kubeconfig /var/lib/microshift/resources/kubeadmin/kubeconfig get pods -A
+echo 1 | sudo /usr/bin/microshift-cleanup-data --all
+
+Done
+```
+
+## Create Virtual Machine (low level)
 Log into the hypervisor host and run the `create-vm.sh` script without arguments to see its usage.
 ```bash
 $ ./scripts/devenv-builder/create-vm.sh
@@ -52,43 +114,3 @@ export DATAVOLSIZE=2
 ```
 
 Watch the output in the `virt-viewer` popup, waiting for a successful completion of the installation procedure.
-
-## Configure Virtual Machine
-Log into the hypervisor host and run the following command to get the IP address of the development virtual machine and use it to remotely connect to the system.
-```bash
-sudo virsh domifaddr microshift-dev
-...
-...
-VMIPADDR=192.168.122.29
-```
-
-Configure SSH not to require a password when logging into the system.
-```bash
-ssh-copy-id -f microshift@${VMIPADDR}
-```
-
-Copy the configuration script and your OpenShift pull secret file to the virtual machine using `microshift:microshift` credentials.
-```bash
-scp scripts/devenv-builder/configure-vm.sh microshift@${VMIPADDR}:
-scp ~/.pull-secret.json microshift@${VMIPADDR}:
-```
-
-Log into the development virtual machine using `microshift:microshift` credentials and run the following command to configure MicroShift.
-```bash
-~/configure-vm.sh ~/.pull-secret.json
-```
-
-> When configuring a RHEL OS and the machine is not yet registered to the Red Hat subscription management service,
-> the script prompts for the user name and password to be used for subscribing.
-
-The configuration script should run unattended and finish with the following message.
-```
-The configuration phase completed. Run the following commands to:
- - Wait until all MicroShift pods are running
- - Clean up MicroShift service configuration
-
-watch sudo $(which oc) --kubeconfig /var/lib/microshift/resources/kubeadmin/kubeconfig get pods -A
-echo 1 | sudo /usr/bin/microshift-cleanup-data --all
-
-Done
-```
---
+++ scripts/devenv-builder/manage-vm.sh
@@ -0,0 +1,257 @@
+#!/usr/bin/bash
+
+# https://github.com/openshift/microshift/blob/main/docs/devenv_setup.md
+# https://github.com/openshift/microshift/blob/main/docs/devenv_setup_auto.md
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPOROOT="$( cd "$SCRIPTDIR/../.." )"
+
+# Establish defaults
+MICROSHIFT_VMDISKDIR="${MICROSHIFT_VMDISKDIR:-/var/lib/libvirt/images}"
+MICROSHIFT_SSH_KEY_FILE="${MICROSHIFT_SSH_KEY_FILE:-$HOME/.ssh/id_rsa.pub}"
+MICROSHIFT_RHEL_VERSION="${MICROSHIFT_RHEL_VERSION:-9}"
+MICROSHIFT_VOL_POOL="${MICROSHIFT_VOL_POOL:-default}"
+
+# We need to pass these parameters to ssh when trying to use sshpass
+# to ensure that we do not get authentication errors because too many
+# ssh keys are tried before defaulting to password authentication.
+SSH_PASSWORD_OPTS="-o PubkeyAuthentication=no -o PreferredAuthentications=password -o StrictHostKeyChecking=no"
+
+# Show the IP address of the VM
+function get_ip {
+    local name="$1"
+    sudo virsh domifaddr "$1" \
+        | grep vnet \
+        | awk '{print $4}' \
+        | cut -f1 -d/
+}
+
+# Use the RHEL version and other settings to build a unique VM name
+# that will be discoverable in the portal in case the subscription
+# needs to be managed by hand.
+function get_vm_name {
+    local rhel_version="$1"
+
+    echo "microshift-${USER}-$(hostname -s)-${rhel_version}"
+}
+
+# Use the RHEL version to get the name of the ISO file to use for
+# installing the VM.
+function get_base_isofile {
+    local rhel_version="$1"
+
+    case $rhel_version in
+        8)
+            echo "rhel-8.7-x86_64-dvd.iso"
+            ;;
+        8.*)
+            echo "rhel-${rhel_version}-x86_64-dvd.iso"
+            ;;
+        9)
+            echo "rhel-baseos-9.1-x86_64-dvd.iso"
+            ;;
+        9.*)
+            echo "rhel-baseos-${rhel_version}-x86_64-dvd.iso"
+            ;;
+        *)
+            echo "unknown RHEL version $rhel_version" 1>&2
+            exit 1
+    esac
+}
+
+# Create the VM, if it does not exist
+function action_create {
+    # Set the variables needed by create-vm.sh instead of passing them as
+    # command line arguments.
+    export VMDISKDIR="${MICROSHIFT_VMDISKDIR}"
+    export NCPUS="${NCPUS:-4}"
+    export RAMSIZE="${RAMSIZE:-8}"
+    export DISKSIZE="${DISKSIZE:-60}"
+    export SWAPSIZE="${SWAPSIZE:-8}"
+    export DATAVOLSIZE="${DATAVOLSIZE:-2}"
+    if [ -z "$ISOFILE" ]; then
+        ISOFILE="${VMDISKDIR}/$(get_base_isofile $MICROSHIFT_RHEL_VERSION)"
+    fi
+    export ISOFILE
+
+    cd "$REPOROOT"
+
+    if [ ! -d "$VMDISKDIR" ]; then
+        echo "Creating $VMDISKDIR ..."
+        mkdir -p $VMDISKDIR
+    fi
+
+    if ! sudo virsh desc ${VMNAME} >/dev/null 2>&1; then
+        echo "Creating VM $VMNAME from $ISOFILE ..."
+        ./scripts/devenv-builder/create-vm.sh
+        if [ $? -ne 0 ]; then
+            echo "exiting failure" 1>&2
+            exit 1
+        fi
+    else
+        echo "VM $VMNAME exists"
+    fi
+
+    # Wait for an IP to be assigned
+    ip=$(get_ip "$VMNAME")
+    while [ -z "${ip}" ]; do
+        echo "Waiting for VM $VMNAME to have an IP"
+        sleep 30
+        ip=$(get_ip "$VMNAME")
+    done
+
+    # Remove any old keys for that IP
+    echo "Removing old ssh keys for ${ip} ..."
+    ssh-keygen -R ${ip}
+
+    # Wait for sshd to be available
+    while ! sshpass -p microshift ssh $SSH_PASSWORD_OPTS microshift@${ip} true; do
+        echo "Waiting to be able to login to microshift@${ip}..."
+        sleep 30
+    done
+
+    # Copy ssh key into the host for passwordless access
+    echo "Copying ssh key $MICROSHIFT_SSH_KEY_FILE ..."
+    sshpass -p microshift \
+            ssh-copy-id \
+            -f \
+            -i "$MICROSHIFT_SSH_KEY_FILE" \
+            $SSH_PASSWORD_OPTS \
+            "microshift@${ip}"
+
+    echo "VM online"
+
+    # Initialize RH subscription
+    echo "Checking subscription-manager..."
+    ssh -t "microshift@${ip}" "if ! sudo subscription-manager status; then sudo subscription-manager register; fi"
+
+    echo "VM online at $ip"
+}
+
+function action_delete {
+    ip=$(get_ip "$VMNAME")
+
+    if [ ! -z "$ip" ]; then
+        ssh "microshift@${ip}" "sudo subscription-manager unregister"
+    fi
+
+    sudo virsh destroy "$VMNAME"
+    sudo virsh undefine "$VMNAME"
+
+    # FIXME: The volume pool here may not be standard. How do we
+    # figure out what it should be?
+    sudo virsh vol-delete "${VMNAME}.qcow2" "${MICROSHIFT_VOL_POOL}"
+}
+
+function action_rm {
+    action_delete
+}
+
+function action_ip {
+    if ! sudo virsh desc ${VMNAME} >/dev/null 2>&1; then
+        echo "VM $VMNAME does not exist" 1>&2
+        exit 1
+    fi
+
+    get_ip "$VMNAME"
+}
+
+function action_help {
+    usage
+}
+
+function usage {
+    script_name=$(basename $0)
+    cat - <<EOF
+$script_name (create|ip|delete|rm|help) [options]
+
+Commands:
+
+  create    -- create a new VM
+  ip        -- show the IP of the VM
+  delete|rm -- delete the VM
+  help      -- show this help
+
+Options:
+
+  -d MICROSHIFT_VMDISKDIR  Specify the location the VM
+                           disk(s) should be created
+                           (Default: $MICROSHIFT_VMDISKDIR)
+
+  -i ISOFILE  Specify the location of the RHEL
+              ISO file instead of relying on computing
+              the name from the RHEL version and VMDISKDIR.
+              (Default: $MICROSHIFT_VMDISKDIR/$(get_base_isofile $MICROSHIFT_RHEL_VERSION))
+
+  -k MICROSHIFT_SSH_KEY_FILE  Specify the location of the
+                              SSH public key to install into
+                              the image.
+                              (Default: $MICROSHIFT_SSH_KEY_FILE)
+
+  -n VMNAME  Specify the name for the new VM
+             (Default: $(get_vm_name $MICROSHIFT_RHEL_VERSION))
+
+  -v MICROSHIFT_RHEL_VERSION  Specify the RHEL version to default to.
+                              Can be a major version ("9") or a
+                              major and minor version ("8.7").
+                              (Default: $MICROSHIFT_RHEL_VERSION)
+
+Variables:
+
+  MICROSHIFT_VMDISKDIR -- Default location for VM disk files.
+
+  MICROSHIFT_VOL_POOL -- Default libvirt volume pool associated with
+  VMDISKDIR.
+
+  MICROSHIFT_SSH_KEY_FILE -- Default ssh public key file to install.
+
+  MICROSHIFT_RHEL_VERSION -- Default RHEL version to use.
+
+EOF
+}
+
+# The first argument should always be what action to take.
+case "$1" in
+    create|ip|delete|rm|help)
+        action="$1"
+        shift
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
+
+# The other arguments are all the same, regardless of the action.
+while getopts "d:i:k:n:v:" opt; do
+    case $opt in
+        d)
+            MICROSHIFT_VMDISKDIR="$OPTARG"
+            ;;
+        i)
+            ISOFILE="$OPTARG"
+            ;;
+        k)
+            MICROSHIFT_SSH_KEY_FILE="$OPTARG"
+            ;;
+        n)
+            VMNAME="$OPTARG"
+            ;;
+        v)
+            MICROSHIFT_RHEL_VERSION="$OPTARG"
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# We always need to know the VMNAME, so compute it one time.
+if [ -z "$VMNAME" ]; then
+    VMNAME="$(get_vm_name $MICROSHIFT_RHEL_VERSION)"
+fi
+export VMNAME
+
+action_${action}
