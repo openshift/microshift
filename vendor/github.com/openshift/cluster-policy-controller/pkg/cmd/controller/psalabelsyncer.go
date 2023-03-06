61bd4f1376b11933ee8e949e0b5bcc7450df04ce
ci-robot <ci-robot@openshift.io>
2023-03-06 05:18:21 +0000

    update vendoring

--- vendor/github.com/openshift/cluster-policy-controller/pkg/cmd/controller/psalabelsyncer.go
+++ vendor/github.com/openshift/cluster-policy-controller/pkg/cmd/controller/psalabelsyncer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/openshift/cluster-policy-controller/pkg/psalabelsyncer"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func runPodSecurityAdmissionLabelSynchronizationController(ctx context.Context, controllerCtx *EnhancedControllerContext) (bool, error) {
@@ -13,19 +14,40 @@ func runPodSecurityAdmissionLabelSynchronizationController(ctx context.Context, 
 		return true, err
 	}
 
-	controller, err := psalabelsyncer.NewPodSecurityAdmissionLabelSynchronizationController(
-		kubeClient.CoreV1().Namespaces(),
-		controllerCtx.KubernetesInformers.Core().V1().Namespaces(),
-		controllerCtx.KubernetesInformers.Rbac().V1(),
-		controllerCtx.KubernetesInformers.Core().V1().ServiceAccounts(),
-		controllerCtx.SecurityInformers.Security().V1().SecurityContextConstraints(),
-		controllerCtx.EventRecorder.ForComponent("podsecurity-admission-label-sync-controller"),
-	)
+	featureGates := sets.NewString(controllerCtx.OpenshiftControllerConfig.FeatureGates...)
+	switch {
+	case featureGates.Has("OpenShiftPodSecurityAdmission=true"):
+		// if explicitly on, enable
+		controller, err := psalabelsyncer.NewEnforcingPodSecurityAdmissionLabelSynchronizationController(
+			kubeClient.CoreV1().Namespaces(),
+			controllerCtx.KubernetesInformers.Core().V1().Namespaces(),
+			controllerCtx.KubernetesInformers.Rbac().V1(),
+			controllerCtx.KubernetesInformers.Core().V1().ServiceAccounts(),
+			controllerCtx.SecurityInformers.Security().V1().SecurityContextConstraints(),
+			controllerCtx.EventRecorder.ForComponent("podsecurity-admission-label-sync-controller"),
+		)
+		if err != nil {
+			return true, err
+		}
+		go controller.Run(ctx, 1)
 
-	if err != nil {
-		return true, err
+	case featureGates.Has("OpenShiftPodSecurityAdmission=false"):
+		// if explicitly off or unspecified, run as logging.
+		fallthrough
+	default:
+		controller, err := psalabelsyncer.NewAdvisingPodSecurityAdmissionLabelSynchronizationController(
+			kubeClient.CoreV1().Namespaces(),
+			controllerCtx.KubernetesInformers.Core().V1().Namespaces(),
+			controllerCtx.KubernetesInformers.Rbac().V1(),
+			controllerCtx.KubernetesInformers.Core().V1().ServiceAccounts(),
+			controllerCtx.SecurityInformers.Security().V1().SecurityContextConstraints(),
+			controllerCtx.EventRecorder.ForComponent("podsecurity-admission-label-sync-controller"),
+		)
+		if err != nil {
+			return true, err
+		}
+		go controller.Run(ctx, 1)
 	}
 
-	go controller.Run(ctx, 1)
 	return true, nil
 }
