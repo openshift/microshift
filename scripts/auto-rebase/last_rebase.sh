a8c574b77fd61ddfef7c4a1efb181b51fed6a7f3
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-03-10 21:19:22 +0100

    update last_rebase.sh

--- scripts/auto-rebase/last_rebase.sh
+++ scripts/auto-rebase/last_rebase.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-./scripts/auto-rebase/rebase.sh to "registry.ci.openshift.org/ocp/release:4.13.0-0.nightly-2023-02-27-101545" "registry.ci.openshift.org/ocp-arm64/release-arm64:4.13.0-0.nightly-arm64-2023-02-28-171639" "registry.access.redhat.com/lvms4/lvms-operator-bundle:v4.12"
+./scripts/auto-rebase/rebase.sh to "registry.ci.openshift.org/ocp/release:4.14.0-0.nightly-2023-03-08-194110" "registry.ci.openshift.org/ocp-arm64/release-arm64:4.14.0-0.nightly-arm64-2023-03-06-143359" "registry.access.redhat.com/lvms4/lvms-operator-bundle:v4.12"
