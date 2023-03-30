5f931d707992cb1995080eedd5a5b30bd3319a44
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-03-30 11:42:23 +0200

    remove references to gcloud, expect some exported functions

--- e2e/main.sh
+++ e2e/main.sh
@@ -7,33 +7,71 @@ set -o pipefail
 SCRIPT_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
 var_should_not_be_empty() {
-    local var=${1}
-    if [ -z "${!var}" ]; then
-        echo >&2 "Environmental variable '${var}' is empty"
-        exit 1
+    local var_name=${1}
+    if [ -z "${!var_name+x}" ]; then
+        echo >&2 "Environmental variable '${var_name}' is unset"
+        return 1
+    elif [ -z "${!var_name}" ]; then
+        echo >&2 "Environmental variable '${var_name}' is empty"
+        return 1
     fi
 }
 
-verify_gcloud() {
-    var_should_not_be_empty INSTANCE_PREFIX
-    var_should_not_be_empty GOOGLE_PROJECT_ID
-    var_should_not_be_empty GOOGLE_COMPUTE_REGION
-    var_should_not_be_empty GOOGLE_COMPUTE_ZONE
+function_should_be_exported() {
+    local fname=${1}
+    if ! declare -F "$fname"; then
+        echo >&2 "Function '$fname' is unexported. It is expected that function is provided for interacting with cloud provider"
+        return 1
+    fi
 }
 
-prepare_kubeconfig() {
-    export IP_ADDRESS="$(gcloud compute instances describe ${INSTANCE_PREFIX} --format='get(networkInterfaces[0].accessConfigs[0].natIP)')"
-    gssh "sudo cat /var/lib/microshift/resources/kubeadmin/${IP_ADDRESS}/kubeconfig" >/tmp/kubeconfig
-    export KUBECONFIG=/tmp/kubeconfig
+file_should_exist() {
+    local file_var="$1"
+
+    var_should_not_be_empty "$file_var" || return 1
+    local file="${!file_var}"
+
+    if [[ ! -e "$file" ]]; then
+        echo >&2 "File '$file' does not exist"
+        return 1
+    fi
+
+    if [[ ! -f "$file" ]]; then
+        echo >&2 "'$file' is not a file"
+        return 1
+    fi
+
+    if [[ ! -r "$file" ]]; then
+        echo >&2 "'$file' is missing 'read' permissions"
+        return 1
+    fi
+
 }
+
+prechecks() {
+    # TODO: It'd be nice to run all prechecks each time, but exit at the end if any is unfullfiled
+    file_should_exist KUBECONFIG || exit 1
+    var_should_not_be_empty USHIFT_IP || exit 1
+    var_should_not_be_empty USHIFT_USER || exit 1
+    # TODO Check passwordless SSH
+    # TODO Check passwordless sudo
+
+    # Just warning for now
+    function_should_be_exported firewall::open_port  # || exit 1
+    function_should_be_exported firewall::close_port # || exit 1
+}
+
+# TODO: check_if_cluster_is_ready() {} - kuttl?
 
 main() {
-    verify_gcloud
-    prepare_kubeconfig
+    prechecks
+
+    export IP="$USHIFT_IP"
+    export USER="$USHIFT_USER"
 
     local gather_debug=false
-    for test in ${SCRIPT_PATH}/tests/*; do
-        echo "RUNNING $(basename ${test})"
+    for test in "${SCRIPT_PATH}"/tests/*.sh; do
+        echo "RUNNING $(basename "${test}")"
         ${test}
         res=$?
         if [ $res -eq 0 ]; then
