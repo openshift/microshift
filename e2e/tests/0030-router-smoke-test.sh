69b02da0d8cf9a112a1fcc66ff39f293c455ae60
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-03-30 11:43:43 +0200

    update router & lb smoke tests

--- e2e/tests/0030-router-smoke-test.sh
+++ e2e/tests/0030-router-smoke-test.sh
@@ -5,38 +5,33 @@ IFS=$'\n\t'
 set -x
 
 SCRIPT_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
-source "${SCRIPT_PATH}/../helpers.sh"
 
+# shellcheck disable=SC2317  # Don't warn about unreachable commands in this function
 cleanup() {
     oc delete route hello-microshift
     oc delete service hello-microshift
     oc delete -f "${SCRIPT_PATH}/assets/hello-microshift.yaml"
+    firewall::close_port tcp:80
 }
 trap cleanup EXIT
 
-gcloud compute firewall-rules update "${INSTANCE_PREFIX}" --allow tcp:22,icmp,tcp:80
+firewall::open_port tcp:80
 oc create -f "${SCRIPT_PATH}/assets/hello-microshift.yaml"
 oc expose pod hello-microshift
 oc expose svc hello-microshift --hostname hello-microshift.cluster.local
 oc wait pods -l app=hello-microshift --for condition=Ready --timeout=60s
 
-set +x
 retries=3
 backoff=3s
-for try in $(seq 1 "${retries}"); do
-    echo "Attempt: ${try}"
-    echo "Running: curl -vk http://hello-microshift.cluster.local --resolve \"hello-microshift.cluster.local:80:${IP}\""
-    RESPONSE=$(curl -vk http://hello-microshift.cluster.local --resolve "hello-microshift.cluster.local:80:${IP}" 2>&1)
+for _ in $(seq 1 "${retries}"); do
+    RESPONSE=$(curl -i http://hello-microshift.cluster.local --resolve "hello-microshift.cluster.local:80:${IP}" 2>&1)
     RESULT=$?
-    echo "Exit code: ${RESULT}"
-    echo -e "Response: \n${RESPONSE}\n\n"
-    if [ $RESULT -eq 0 ] && echo "${RESPONSE}" | grep -q -E "HTTP.*200 OK"; then
-        echo "Request fulfilled conditions to be successful (exit code = 0, response contains 'HTTP.*200 OK')"
+    [ $RESULT -eq 0 ] &&
+        echo "${RESPONSE}" | grep -q -E "HTTP.*200" &&
+        echo "${RESPONSE}" | grep -q "Hello MicroShift" &&
         exit 0
-    fi
-    echo -e "Waiting ${backoff} before next retry\n\n"
+
     sleep "${backoff}"
 done
-set -x
 
 exit 1
