e4f8da76796d3ca7a9ba59edf5b452076a955f42
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-04-04 17:17:43 +0200

    fix inner greenboot test path

--- e2e/tests/0050-greenboot.sh
+++ e2e/tests/0050-greenboot.sh
@@ -8,4 +8,4 @@ set -x
 SCRIPT_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
 scp "$SCRIPT_PATH/assets/busybox_running_check.sh" "$SCRIPT_PATH/assets/greenboot-test.sh" "$USHIFT_USER@$USHIFT_IP":/tmp/
-ssh -q "$USHIFT_USER@$USHIFT_IP" "chmod +x ~/greenboot-test.sh /tmp/busybox_running_check.sh && sudo ~/greenboot-test.sh"
+ssh -q "$USHIFT_USER@$USHIFT_IP" "chmod +x /tmp/greenboot-test.sh /tmp/busybox_running_check.sh && sudo /tmp/greenboot-test.sh"
