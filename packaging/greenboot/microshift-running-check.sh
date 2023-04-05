c3912c3c7856fe457d7a01ddc49ec09ce707cb8b
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-04-05 11:45:00 +0200

    run pod restart check in parallel

--- packaging/greenboot/microshift-running-check.sh
+++ packaging/greenboot/microshift-running-check.sh
@@ -119,9 +119,15 @@ for i in "${!PODS_NS_LIST[@]}"; do
 done
 
 # Verify that MicroShift core pods are not restarting
+pids=()
 for i in "${!PODS_NS_LIST[@]}"; do
     CHECK_PODS_NS=${PODS_NS_LIST[${i}]}
 
     echo "Checking pod restart count in the '${CHECK_PODS_NS}' namespace"
     namespace_pods_not_restarting "${CHECK_PODS_NS}"
+    pids+=($!)
+done
+
+for pid in "${pids[@]}"; do
+    wait "${pid}"
 done
