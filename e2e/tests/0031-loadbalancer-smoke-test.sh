3fb8f9479ff55a92ce76b6c4f4705817b434355d
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-04-06 12:27:52 +0200

    more bash sanity to router and lb tests

--- e2e/tests/0031-loadbalancer-smoke-test.sh
+++ e2e/tests/0031-loadbalancer-smoke-test.sh
@@ -7,33 +7,33 @@ set -x
 
 SCRIPT_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
+RETRIES=3
+BACKOFF=3s
+
 # shellcheck disable=SC2317  # Don't warn about unreachable commands in this function
 cleanup() {
     oc delete service hello-microshift || true
     oc delete -f "${SCRIPT_PATH}/assets/hello-microshift.yaml" || true
     if declare -F firewall::close_port; then firewall::close_port 5678 tcp || true; fi
 }
-RESULT=1
-# explicit exit after cleanup, to not have cleanup override RESULT
-trap 'cleanup; exit $RESULT' EXIT
+trap 'cleanup' EXIT
 
 declare -F firewall::open_port && firewall::open_port 5678 tcp
 oc create -f "${SCRIPT_PATH}/assets/hello-microshift.yaml"
 oc create service loadbalancer hello-microshift --tcp=5678:8080
 oc wait pods -l app=hello-microshift --for condition=Ready --timeout=60s
 
-retries=3
-backoff=3s
-for _ in $(seq 1 "${retries}"); do
-    set +x
-    RESPONSE=$(curl -i "${USHIFT_IP}":5678 2>&1)
-    RESULT=$?
-    set -x
-    if [ $RESULT -eq 0 ] &&
-        echo "${RESPONSE}" | grep -q -E "HTTP.*200" &&
-        echo "${RESPONSE}" | grep -q "Hello MicroShift"; then
-        RESULT=0
-        break
-    fi
-    sleep "${backoff}"
+for _ in $(seq "${RETRIES}"); do
+    set +e
+    response=$(curl -i "${USHIFT_IP}":5678 2>&1)
+    result=$?
+    set -e
+
+    [ $result -eq 0 ] &&
+        echo "${response}" | grep -q -E "HTTP.*200" &&
+        echo "${response}" | grep -q "Hello MicroShift" &&
+        exit 0
+
+    sleep "${BACKOFF}"
 done
+exit 1
