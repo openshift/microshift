3af9abda33a3ec3c32f1ebf59fe7221ecf3b6288
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-04-06 14:35:47 +0200

    reboot: allow for ssh exit 0 or 255

--- e2e/tests/0100-reboot.sh
+++ e2e/tests/0100-reboot.sh
@@ -29,7 +29,17 @@ oc label namespaces default "pod-security.kubernetes.io/"{enforce,audit,warn}"=p
 oc create -f "${SCRIPT_PATH}/assets/pod-with-pvc.yaml"
 oc wait --for=condition=Ready --timeout=120s pod/test-pod
 
-ssh "$USHIFT_USER@$USHIFT_IP" "sudo reboot now"
+set +e
+ssh -v "$USHIFT_USER@$USHIFT_IP" "sudo reboot now"
+res=$?
+set -e
+
+# Allow for `ssh` command errors (255 exit code) like "connection closed by remote host"
+# Fail on other errors (coming from the command executed remotely itself)
+if [ "${res}" -ne 0 ] && [ "${res}" -ne 255 ]; then
+    exit 1
+fi
+
 wait_until ssh "$USHIFT_USER@$USHIFT_IP" "true"
 # Just check if KAS is up and serving
 wait_until oc get node
