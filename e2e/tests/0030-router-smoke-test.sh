f8be940e4022d789b950d561763032582bdd71bf
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-04-06 12:27:52 +0200

    more bash sanity to router and lb tests

--- e2e/tests/0030-router-smoke-test.sh
+++ e2e/tests/0030-router-smoke-test.sh
@@ -7,6 +7,9 @@ set -x
 
 SCRIPT_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
+RETRIES=3
+BACKOFF=3s
+
 # shellcheck disable=SC2317  # Don't warn about unreachable commands in this function
 cleanup() {
     oc delete route hello-microshift || true
@@ -14,9 +17,7 @@ cleanup() {
     oc delete -f "${SCRIPT_PATH}/assets/hello-microshift.yaml" || true
     if declare -F firewall::close_port; then firewall::close_port 80 tcp || true; fi
 }
-RESULT=1
-# explicit exit after cleanup, to not have cleanup override RESULT
-trap 'cleanup; exit $RESULT' EXIT
+trap 'cleanup' EXIT
 
 declare -F firewall::open_port && firewall::open_port 80 tcp
 
@@ -25,19 +26,17 @@ oc expose pod hello-microshift
 oc expose svc hello-microshift --hostname hello-microshift.cluster.local
 oc wait pods -l app=hello-microshift --for condition=Ready --timeout=60s
 
-retries=3
-backoff=3s
-for _ in $(seq 1 "${retries}"); do
-    set +x
-    RESPONSE=$(curl -i http://hello-microshift.cluster.local --resolve "hello-microshift.cluster.local:80:${USHIFT_IP}" 2>&1)
-    RESULT=$?
-    set -x
-    if [ $RESULT -eq 0 ] &&
-        echo "${RESPONSE}" | grep -q -E "HTTP.*200" &&
-        echo "${RESPONSE}" | grep -q "Hello MicroShift"; then
-        RESULT=0
-        break
-    fi
+for _ in $(seq "${RETRIES}"); do
+    set +e
+    response=$(curl -i http://hello-microshift.cluster.local --resolve "hello-microshift.cluster.local:80:${USHIFT_IP}" 2>&1)
+    result=$?
+    set -e
 
-    sleep "${backoff}"
+    [ $result -eq 0 ] &&
+        echo "${response}" | grep -q -E "HTTP.*200" &&
+        echo "${response}" | grep -q "Hello MicroShift" &&
+        exit 0
+
+    sleep "${BACKOFF}"
 done
+exit 1
