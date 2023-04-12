9c5bdf9438e1aaced4d52cd0a755662a8e0a7d4b
chiragkyal <chirag.kyal@gmail.com>
2023-04-12 13:37:11 +0530

    USHIFT-1030: always create venv for python

--- hack/verify-py.sh
+++ hack/verify-py.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+set -euo pipefail
+
 ROOTDIR=$(git rev-parse --show-toplevel)
+VENV_DIR="${ROOTDIR}/_output"
+VENV="${VENV_DIR}/venv"
+
 REQ_FILE=${ROOTDIR}/scripts/requirements.txt
-VENV="/tmp/venv"
-PYLINT="pylint"
 
-if ! command -v ${PYLINT} &>/dev/null; then
-    
-    check="$(python3 -m pip install -r ${REQ_FILE} 2>&1 | { grep 'Permission denied' || true; })"
-    
-    # Install pylint in a virtual environment for CI
-    if [ ! -z "$check" ] ; then
-        printf "Installing pylint in '${VENV}' virtual environment"
-        python3 -m venv ${VENV}
- 	    ${VENV}/bin/python3 -m pip install --upgrade pip
- 	    ${VENV}/bin/python3 -m pip install -r ${REQ_FILE}
-        PYLINT="${VENV}/bin/pylint"
+create_venv() {
+    local vpython="${VENV}/bin/python3"
+
+    [ -f "${REQ_FILE}" ] || { echo "${REQ_FILE} is not present"; exit 1; }
+    [ -d "${VENV_DIR}" ] || mkdir -p "${VENV_DIR}"
+
+    echo "Creating venv in '${VENV}' and installing packages..."
+    python3 -m venv ${VENV}
+    ${vpython} -m pip install --upgrade pip
+    ${vpython} -m pip install -r ${REQ_FILE}
+    echo "Done!"
+}
+
+run_pylint() {
+    local pylint="${VENV}/bin/pylint"
+    local pyfiles=$(find . -type d \( -path ./_output -o -path ./vendor -o -path ./assets -o -path ./etcd/vendor \) -prune -o -name '*.py' -print)
+
+    if ! command -v ${pylint} &>/dev/null; then
+        echo "Installing pylint..."
+        create_venv
     fi
-fi
+    
+    printf "Running ${pylint} for \n${pyfiles}\n"
+    ${pylint} --variable-naming-style=any ${pyfiles}
+}
 
-PYFILES=$(find . -type d \( -path ./_output -o -path ./vendor -o -path ./assets -o -path ./etcd/vendor \) -prune -o -name '*.py' -print)
-printf "Running ${PYLINT} for \n${PYFILES}\n"
-
-${PYLINT} --variable-naming-style=any ${PYFILES}
+run_pylint
