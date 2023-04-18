a54f2b31942624c7278d3391dec510923ffa64a4
ehila <ehila@redhat.com>
2023-04-18 09:27:10 -0400

    feat: renamed to yaml-gen to config-gen
    Signed-off-by: ehila <ehila@redhat.com>

--- hack/generate-config.sh
+++ hack/generate-config.sh
@@ -6,15 +6,15 @@ ROOTDIR=$(git rev-parse --show-toplevel)
 CONTROLLER_BIN="${ROOTDIR}/_output/bin/controller-gen"
 
 generate_crd() {
-    ${CONTROLLER_BIN} crd paths=../../hack/yaml-gen/configcrd output:stdout
+    ${CONTROLLER_BIN} crd paths=../../hack/config-gen/configcrd output:stdout
 }
 
 echo "Generating packaging/microshift/config.yaml and cockpit-plugin/packaging/config-openapi-spec.json"
-generate_crd | go run -mod vendor ../../hack/yaml-gen \
+generate_crd | go run -mod vendor ../../hack/config-gen \
 -a ../../cockpit-plugin/packaging/config-openapi-spec.json \
 -o ../../packaging/microshift/config.yaml
 
 echo "Updating docs/howto_config.md"
-generate_crd | go run -mod vendor ../../hack/yaml-gen \
+generate_crd | go run -mod vendor ../../hack/config-gen \
 -o ../../docs/howto_config.md \
 -t ../../docs/howto_config.md
