6b3eb4778e5242d49a70621cf107b250d21e2c84
chiragkyal <chirag.kyal@gmail.com>
2023-04-21 13:07:24 +0530

    USHIFT-1064: change hello-microshift to microshift-test

--- e2e/tests/0031-loadbalancer-smoke-test.sh
+++ e2e/tests/0031-loadbalancer-smoke-test.sh
@@ -14,16 +14,16 @@ BACKOFF=3s
 
 # shellcheck disable=SC2317  # Don't warn about unreachable commands in this function
 cleanup() {
-    oc delete service hello-microshift || true
-    oc delete -f "${SCRIPT_PATH}/assets/hello-microshift.yaml" || true
+    oc delete service microshift-test || true
+    oc delete -f "${SCRIPT_PATH}/assets/microshift-test.yaml" || true
     if declare -F firewall::close_port; then firewall::close_port 5678 tcp || true; fi
 }
 trap 'cleanup' EXIT
 
 declare -F firewall::open_port && firewall::open_port 5678 tcp
-oc create -f "${SCRIPT_PATH}/assets/hello-microshift.yaml"
-oc create service loadbalancer hello-microshift --tcp=5678:8080
-oc wait pods -l app=hello-microshift --for condition=Ready --timeout=60s
+oc create -f "${SCRIPT_PATH}/assets/microshift-test.yaml"
+oc create service loadbalancer microshift-test --tcp=5678:8080
+oc wait pods -l app=microshift-test --for condition=Ready --timeout=60s
 
 for _ in $(seq "${RETRIES}"); do
     set +e
