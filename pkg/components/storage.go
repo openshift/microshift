e439d20be88ddf4ac5029a4e1b5b68440885646e
Jon Cope <jcope@redhat.com>
2023-04-26 17:13:48 -0500

    Add brief documentation for resizing
    Signed-off-by: Jon Cope <jcope@redhat.com>
    
    add smoke test
    
    Signed-off-by: Jon Cope <jcope@redhat.com>
    
    cleaned up role meta
    
    Signed-off-by: Jon Cope <jcope@redhat.com>
    
    update rbac to enable topolvm resizer
    
    Signed-off-by: Jon Cope <jcope@redhat.com>

--- pkg/components/storage.go
+++ pkg/components/storage.go
@@ -47,12 +47,14 @@ func startCSIPlugin(ctx context.Context, cfg *config.Config, kubeconfigPath stri
 			"components/lvms/topolvm-controller_rbac.authorization.k8s.io_v1_clusterrole.yaml",
 			"components/lvms/topolvm-node-scc_rbac.authorization.k8s.io_v1_clusterrole.yaml",
 			"components/lvms/topolvm-node_rbac.authorization.k8s.io_v1_clusterrole.yaml",
+			"components/lvms/topolvm-controller-resizer_rbac.authorization.k8s.io_v1_clusterrole.yaml",
 		}
 		crb = []string{
 			"components/lvms/topolvm-controller_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml",
 			"components/lvms/topolvm-csi-provisioner_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml",
 			"components/lvms/topolvm-node-scc_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml",
 			"components/lvms/topolvm-node_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml",
+			"components/lvms/topolvm-controller-resizer_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml",
 		}
 		cd = []string{
 			"components/lvms/csi-driver.yaml",
