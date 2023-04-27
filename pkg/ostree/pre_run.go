1e04371a376d0c1dfa17252da0912d116b83449a
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-04-27 16:02:52 +0200

    make sure microshift is not running

--- pkg/ostree/pre_run.go
+++ pkg/ostree/pre_run.go
@@ -2,13 +2,34 @@ package ostree
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/openshift/microshift/pkg/backup"
 	"k8s.io/klog/v2"
 )
 
+func microshiftShouldNotRun() error {
+	cmd := exec.Command("sh", "-c", "ps aux | grep  -v grep | grep 'microshift run'")
+	out, err := cmd.CombinedOutput()
+	sout := string(out)
+
+	if err == nil {
+		klog.Infof("Detected that MicroShift is already running: %v", sout)
+		return fmt.Errorf("command requires that MicroShift must not run")
+	}
+	if err != nil && sout != "" {
+		klog.Info("Unexpected error when checking if MicroShift is running already: %v", err)
+		return err
+	}
+	return nil
+}
+
 func PreRun() error {
 	klog.Info("Pre-run procedure starting")
+
+	if err := microshiftShouldNotRun(); err != nil {
+		return err
+	}
 
 	action, err := preRunActionFromDisk()
 	if err != nil {
