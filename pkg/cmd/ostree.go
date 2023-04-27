588cc62a4b55907e106f65e1de807a36398a298c
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-04-27 15:40:12 +0200

    protect ostree commands against running on non-ostree

--- pkg/cmd/ostree.go
+++ pkg/cmd/ostree.go
@@ -1,9 +1,24 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/openshift/microshift/pkg/ostree"
+	"github.com/openshift/microshift/pkg/util"
 	"github.com/spf13/cobra"
 )
+
+func failIfNotOstree(*cobra.Command, []string) error {
+	ostreeBootedFile := "/run/ostree-booted"
+	exists, err := util.CheckIfFileExists(ostreeBootedFile)
+	if exists {
+		return nil
+	}
+	if !exists {
+		return fmt.Errorf("command is intended for ostree-based systems only")
+	}
+	return fmt.Errorf("failed checking if %s exists: %w", ostreeBootedFile, err)
+}
 
 func NewOstreeCommand() *cobra.Command {
 	preRun := &cobra.Command{
@@ -12,6 +27,7 @@ func NewOstreeCommand() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return ostree.PreRun()
 		},
+		PreRunE: failIfNotOstree,
 	}
 	scheduleBackup := &cobra.Command{
 		Use:   "schedule-backup",
@@ -19,6 +35,7 @@ func NewOstreeCommand() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return ostree.ScheduleBackup()
 		},
+		PreRunE: failIfNotOstree,
 	}
 
 	cmd := &cobra.Command{
