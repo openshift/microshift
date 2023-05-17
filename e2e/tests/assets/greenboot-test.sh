6158c42005e49ad3ccfc82a9de8ff6c0a305bbfa
Gregory Giguashvili <ggiguash@redhat.com>
2023-05-17 05:35:20 +0000

    Increate greenboot test timeout to 4m

--- e2e/tests/assets/greenboot-test.sh
+++ e2e/tests/assets/greenboot-test.sh
@@ -31,7 +31,7 @@ function check_greenboot_exit_status() {
 # Initial check must succeed (set timeout of 180s to speed up the process)
 #
 tee /etc/greenboot/greenboot.conf &>/dev/null <<EOF
-MICROSHIFT_WAIT_TIMEOUT_SEC=180
+MICROSHIFT_WAIT_TIMEOUT_SEC=240
 EOF
 check_greenboot_exit_status 0 1
 
