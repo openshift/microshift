5540b07abcc959dfc8b3b5ce1dc29cfc875a4274
Doug Hellmann <doug@doughellmann.com>
2023-05-17 11:40:57 -0400

    USHIFT-1265: update configure-vm.sh to install go 1.20.4

--- scripts/devenv-builder/configure-vm.sh
+++ scripts/devenv-builder/configure-vm.sh
@@ -88,6 +88,22 @@ if ${INSTALL_BUILD_DEPS} || ${BUILD_AND_RUN}; then
     sudo systemctl enable --now cockpit.socket
 fi
 
+GO_VER=1.20.4
+GO_ARCH=$([ "$(uname -i)" == "x86_64" ] && echo "amd64" || echo "arm64")
+GO_INSTALL_DIR="/usr/local/go${GO_VER}"
+if ${INSTALL_BUILD_DEPS} && [ ! -d "${GO_INSTALL_DIR}" ]; then
+    echo "Installing go ${GO_VER}..."
+    # This is installed into different location (/usr/local/bin/go) from dnf installed Go (/usr/bin/go) so it doesn't conflict
+    # /usr/local/bin is before /usr/bin in $PATH so newer one is picked up
+    curl -L -o "go${GO_VER}.linux-${GO_ARCH}.tar.gz" "https://go.dev/dl/go${GO_VER}.linux-${GO_ARCH}.tar.gz" &&
+        sudo rm -rf "/usr/local/go${GO_VER}" && \
+            sudo mkdir -p "/usr/local/go${GO_VER}" && \
+            sudo tar -C "/usr/local/go${GO_VER}" -xzf "go${GO_VER}.linux-${GO_ARCH}.tar.gz" --strip-components 1 && \
+            sudo rm -rfv /usr/local/bin/{go,gofmt} && \
+            sudo ln --symbolic /usr/local/go${GO_VER}/bin/{go,gofmt} /usr/local/bin/ && \
+            rm -rfv "go${GO_VER}.linux-${GO_ARCH}.tar.gz"
+fi
+
 if ${BUILD_AND_RUN}; then
     # Build MicroShift
     # https://github.com/openshift/microshift/blob/main/docs/devenv_setup.md#build-microshift
