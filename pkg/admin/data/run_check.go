52fe5d387a08446ebcdc5fc39d9e43ffd386c205
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-05-22 17:45:29 +0200

    check if svc is inactive instead of `ps aux`

--- pkg/admin/data/run_check.go
+++ pkg/admin/data/run_check.go
@@ -8,18 +8,29 @@ import (
 	"k8s.io/klog/v2"
 )
 
-func microshiftShouldNotRun() error {
-	cmd := exec.Command("sh", "-c", "ps aux | grep  -v grep | grep 'microshift run'")
-	out, err := cmd.CombinedOutput()
-	sout := strings.TrimSpace(string(out))
+const (
+	expectedState = "inactive"
+)
 
-	if err == nil {
-		klog.InfoS("MicroShift is running", "process", sout)
-		return fmt.Errorf("command requires that MicroShift must not run")
+var (
+	services = []string{"microshift.service", "microshift-etcd.scope"}
+)
+
+func microshiftShouldNotRun() error {
+	for _, service := range services {
+		cmd := exec.Command("systemctl", "show", "-p", "ActiveState", "--value", service)
+		out, err := cmd.CombinedOutput()
+		state := strings.TrimSpace(string(out))
+		if err != nil {
+			return fmt.Errorf("error when checking if %s is active: %w", service, err)
+		}
+
+		if state != expectedState {
+			return fmt.Errorf("%s is %s - expected to be %s", service, state, expectedState)
+		}
+
+		klog.Infof("Service %s is %s", service, state)
 	}
-	if err != nil && sout != "" {
-		klog.ErrorS(err, "Unexpected error when checking if MicroShift is running already", "output", sout)
-		return fmt.Errorf("checking if MicroShift is running failed: %w", err)
-	}
+
 	return nil
 }
