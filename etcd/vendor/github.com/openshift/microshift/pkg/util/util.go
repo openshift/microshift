e2f3f0196af35ec43c15b0f6fcbbf9c63db1c8b5
ci-robot <ci-robot@openshift.io>
2023-06-05 05:16:58 +0000

    update etcd/vendor

--- etcd/vendor/github.com/openshift/microshift/pkg/util/util.go
+++ etcd/vendor/github.com/openshift/microshift/pkg/util/util.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func Must(err error) {
@@ -15,4 +17,18 @@ func Default(s string, defaultS string) string {
 		return defaultS
 	}
 	return s
+}
+
+func PathExists(path string) (bool, error) {
+	if _, err := os.Stat(path); err == nil {
+		return true, nil
+	} else if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	} else {
+		return false, err
+	}
+}
+
+func MakeDir(path string) error {
+	return os.MkdirAll(path, 0700)
 }
