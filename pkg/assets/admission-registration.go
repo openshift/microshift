41e88a26a9052904c1e9ff80c847944a2b38ffbf
Jon Cope <jcope@redhat.com>
2023-06-26 12:19:49 -0500

    refactored schema handling, current schema not recognized by universal decoder
    Signed-off-by: Jon Cope <jcope@redhat.com>

--- pkg/assets/admission-registration.go
+++ pkg/assets/admission-registration.go
@@ -3,12 +3,11 @@ package assets
 import (
 	"context"
 	"fmt"
-
 	"github.com/openshift/library-go/pkg/operator/resource/resourceapply"
-	admissionregistrationV1 "k8s.io/api/admissionregistration/v1"
+	arV1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	admclientV1 "k8s.io/client-go/kubernetes/typed/admissionregistration/v1"
+	arClientV1 "k8s.io/client-go/kubernetes/typed/admissionregistration/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
@@ -16,14 +15,10 @@ import (
 	embedded "github.com/openshift/microshift/assets"
 )
 
-var (
-	admScheme = runtime.NewScheme()
-	admCodecs = serializer.NewCodecFactory(admScheme)
-)
-
 type validationWebhookCfg struct {
-	Client                  *admclientV1.AdmissionregistrationV1Client
-	validationWebhookConfig *admissionregistrationV1.ValidatingWebhookConfiguration
+	Client                  *arClientV1.AdmissionregistrationV1Client
+	validationWebhookConfig *arV1.ValidatingWebhookConfiguration
+	codec                   serializer.CodecFactory
 }
 
 func (v *validationWebhookCfg) Reader(objBytes []byte, renderFunc RenderFunc, params RenderParams) {
@@ -34,24 +29,24 @@ func (v *validationWebhookCfg) Reader(objBytes []byte, renderFunc RenderFunc, pa
 			panic(err)
 		}
 	}
-	obj, err := runtime.Decode(admCodecs.UniversalDecoder(admissionregistrationV1.SchemeGroupVersion), objBytes)
+	obj, err := runtime.Decode(v.codec.UniversalDecoder(arV1.SchemeGroupVersion), objBytes)
 	if err != nil {
 		panic(err)
 	}
-	v.validationWebhookConfig = obj.(*admissionregistrationV1.ValidatingWebhookConfiguration)
+	v.validationWebhookConfig = obj.(*arV1.ValidatingWebhookConfiguration)
 }
 
 func (v *validationWebhookCfg) Applier(ctx context.Context) error {
-	_, _, err := resourceapply.ApplyValidatingWebhookConfigurationImproved(ctx, v.Client, assetsEventRecorder, v.validationWebhookConfig, nil)
+	_, _, err := resourceapply.ApplyValidatingWebhookConfigurationImproved(ctx, v.Client, assetsEventRecorder, v.validationWebhookConfig, resourceapply.NewResourceCache())
 	return err
 }
 
-func admissionRegistrationClient(kubeconfigPath string) *admclientV1.AdmissionregistrationV1Client {
+func admissionRegistrationClient(kubeconfigPath string) *arClientV1.AdmissionregistrationV1Client {
 	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
 		klog.Fatalf("failed to create admission-registration client: %w", err)
 	}
-	return admclientV1.NewForConfigOrDie(rest.AddUserAgent(restConfig, "admission-registration"))
+	return arClientV1.NewForConfigOrDie(rest.AddUserAgent(restConfig, "admission-registration"))
 }
 
 func applyAdmissionRegistration(ctx context.Context, admissionRegistrations []string, applier readerApplier, render RenderFunc, params RenderParams) error {
@@ -74,7 +69,13 @@ func applyAdmissionRegistration(ctx context.Context, admissionRegistrations []st
 }
 
 func ApplyValidatingWebhookConfiguration(ctx context.Context, admissionRegistrations []string, kubeconfigPath string) error {
+	scheme := runtime.NewScheme()
+	if err := arV1.AddToScheme(scheme); err != nil {
+		return err
+	}
+
 	a := &validationWebhookCfg{}
 	a.Client = admissionRegistrationClient(kubeconfigPath)
+	a.codec = serializer.NewCodecFactory(scheme)
 	return applyAdmissionRegistration(ctx, admissionRegistrations, a, nil, nil)
 }
