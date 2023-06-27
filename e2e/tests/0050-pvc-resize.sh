7c946663653a3a566b32c0d3054fec37f33bd7db
Jon Cope <jcope@redhat.com>
2023-06-27 17:24:46 -0500

    use kustomize to dynamically patch PVCs to a pod manifest. This is useful for tests that require the same basic pod, but varying PVCs.

--- e2e/tests/0050-pvc-resize.sh
+++ e2e/tests/0050-pvc-resize.sh
@@ -5,28 +5,35 @@ PS4='+ $(date "+%T.%N")\011 '
 set -x
 
 SCRIPT_PATH="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+POD_MANIFEST="${SCRIPT_PATH}/assets/kustomizations/patches/pvc-thick"
 
 RETRIES=5
-BACKOFF=20
+RETRY_WAIT=20
 
 wait_until() {
     local cmd=$*
     for _ in $(seq "${RETRIES}"); do
         ${cmd} && return 0
-        sleep "${BACKOFF}"
+        sleep "${RETRY_WAIT}"
     done
     return 1
 }
 
+MANIFEST="$(mktemp -d)/manifest.yaml"
+oc kustomize "${POD_MANIFEST}" | tee "${MANIFEST}"
+
+CLAIM_NAME="$(yq 'select(.kind == "PersistentVolumeClaim") | .metadata.name' "${MANIFEST}")"
+POD_NAME="$(yq 'select(.kind == "Pod") | .metadata.name' "${MANIFEST}")"
+
 cleanup() {
-    oc delete -f "${SCRIPT_PATH}/assets/pod-with-pvc.yaml"
+    oc delete -f "${MANIFEST}"
 }
 trap 'cleanup' EXIT
 
-oc create -f "${SCRIPT_PATH}/assets/pod-with-pvc.yaml"
-oc wait --for=condition=Ready --timeout=120s pod/test-pod
+oc apply -f "${MANIFEST}"
+oc wait --for=condition=Ready --timeout=120s pod/"${POD_NAME}"
 
 RESIZE_TO=2Gi
 TIME_OUT=3m
-oc patch pvc test-claim -p '{"spec":{"resources":{"requests":{"storage":"'${RESIZE_TO}'"}}}}'
-oc wait --timeout ${TIME_OUT} --for=jsonpath="{.spec.resources.requests.storage}"=${RESIZE_TO} pvc/test-claim
+oc patch pvc "${CLAIM_NAME}" -p '{"spec":{"resources":{"requests":{"storage":"'${RESIZE_TO}'"}}}}'
+oc wait --timeout ${TIME_OUT} --for=jsonpath="{.spec.resources.requests.storage}"=${RESIZE_TO} pvc/"${CLAIM_NAME}"
