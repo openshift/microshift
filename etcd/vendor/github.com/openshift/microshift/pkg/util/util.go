a6dc60c5e17aa64eb65b340958a8fbc20900523b
Doug Hellmann <doug@doughellmann.com>
2023-07-06 15:44:55 -0400

    update etcd/vendor

--- etcd/vendor/github.com/openshift/microshift/pkg/util/util.go
+++ etcd/vendor/github.com/openshift/microshift/pkg/util/util.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"os"
+
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func Must(err error) {
@@ -31,4 +33,34 @@ func PathExists(path string) (bool, error) {
 
 func MakeDir(path string) error {
 	return os.MkdirAll(path, 0700)
+}
+
+func PathExistsAndIsNotEmpty(path string, ignores ...string) (bool, error) {
+	fi, err := os.Stat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		} else {
+			return false, fmt.Errorf("checking if path (%s) exists failed: %w", path, err)
+		}
+	}
+
+	if !fi.IsDir() {
+		return fi.Size() != 0, nil
+	}
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return false, fmt.Errorf("failed to ReadDir %q: %w", path, err)
+	}
+
+	iset := sets.New[string](ignores...)
+	for _, f := range files {
+		if iset.Has(f.Name()) {
+			continue
+		}
+		return true, nil
+	}
+
+	return false, nil
 }
