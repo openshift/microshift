6ba63980fee24f3af92eccf15e6a8120d78cc692
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-07-11 13:56:06 +0200

    remove any notion of migration from version checking

--- pkg/admin/prerun/version_test.go
+++ pkg/admin/prerun/version_test.go
@@ -8,55 +8,48 @@ import (
 
 func TestCheckVersionDiff(t *testing.T) {
 	testData := []struct {
-		name                      string
-		execVer                   versionMetadata
-		dataVer                   versionMetadata
-		expectedMigrationRequired bool
-		errExpected               bool
+		name        string
+		execVer     versionMetadata
+		dataVer     versionMetadata
+		errExpected bool
 	}{
 		{
-			name:                      "equal versions: no migration, no error",
-			execVer:                   versionMetadata{Major: 4, Minor: 14},
-			dataVer:                   versionMetadata{Major: 4, Minor: 14},
-			expectedMigrationRequired: false,
-			errExpected:               false,
+			name:        "equal versions: no migration, no error",
+			execVer:     versionMetadata{Major: 4, Minor: 14},
+			dataVer:     versionMetadata{Major: 4, Minor: 14},
+			errExpected: false,
 		},
 		{
-			name:                      "X versions must be the same",
-			execVer:                   versionMetadata{Major: 4, Minor: 14},
-			dataVer:                   versionMetadata{Major: 5, Minor: 14},
-			expectedMigrationRequired: false,
-			errExpected:               true,
+			name:        "X versions must be the same",
+			execVer:     versionMetadata{Major: 4, Minor: 14},
+			dataVer:     versionMetadata{Major: 5, Minor: 14},
+			errExpected: true,
 		},
 		{
-			name:                      "binary must not be older than data",
-			execVer:                   versionMetadata{Major: 4, Minor: 14},
-			dataVer:                   versionMetadata{Major: 4, Minor: 15},
-			expectedMigrationRequired: false,
-			errExpected:               true,
+			name:        "binary must not be older than data",
+			execVer:     versionMetadata{Major: 4, Minor: 14},
+			dataVer:     versionMetadata{Major: 4, Minor: 15},
+			errExpected: true,
 		},
 		{
-			name:                      "binary must be newer only by one minor version",
-			execVer:                   versionMetadata{Major: 4, Minor: 15},
-			dataVer:                   versionMetadata{Major: 4, Minor: 14},
-			expectedMigrationRequired: true,
-			errExpected:               false,
+			name:        "binary must be newer only by one minor version",
+			execVer:     versionMetadata{Major: 4, Minor: 15},
+			dataVer:     versionMetadata{Major: 4, Minor: 14},
+			errExpected: false,
 		},
 		{
-			name:                      "binary newer more than one minor version is not supported",
-			execVer:                   versionMetadata{Major: 4, Minor: 15},
-			dataVer:                   versionMetadata{Major: 4, Minor: 13},
-			expectedMigrationRequired: false,
-			errExpected:               true,
+			name:        "binary newer more than one minor version is not supported",
+			execVer:     versionMetadata{Major: 4, Minor: 15},
+			dataVer:     versionMetadata{Major: 4, Minor: 13},
+			errExpected: true,
 		},
 	}
 
 	for _, td := range testData {
 		td := td
 		t.Run(td.name, func(t *testing.T) {
-			migrationRequired, err := checkVersionDiff(td.execVer, td.dataVer)
+			err := checkVersionCompatibility(td.execVer, td.dataVer)
 
-			assert.Equal(t, td.expectedMigrationRequired, migrationRequired)
 			if td.errExpected {
 				assert.Error(t, err)
 			} else {
