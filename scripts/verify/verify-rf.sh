dc5225f5bf14d844ce0dbae25838bfc14dacdacb
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-07-14 09:24:30 +0200

    increase robocop's limits
    Increasing 504 (too long test case) to
    accomodate more comment lines.
    
    Increasing 505 (too many calls in test case)
    because upgrade tests need to persist some
    variables for later steps.

--- scripts/verify/verify-rf.sh
+++ scripts/verify/verify-rf.sh
@@ -8,7 +8,14 @@ RF_VENV="${ROOTDIR}/_output/robotenv"
 
 cd "${ROOTDIR}/test"
 
+# Configured robocop rules:
+# https://robocop.readthedocs.io/en/stable/rules.html#too-long-test-case-w0504
+# https://robocop.readthedocs.io/en/stable/rules.html#too-many-calls-in-test-case-w0505
+
 set -x
-"${RF_VENV}/bin/robocop" --exclude 1015
+"${RF_VENV}/bin/robocop" \
+    --exclude 1015 \
+    --configure 0504:max_len:40 \
+    --configure 0505:max_calls:20
 
 "${RF_VENV}/bin/robotidy" --check --diff .
