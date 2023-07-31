fc4731ebe4b77189041cff1bca6a5b8341173337
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-07-31 15:29:32 +0200

    USHIFT-1459: Add custom port to cncf conformance scenario

--- test/scenarios/periodics/rhel-9.2-cncf-conformance.sh
+++ test/scenarios/periodics/rhel-9.2-cncf-conformance.sh
@@ -5,23 +5,24 @@
 prepare_hosts() {
     local primary_host_ip=$(cat "${SCENARIO_INFO_DIR}/${SCENARIO}/vms/host1/public_ip")
     local secondary_host_ip=$(cat "${SCENARIO_INFO_DIR}/${SCENARIO}/vms/host2/public_ip")
-    #tOdo need the ports too
+    local primary_host_ssh_port=$(cat "${SCENARIO_INFO_DIR}/${SCENARIO}/vms/host1/ssh_port")
+    local secondary_host_ssh_port=$(cat "${SCENARIO_INFO_DIR}/${SCENARIO}/vms/host2/ssh_port")
     local primary_host_name="$(full_vm_name host1)"
     local secondary_host_name="$(full_vm_name host2)"
 
-    scp "${ROOTDIR}/scripts/multinode/configure-pri.sh" "redhat@${primary_host_ip}":
-    ssh redhat@${primary_host_ip} ./configure-pri.sh "${primary_host_name}" "${primary_host_ip}" "${secondary_host_name}" "${secondary_host_ip}"
+    scp -P "${primary_host_ssh_port}" "${ROOTDIR}/scripts/multinode/configure-pri.sh" "redhat@${primary_host_ip}":
+    ssh -p "${primary_host_ssh_port}" "redhat@${primary_host_ip}" ./configure-pri.sh "${primary_host_name}" "${primary_host_ip}" "${secondary_host_name}" "${secondary_host_ip}"
 
-    scp -3 \
+    scp -3 -P "${primary_host_ssh_port}" \
         redhat@${primary_host_ip}:/home/redhat/kubelet-${secondary_host_name}.{key,crt} \
         redhat@${primary_host_ip}:/home/redhat/kubeconfig-${primary_host_name} \
         redhat@${secondary_host_ip}:
 
-    scp "${ROOTDIR}/scripts/multinode/configure-sec.sh" redhat@${secondary_host_ip}:
-    ssh redhat@${secondary_host_ip} ./configure-sec.sh "${primary_host_name}" "${primary_host_ip}" "${secondary_host_name}" "${secondary_host_ip}"
+    scp -P "${secondary_host_ssh_port}" "${ROOTDIR}/scripts/multinode/configure-sec.sh" redhat@${secondary_host_ip}:
+    ssh -p "${secondary_host_ssh_port}" "redhat@${secondary_host_ip}" ./configure-sec.sh "${primary_host_name}" "${primary_host_ip}" "${secondary_host_name}" "${secondary_host_ip}"
 
     export KUBECONFIG=$(mktemp /tmp/microshift-kubeconfig.XXXXXXXXXX)
-    scp redhat@${primary_host_ip}:/home/redhat/kubeconfig-${primary_host_name} ${KUBECONFIG}
+    scp -P "${primary_host_ssh_port}" redhat@${primary_host_ip}:/home/redhat/kubeconfig-${primary_host_name} ${KUBECONFIG}
     echo "${primary_host_ip} ${primary_host_name}" | sudo tee -a /etc/hosts &>/dev/null
 }
 
