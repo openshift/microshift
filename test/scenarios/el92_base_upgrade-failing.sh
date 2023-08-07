f3d1a5aa090641774078cc48eea4556400e9315f
Patryk Matuszak <pmtk@redhat.com>
2023-08-07 18:14:58 +0200

    Scenario filenames: rename for accurate description

---
+++ test/scenarios/el92_base_upgrade-failing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Sourced from cleanup_scenario.sh and uses functions defined there.
+
+scenario_create_vms() {
+	prepare_kickstart host1 kickstart.ks.template rhel-9.2-microshift-source-base
+	launch_vm host1
+}
+
+scenario_remove_vms() {
+	remove_vm host1
+}
+
+scenario_run_tests() {
+	run_tests host1 \
+		--variable "TARGET_REF:rhel-9.2-microshift-source" \
+		--variable "REASON:fail_greenboot" \
+		suites-ostree/failed-upgrade.robot
+}
