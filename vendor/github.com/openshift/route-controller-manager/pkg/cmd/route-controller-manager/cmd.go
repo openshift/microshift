1e9796d2e4edc9fdc68f06a1bc1f6bdc7c7a4dc8
Doug Hellmann <doug@doughellmann.com>
2023-08-11 11:20:07 -0400

    update microshift/vendor

--- vendor/github.com/openshift/route-controller-manager/pkg/cmd/route-controller-manager/cmd.go
+++ vendor/github.com/openshift/route-controller-manager/pkg/cmd/route-controller-manager/cmd.go
@@ -2,125 +2,42 @@ package openshift_controller_manager
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"path"
-
 	"github.com/spf13/cobra"
+	"os"
 
-	kerrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/tools/clientcmd/api"
-	"k8s.io/klog/v2"
-	kcmdutil "k8s.io/kubectl/pkg/cmd/util"
-	"k8s.io/kubectl/pkg/util/templates"
+	corev1 "k8s.io/api/core/v1"
 
-	configv1 "github.com/openshift/api/config/v1"
-	openshiftcontrolplanev1 "github.com/openshift/api/openshiftcontrolplane/v1"
-	"github.com/openshift/library-go/pkg/config/helpers"
-	"github.com/openshift/library-go/pkg/serviceability"
+	"github.com/openshift/library-go/pkg/controller/controllercmd"
+
+	rcmversion "github.com/openshift/route-controller-manager/pkg/version"
 )
 
-type RouteControllerManager struct {
-	ConfigFilePath string
-	Kubeconfig     string
-	Namespace      string
-	Output         io.Writer
-}
+const (
+	podNameEnv      = "POD_NAME"
+	podNamespaceEnv = "POD_NAMESPACE"
+)
 
-var longDescription = templates.LongDesc(`
-	Start the additional Ingress and Route controllers`)
-
-func NewRouteControllerManagerCommand(name string, out, errout io.Writer, ctx context.Context) *cobra.Command {
-	options := &RouteControllerManager{Output: out}
-
-	cmd := &cobra.Command{
-		Use:   name,
-		Short: "Start the additional Ingress and Route controllers",
-		Long:  longDescription,
-		Run: func(c *cobra.Command, args []string) {
-			kcmdutil.CheckErr(options.Validate())
-
-			serviceability.StartProfiler()
-
-			if err := options.StartControllerManager(ctx); err != nil {
-				if kerrors.IsInvalid(err) {
-					if details := err.(*kerrors.StatusError).ErrStatus.Details; details != nil {
-						fmt.Fprintf(errout, "Invalid %s %s\n", details.Kind, details.Name)
-						for _, cause := range details.Causes {
-							fmt.Fprintf(errout, "  %s: %s\n", cause.Field, cause.Message)
-						}
-						os.Exit(255)
-					}
-				}
-				klog.Fatal(fmt.Sprintf("fatal error occurred: %v", err))
-			}
-		},
-	}
-
-	flags := cmd.Flags()
-	// This command only supports reading from config
-	flags.StringVar(&options.ConfigFilePath, "config", options.ConfigFilePath, "Location of the master configuration file to run from.")
-	cmd.MarkFlagFilename("config", "yaml", "yml")
-	cmd.MarkFlagRequired("config")
-
-	// dummy flags needed for a switch to library-go server and ControlerCommand
-	flags.StringVar(&options.Kubeconfig, "kubeconfig", options.Kubeconfig, "dummy flag required for a switch to library-go server and ControlerCommand")
-	flags.StringVar(&options.Namespace, "namespace", options.Namespace, "dummy flag required for a switch to library-go server and ControlerCommand")
+func NewRouteControllerManagerCommand(name string) *cobra.Command {
+	cmd := controllercmd.NewControllerCommandConfig("route-controller-manager", rcmversion.Get(), RunRouteControllerManager).
+		WithComponentOwnerReference(&corev1.ObjectReference{
+			Kind:      "Pod",
+			Name:      os.Getenv(podNameEnv),
+			Namespace: getNamespace(),
+		}).
+		NewCommandWithContext(context.Background())
+	cmd.Use = name
+	cmd.Short = "Start the additional Ingress and Route controllers"
 
 	return cmd
 }
 
-func (o *RouteControllerManager) Validate() error {
-	if len(o.ConfigFilePath) == 0 {
-		return errors.New("--config is required for this command")
+// getNamespace returns in-cluster namespace
+func getNamespace() string {
+	if nsBytes, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
+		return string(nsBytes)
 	}
-
-	return nil
-}
-
-// StartControllerManager takes the options, starts the controllers and then blocks forever
-func (o *RouteControllerManager) StartControllerManager(ctx context.Context) error {
-	// try to decode into our new types first.  right now there is no validation, no file path resolution.  this unsticks the operator to start.
-	// TODO add those things
-	configContent, err := ioutil.ReadFile(o.ConfigFilePath)
-	if err != nil {
-		return err
+	if podNamespace := os.Getenv(podNamespaceEnv); len(podNamespace) > 0 {
+		return podNamespace
 	}
-	scheme := runtime.NewScheme()
-	utilruntime.Must(openshiftcontrolplanev1.Install(scheme))
-	codecs := serializer.NewCodecFactory(scheme)
-	obj, err := runtime.Decode(codecs.UniversalDecoder(openshiftcontrolplanev1.GroupVersion, configv1.GroupVersion), configContent)
-	if err != nil {
-		return err
-	}
-
-	// Resolve relative to CWD
-	absoluteConfigFile, err := api.MakeAbs(o.ConfigFilePath, "")
-	if err != nil {
-		return err
-	}
-	configFileLocation := path.Dir(absoluteConfigFile)
-
-	config := obj.(*openshiftcontrolplanev1.OpenShiftControllerManagerConfig)
-	/// this isn't allowed to be nil when by itself.
-	// TODO remove this when the old path is gone.
-	if config.ServingInfo == nil {
-		config.ServingInfo = &configv1.HTTPServingInfo{}
-	}
-	if err := helpers.ResolvePaths(getOpenShiftControllerConfigFileReferences(config), configFileLocation); err != nil {
-		return err
-	}
-	setRecommendedOpenShiftControllerConfigDefaults(config)
-
-	clientConfig, err := helpers.GetKubeClientConfig(config.KubeClientConfig)
-	if err != nil {
-		return err
-	}
-	return RunRouteControllerManager(config, clientConfig, ctx)
+	return "openshift-route-controller-manager"
 }
