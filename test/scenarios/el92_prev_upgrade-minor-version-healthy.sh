d0ea0faf2f2c7762d6ae9a50ad47819cc5c665d5
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2023-08-16 08:47:07 +0200

    fix install commits

--- test/scenarios/el92_prev_upgrade-minor-version-healthy.sh
+++ test/scenarios/el92_prev_upgrade-minor-version-healthy.sh
@@ -3,12 +3,7 @@
 # Sourced from cleanup_scenario.sh and uses functions defined there.
 
 scenario_create_vms() {
-    # Determine the starting image based on the source tree current
-    # version, minus one.
-    local start_image
-    start_image="rhel-9.2-microshift-4.$(previous_minor_version)"
-
-    prepare_kickstart host1 kickstart.ks.template "${start_image}"
+    prepare_kickstart host1 kickstart.ks.template el92-prev
     launch_vm host1
 }
 
