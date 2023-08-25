f43cd3ce1b9c8e05089ddebe3f6809ee3ab4e605
ehila <ehila@redhat.com>
2023-08-25 01:46:19 -0400

    refactor: changed to make selinux tests callable
    reworked to make selinux callable validate functions that can be used in tests
    removed scenario to avoid VM creation
    
    Signed-off-by: ehila <ehila@redhat.com>
    
    fix: added more context in log added check for 4.13
    
    Signed-off-by: ehila <ehila@redhat.com>
    
    fix: allow for different user context on file
    
    Signed-off-by: ehila <ehila@redhat.com>
    
    fix: remove hardcoded health.json check
    
    in non ostree systems health.json does not exist
    
    Signed-off-by: ehila <ehila@redhat.com>
    
    fix: python script and context lookup logic
    
    updated python script to use splitlines
    fixed error for backup look up, now checking access on kubeconfig
    
    Signed-off-by: ehila <ehila@redhat.com>

--- test/resources/selinux.py
+++ test/resources/selinux.py
@@ -5,7 +5,6 @@ from typing import List
 ACCESS_CHECK_MAP = {
     "/var/lib/microshift/version": ["cat"],
     "/etc/microshift/config.yaml.default": ["cat"],
-    "/var/lib/microshift-backups/health.json": ["cat"],
 }
 
 CONTEXT_CHECK_MAP = {
@@ -59,14 +58,14 @@ def get_expected_ocp_microshift_fcontext_list() -> List[str]:
 def get_fcontext_list() -> List[str]:
     context_list = "kubernetes_file_t|container_var_lib_t|kubelet_exec_t|container_t"
     semanage_filter_cmd = f"semanage fcontext -l | grep -E  \"({context_list})\" | awk '{{print $1 }}'"
-    return remote_sudo(semanage_filter_cmd).split('\n')
+    return remote_sudo(semanage_filter_cmd).splitlines()
 
 
 def get_denial_audit_log() -> List[str]:
     ausearch_filter_cmd = "ausearch --input-logs -m avc | grep microshift"
     stdout, rc = remote_sudo_rc(ausearch_filter_cmd)
     if rc == 0:
-        return stdout.split('\n')
+        return stdout.splitlines()
     return []
 
 
@@ -75,7 +74,8 @@ def run_access_check(access_check_map: dict[str, List[str]]) -> List[str]:
     allowed_access = []
     for file_path, commands in access_check_map.items():
         for command in commands:
-            stdout, rc = remote_sudo_rc(f"{runcon_cmd} {command} {file_path}")
+            stdout, rc = remote_sudo_rc(f"{runcon_cmd} {command} {file_path} 2>&1")
+            BuiltIn().should_not_match(stdout, "*No such file or directory*")
             if rc == 0:
                 allowed_access.append(f"should not have been allowed access to {file_path} by running {command}")
 
@@ -94,7 +94,27 @@ def run_fcontext_check() -> List[str]:
             stdout, rc = remote_sudo_rc(f"{ls_cmd} {file_path} | awk '{{print $1 }}'")
             BuiltIn().should_not_be_empty(stdout)
             BuiltIn().should_be_equal_as_integers(rc, 0)
-            if stdout != context:
-                incorrect_fcontext.append(f"should have the same context as {context} but got {stdout}")
+            if context_do_not_match(stdout, context):
+                incorrect_fcontext.append(f"expected {file_path} to have context of ({context}) but got ({stdout})")
 
     return incorrect_fcontext
+
+
+def context_do_not_match(context_string: str, expected_context_string: str) -> bool:
+    context_parts = context_string.split(":")
+    context_only = ":".join(context_parts[1:])
+
+    expected_context_parts = expected_context_string.split(":")
+    expected_context_only = ":".join(expected_context_parts[1:])
+
+    # Since in CI we sometimes manually create folders, the user context might be
+    # unconfined_u instead of system_u. That is valid within SELinux practices, so if the context
+    # don't match and they don't match with out the user, then we fail.
+    if context_string != expected_context_string and context_only != expected_context_only:
+        return True
+
+    # If context do match but it's not a system_u and an unconfined_u user, that's not expected and should be looked at.
+    if context_parts[0] != "system_u" and context_parts[0] != "unconfined_u":
+        return True
+
+    return False
--- test/suites/standard/validate-selinux-policy.robot
+++ test/suites/standard/validate-selinux-policy.robot
@@ -2,54 +2,22 @@
 Documentation       Tests related to upgrading MicroShift
 
 Resource            ../../resources/common.resource
-Resource            ../../resources/ostree.resource
 Resource            ../../resources/selinux.resource
 Library             Collections
 
 Suite Setup         Setup
 Suite Teardown      Teardown
 
-Test Tags           ostree
-
 
 *** Variables ***
 ${USHIFT_HOST}      ${EMPTY}
 ${USHIFT_USER}      ${EMPTY}
-${BACKUP_NAME}      backup-name
 
 
 *** Test Cases ***
-Containers Should Not Have Access To Container Var Lib Labels
-    [Documentation]    Performs a check to make sure containers can not access
-    ...    files or folders that are labeled with container var lib as well as the
-    ...    generated backup file
-
-    ${default_result}=    Run Default Container Access Check
-    Should Be Empty    ${default_result}
-
-    ${result}=    Run Container Access Check On    "${BACKUP_STORAGE}/${BACKUP_NAME}/version"
-    Should Be Empty    ${result}
-
-Folders Should Have Expected Fcontext Types
-    [Documentation]    Performs a check to make sure the folders created during rpm install
-    ...    have the expected fcontext values
-
-    ${result}=    Run Fcontext Types Check
-    Should Be Empty    ${result}
-
-Semanage Fcontext Should Have Combined List of OCP and MicroShift Rules
-    [Documentation]    Validates that the fcontext data is the combined set for
-    ...    OCP and MicroShift
-
-    ${result}=    Get Current Fcontext List
-    ${expected}=    Get Expected Fcontext List
-    Lists Should Be Equal    ${result}    ${expected}
-
-Audit Log Should Be Empty For MicroShift
-    [Documentation]    Checks that no permission denials have occured during running MicroShift
-
-    ${result}=    Get Denial Audit Log List
-    Should Be Empty    ${result}
+Standard SELinux Validation
+    [Documentation]    Performs a standard SELinux validation against the host
+    Validate SELinux
 
 
 *** Keywords ***
@@ -57,8 +25,6 @@ Setup
     [Documentation]    Test suite setup
     Check Required Env Variables
     Login MicroShift Host
-    Wait For Healthy System
-    Create Backup    ${BACKUP_NAME}
 
 Teardown
     [Documentation]    Test suite teardown
