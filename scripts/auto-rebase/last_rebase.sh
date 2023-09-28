52d38bed3b1d4389d60a28160196f837dd7ba878
ci-robot <ci-robot@openshift.io>
2023-09-28 05:15:49 +0000

    update last_rebase.sh

--- scripts/auto-rebase/last_rebase.sh
+++ scripts/auto-rebase/last_rebase.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-./scripts/auto-rebase/rebase.sh to "registry.ci.openshift.org/ocp/release:4.13.0-0.nightly-2023-09-26-015823" "registry.ci.openshift.org/ocp-arm64/release-arm64:4.13.0-0.nightly-arm64-2023-09-26-232049" "registry.access.redhat.com/lvms4/lvms-operator-bundle:v4.12"
+./scripts/auto-rebase/rebase.sh to "registry.ci.openshift.org/ocp/release:4.13.0-0.nightly-2023-09-27-193040" "registry.ci.openshift.org/ocp-arm64/release-arm64:4.13.0-0.nightly-arm64-2023-09-28-000538" "registry.access.redhat.com/lvms4/lvms-operator-bundle:v4.12"
