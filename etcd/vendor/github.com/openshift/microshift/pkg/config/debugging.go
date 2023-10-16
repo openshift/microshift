d486a323b2c60885e48a9fd4aba62bf877420153
ci-robot <ci-robot@openshift.io>
2023-10-16 05:08:01 +0000

    update etcd/vendor

--- etcd/vendor/github.com/openshift/microshift/pkg/config/debugging.go
+++ etcd/vendor/github.com/openshift/microshift/pkg/config/debugging.go
@@ -1,6 +1,13 @@
 package config
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
+
+// Use the upper-case version of the word to match the kubebuilder
+// default.
+const defaultLogLevel = "Normal"
 
 type Debugging struct {
 	// Valid values are: "Normal", "Debug", "Trace", "TraceAll".
@@ -9,15 +16,32 @@ type Debugging struct {
 	LogLevel string `json:"logLevel"`
 }
 
-// GetVerbosity returns the numerical value for LogLevel which is an enum
-func (c *Config) GetVerbosity() int {
-	var levelNames = map[string]int{
-		"normal":   2,
-		"debug":    4,
-		"trace":    6,
-		"traceall": 8,
+var logLevelNames = map[string]int{
+	"normal":   2,
+	"debug":    4,
+	"trace":    6,
+	"traceall": 8,
+}
+
+// computeLoggingSetting validates the logging setting and saves a
+// warning if there is an issue.
+func (c *Config) computeLoggingSetting() {
+	_, ok := logLevelNames[strings.ToLower(c.Debugging.LogLevel)]
+	if !ok {
+		if c.Debugging.LogLevel != "" {
+			c.AddWarning(fmt.Sprintf("Unrecognized log level %q, defaulting to %q",
+				c.Debugging.LogLevel, defaultLogLevel))
+		}
+		// Reset the value so that `show-config` reports the value
+		// being used instead of the value in the config file.
+		c.Debugging.LogLevel = defaultLogLevel
 	}
-	verbosity, ok := levelNames[strings.ToLower(c.Debugging.LogLevel)]
+}
+
+// GetVerbosity returns the numerical value for LogLevel which is an
+// enum.
+func (c *Config) GetVerbosity() int {
+	verbosity, ok := logLevelNames[strings.ToLower(c.Debugging.LogLevel)]
 	if !ok {
 		verbosity = 2
 	}
