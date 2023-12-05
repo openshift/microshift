ded292eb404049559ec38220f42489415edeb501
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-12-05 12:28:18 +0100

    USHIFT-1956: Compute containers to mirror in mirror_registry

--- test/bin/mirror_registry.sh
+++ test/bin/mirror_registry.sh
@@ -8,12 +8,26 @@ source "${SCRIPTDIR}/common.sh"
 QUAY_VERSION=1.3.9
 REGISTRY_HOST=${REGISTRY_HOST:-$(hostname):8443}
 REGISTRY_ROOT=${REGISTRY_ROOT:-${HOME}/mirror-registry}
-REGISTRY_CONTAINERS=${REGISTRY_CONTAINERS:-${HOME}/containers}
+REGISTRY_CONTAINER_DIR=${REGISTRY_CONTAINER_DIR:-${REGISTRY_ROOT}/containers}
+REGISTRY_CONTAINER_LIST=${REGISTRY_CONTAINER_LIST:-${REGISTRY_ROOT}/mirror-list.txt}
 PULL_SECRET=${PULL_SECRET:-${HOME}/.pull-secret.json}
+
+get_container_images() {
+    containers=""
+    local -r release_info_rpm=$(find "${IMAGEDIR}/rpm-repos" -name "microshift-release-info-*.rpm" | sort)
+    if [ -z "${release_info_rpm}" ] ; then
+        echo "Error: missing microshift-release-info RPMs"
+        exit 1
+    fi
+    for package in ${release_info_rpm}; do
+        containers="$(rpm2cpio "${package}" | cpio  -i --to-stdout "*release-$(uname -m).json" 2> /dev/null | jq -r '[ .images[] ] | join("\n")')\n${containers}"
+    done
+    echo -n -e "${containers}" | sort -u
+}
 
 prereqs() {
     mkdir -p "${REGISTRY_ROOT}"
-    mkdir -p "${REGISTRY_CONTAINERS}"
+    mkdir -p "${REGISTRY_CONTAINER_DIR}"
     curl -L https://github.com/quay/mirror-registry/releases/download/v${QUAY_VERSION}/mirror-registry-offline.tar.gz -o /tmp/mirror-registry-offline.tar.gz
     tar xf /tmp/mirror-registry-offline.tar.gz -C "${REGISTRY_ROOT}"
     rm -f /tmp/mirror-registry-offline.tar.gz
@@ -30,11 +44,12 @@ setup_registry() {
 }
 
 mirror_images() {
-    "${ROOTDIR}/scripts/image-builder/mirror-images.sh" --reg-to-dir "${PULL_SECRET}" "${MIRROR_CONTAINERS_LIST}" "${REGISTRY_CONTAINERS}"
-    "${ROOTDIR}/scripts/image-builder/mirror-images.sh" --dir-to-reg "${REGISTRY_ROOT}/local-auth.json" "${REGISTRY_CONTAINERS}" "${REGISTRY_HOST}"
+    get_container_images > "${REGISTRY_CONTAINER_LIST}"
+    "${ROOTDIR}/scripts/image-builder/mirror-images.sh" --reg-to-dir "${PULL_SECRET}" "${REGISTRY_CONTAINER_LIST}" "${REGISTRY_CONTAINER_DIR}"
+    "${ROOTDIR}/scripts/image-builder/mirror-images.sh" --dir-to-reg "${REGISTRY_ROOT}/local-auth.json" "${REGISTRY_CONTAINER_DIR}" "${REGISTRY_HOST}"
     jq -s '.[0] * .[1]' "${PULL_SECRET}" "${REGISTRY_ROOT}/local-auth.json" > "${PULL_SECRET}.tmp"
     mv "${PULL_SECRET}.tmp" "${PULL_SECRET}"
-    rm -rf "${REGISTRY_CONTAINERS}"
+    rm -rf "${REGISTRY_CONTAINER_DIR}"
 }
 
 prereqs
