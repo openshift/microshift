aa64f3dd35f4b869e3dea9a2f6c27cc0340a03ab
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2023-12-14 12:18:29 +0100

    USHIFT-1956: Remove workaround for stuck images
    Removing this workaround as part of the local registry to detect if
    issues come from the registry itself or there is some other bug in
    another part of the system.
    By not deleting pods we are trusting the registry does not have any
    connectivity issues, as its local now.

--- test/bin/scenario.sh
+++ test/bin/scenario.sh
@@ -278,10 +278,8 @@ wait_for_greenboot() {
 
     local -r start_time=$(date +%s)
     local -r ssh_cmd="ssh -oConnectTimeout=10 -oBatchMode=yes -oStrictHostKeyChecking=accept-new redhat@${ip}"
-    local retry_count=2
     while [ $(( $(date +%s) - start_time )) -lt "${VM_BOOT_TIMEOUT}" ] ; do
         local svc_state
-
         svc_state="$(${ssh_cmd} systemctl show --property=SubState --value greenboot-healthcheck || true)"
         if [ "${svc_state}" = "exited" ] ; then
             record_junit "${vmname}" "greenboot-check" "OK"
@@ -290,26 +288,6 @@ wait_for_greenboot() {
 
         # Print the last log and check for terminal failure
         ${ssh_cmd} "sudo journalctl -n 10 -u greenboot-healthcheck" || true
-        if [ "${svc_state}" = "failed" ] ; then
-            # FIXME: See OCPBUGS-24222
-            # Workaround for TopoLVM images getting stuck
-            # Delete the pods (TopoLVM included) and retry greenboot check
-            # Remove this code when the problem is addressed
-            if [ ${retry_count} -gt 0 ] ; then
-                echo "Deleting MicroShift pods and retrying the greenboot checks (${retry_count} attempts remaining)"
-                (( retry_count-- ))
-                # The '--ovn' option keeps the data, but deletes all pods and networking components
-                if ! ${ssh_cmd} "sudo microshift-cleanup-data --ovn ; sudo systemctl enable --now microshift" ; then
-                    echo "WARNING: MicroShift data cleanup returned an error"
-                fi
-                if ! ${ssh_cmd} "sudo systemctl restart --no-block greenboot-healthcheck.service" ; then
-                    echo "WARNING: Greenboot service restart returned an error"
-                fi
-            else
-                echo "The greenboot service reported a failed state, no need to wait any longer"
-                break
-            fi
-        fi
 
         date
         sleep 10
