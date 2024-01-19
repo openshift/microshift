c7b0e549151822d49dc692cd00438a9e5ee0895d
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2024-01-19 11:37:34 +0100

    USHIFT-2201: Update golang to 1.21.3

--- scripts/devenv-builder/configure-vm.sh
+++ scripts/devenv-builder/configure-vm.sh
@@ -115,7 +115,7 @@ if ${INSTALL_BUILD_DEPS} || ${BUILD_AND_RUN}; then
     sudo systemctl enable --now cockpit.socket
 fi
 
-GO_VER=1.20.10
+GO_VER=1.21.3  # released 2023-10-10 (matches CI images)
 GO_ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
 GO_INSTALL_DIR="/usr/local/go${GO_VER}"
 if ${INSTALL_BUILD_DEPS} && [ ! -d "${GO_INSTALL_DIR}" ]; then
