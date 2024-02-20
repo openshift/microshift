df2be903ddd635bd6858a6ab24a315dbbe53b4c5
ci-robot <ci-robot@openshift.io>
2024-02-20 05:17:13 +0000

    update microshift/vendor

--- vendor/k8s.io/kubernetes/pkg/features/kube_features.go
+++ vendor/k8s.io/kubernetes/pkg/features/kube_features.go
@@ -1209,6 +1209,8 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	genericfeatures.ServerSideFieldValidation: {Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.29
 
+	genericfeatures.StructuredAuthenticationConfiguration: {Default: false, PreRelease: featuregate.Alpha},
+
 	genericfeatures.UnauthenticatedHTTP2DOSMitigation: {Default: true, PreRelease: featuregate.Beta},
 
 	// inherited features from apiextensions-apiserver, relisted here to get a conflict if it is changed
