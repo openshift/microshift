1314b2ebcc424bcf3074f0b82c752192e472f45c
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-03-01 17:29:27 +0100

    USHIFT-2444: Add deletion of cluster wide resources on disable

--- pkg/assets/rbac.go
+++ pkg/assets/rbac.go
@@ -55,6 +55,33 @@ func (crb *clusterRoleBindingApplier) Handle(ctx context.Context) error {
 	return err
 }
 
+type clusterRoleBindingDeleter struct {
+	client *kubernetes.Clientset
+	crb    *rbacv1.ClusterRoleBinding
+}
+
+func (crb *clusterRoleBindingDeleter) New(kubeconfigPath string) {
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	crb.client = kubernetes.NewForConfigOrDie(rest.AddUserAgent(restConfig, "rbac-agent"))
+}
+
+func (crb *clusterRoleBindingDeleter) Read(objBytes []byte, _ RenderFunc, _ RenderParams) {
+	obj, err := runtime.Decode(rbacCodecs.UniversalDecoder(rbacv1.SchemeGroupVersion), objBytes)
+	if err != nil {
+		panic(err)
+	}
+	crb.crb = obj.(*rbacv1.ClusterRoleBinding)
+}
+
+func (crb *clusterRoleBindingDeleter) Handle(ctx context.Context) error {
+	_, _, err := resourceapply.DeleteClusterRoleBinding(ctx, crb.client.RbacV1(), assetsEventRecorder, crb.crb)
+	return err
+}
+
 type clusterRoleApplier struct {
 	client *kubernetes.Clientset
 	cr     *rbacv1.ClusterRole
@@ -109,6 +136,33 @@ func (rb *roleBindingApplier) Handle(ctx context.Context) error {
 	return err
 }
 
+type clusterRoleDeleter struct {
+	client *kubernetes.Clientset
+	cr     *rbacv1.ClusterRole
+}
+
+func (cr *clusterRoleDeleter) New(kubeconfigPath string) {
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	cr.client = kubernetes.NewForConfigOrDie(rest.AddUserAgent(restConfig, "rbac-agent"))
+}
+
+func (cr *clusterRoleDeleter) Read(objBytes []byte, _ RenderFunc, _ RenderParams) {
+	obj, err := runtime.Decode(rbacCodecs.UniversalDecoder(rbacv1.SchemeGroupVersion), objBytes)
+	if err != nil {
+		panic(err)
+	}
+	cr.cr = obj.(*rbacv1.ClusterRole)
+}
+
+func (cr *clusterRoleDeleter) Handle(ctx context.Context) error {
+	_, _, err := resourceapply.DeleteClusterRole(ctx, cr.client.RbacV1(), assetsEventRecorder, cr.cr)
+	return err
+}
+
 type roleApplier struct {
 	client *kubernetes.Clientset
 	r      *rbacv1.Role
@@ -136,19 +190,19 @@ func (r *roleApplier) Handle(ctx context.Context) error {
 	return err
 }
 
-func applyRbac(ctx context.Context, rbacs []string, handler resourceHandler) error {
+func handleRbac(ctx context.Context, rbacs []string, handler resourceHandler) error {
 	lock.Lock()
 	defer lock.Unlock()
 
 	for _, rbac := range rbacs {
-		klog.Infof("Applying rbac %s", rbac)
+		klog.Infof("Handling rbac %s", rbac)
 		objBytes, err := embedded.Asset(rbac)
 		if err != nil {
 			return fmt.Errorf("error getting asset %s: %v", rbac, err)
 		}
 		handler.Read(objBytes, nil, nil)
 		if err := handler.Handle(ctx); err != nil {
-			klog.Warningf("Failed to apply rbac %s: %v", rbac, err)
+			klog.Warningf("Failed to handle rbac %s: %v", rbac, err)
 			return err
 		}
 	}
@@ -159,22 +213,35 @@ func applyRbac(ctx context.Context, rbacs []string, handler resourceHandler) err
 func ApplyClusterRoleBindings(ctx context.Context, rbacs []string, kubeconfigPath string) error {
 	crb := &clusterRoleBindingApplier{}
 	crb.New(kubeconfigPath)
-	return applyRbac(ctx, rbacs, crb)
+	return handleRbac(ctx, rbacs, crb)
+}
+
+func DeleteClusterRoleBindings(ctx context.Context, rbacs []string, kubeconfigPath string) error {
+	crb := &clusterRoleBindingDeleter{}
+	crb.New(kubeconfigPath)
+	return handleRbac(ctx, rbacs, crb)
 }
 
 func ApplyClusterRoles(ctx context.Context, rbacs []string, kubeconfigPath string) error {
 	cr := &clusterRoleApplier{}
 	cr.New(kubeconfigPath)
-	return applyRbac(ctx, rbacs, cr)
+	return handleRbac(ctx, rbacs, cr)
 }
+
+func DeleteClusterRoles(ctx context.Context, rbacs []string, kubeconfigPath string) error {
+	cr := &clusterRoleDeleter{}
+	cr.New(kubeconfigPath)
+	return handleRbac(ctx, rbacs, cr)
+}
+
 func ApplyRoleBindings(ctx context.Context, rbacs []string, kubeconfigPath string) error {
 	rb := &roleBindingApplier{}
 	rb.New(kubeconfigPath)
-	return applyRbac(ctx, rbacs, rb)
+	return handleRbac(ctx, rbacs, rb)
 }
 
 func ApplyRoles(ctx context.Context, rbacs []string, kubeconfigPath string) error {
 	r := &roleApplier{}
 	r.New(kubeconfigPath)
-	return applyRbac(ctx, rbacs, r)
+	return handleRbac(ctx, rbacs, r)
 }
