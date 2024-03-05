3e1d18cd55678f7888476f27c4964387c985d3ed
Gregory Giguashvili <ggiguash@redhat.com>
2024-03-05 19:47:00 +0200

    Add junit integration to CNCF tests

--- test/scenarios-periodics/el92-src@cncf-conformance.sh
+++ test/scenarios-periodics/el92-src@cncf-conformance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Sourced from cleanup_scenario.sh and uses functions defined there.
+# Sourced from scenario.sh and uses functions defined there.
 
 KUBECONFIG="${SCENARIO_INFO_DIR}/${SCENARIO}/kubeconfig"
 # Timeout in seconds
@@ -14,10 +14,20 @@ prepare_hosts() {
     local -r secondary_host_ssh_port=$(cat "${SCENARIO_INFO_DIR}/${SCENARIO}/vms/host2/ssh_port")
     local -r primary_host_name="$(full_vm_name host1)"
     local -r secondary_host_name="$(full_vm_name host2)"
+    local rc=0
 
+    # Configure primary host
     scp -P "${primary_host_ssh_port}" "${ROOTDIR}/scripts/multinode/configure-pri.sh" "redhat@${primary_host_ip}":
-    ssh -p "${primary_host_ssh_port}" "redhat@${primary_host_ip}" ./configure-pri.sh "${primary_host_name}" "${primary_host_ip}" "${secondary_host_name}" "${secondary_host_ip}"
+    ssh -p "${primary_host_ssh_port}" "redhat@${primary_host_ip}" ./configure-pri.sh \
+        "${primary_host_name}" "${primary_host_ip}" \
+        "${secondary_host_name}" "${secondary_host_ip}" || rc=$?
+    if [ ${rc} -ne 0 ] ; then
+        record_junit "prepare_hosts" "configure_primary" "FAILED"
+        return ${rc}
+    fi
+    record_junit "prepare_hosts" "configure_primary" "OK"
 
+    # Configure secondary host
     scp -3 -P "${primary_host_ssh_port}" \
         "redhat@${primary_host_ip}:/home/redhat/kubelet-${secondary_host_name}.key" \
         "redhat@${primary_host_ip}:/home/redhat/kubelet-${secondary_host_name}.crt" \
@@ -25,72 +35,125 @@ prepare_hosts() {
         "redhat@${secondary_host_ip}":
 
     scp -P "${secondary_host_ssh_port}" "${ROOTDIR}/scripts/multinode/configure-sec.sh" "redhat@${secondary_host_ip}":
-    ssh -p "${secondary_host_ssh_port}" "redhat@${secondary_host_ip}" ./configure-sec.sh "${primary_host_name}" "${primary_host_ip}" "${secondary_host_name}" "${secondary_host_ip}"
+    ssh -p "${secondary_host_ssh_port}" "redhat@${secondary_host_ip}" ./configure-sec.sh \
+        "${primary_host_name}" "${primary_host_ip}" \
+        "${secondary_host_name}" "${secondary_host_ip}" || rc=$?
+    if [ ${rc} -ne 0 ] ; then
+        record_junit "prepare_hosts" "configure_secondary" "FAILED"
+        return ${rc}
+    fi
+    record_junit "prepare_hosts" "configure_secondary" "OK"
 
+    # Configure kubeconfig and host name resolution
     scp -P "${primary_host_ssh_port}" "redhat@${primary_host_ip}:/home/redhat/kubeconfig-${primary_host_name}" "${KUBECONFIG}"
     export KUBECONFIG="${KUBECONFIG}"
     echo "${primary_host_ip} ${primary_host_name}" | sudo tee -a /etc/hosts &>/dev/null
+
+    return ${rc}
 }
 
 run_sonobuoy() {
-    # Configure cluster prerequisites
-    oc adm policy add-scc-to-group privileged system:authenticated system:serviceaccounts
-    oc adm policy add-scc-to-group anyuid     system:authenticated system:serviceaccounts
+    local rc=0
+    local start
+    local now
 
+    # Configure cluster prerequisites
+    oc adm policy add-scc-to-group privileged system:authenticated system:serviceaccounts || rc=$?
+    oc adm policy add-scc-to-group anyuid     system:authenticated system:serviceaccounts || rc=$?
+    if [ ${rc} -ne 0 ] ; then
+        record_junit "run_sonobuoy" "add_scc_to_group" "FAILED"
+        return ${rc}
+    fi
+
+    # Initiate test startup
     go install github.com/vmware-tanzu/sonobuoy@v0.57.1
     ~/go/bin/sonobuoy run \
         --mode=certified-conformance \
         --dns-namespace=openshift-dns \
         --dns-pod-labels=dns.operator.openshift.io/daemonset-dns=default \
-        --e2e-parallel=y 
+        --e2e-parallel=y || rc=$?
+    if [ ${rc} -ne 0 ] ; then
+        record_junit "run_sonobuoy" "start_e2e" "FAILED"
+        return ${rc}
+    fi
+    record_junit "run_sonobuoy" "start_e2e" "OK"
 
     # Wait for up to 5m until tests start
-    WAIT_FAILURE=true
+    rc=1
     for _ in $(seq 1 150) ; do
         if [ "$(~/go/bin/sonobuoy status --json | jq -r '.status')" = "running" ]; then
-            WAIT_FAILURE=false
+            rc=0
             break
         fi
         sleep 2
     done
-    if ${WAIT_FAILURE}; then
+    if [ ${rc} -ne 0 ] ; then
         echo "Failed to start tests after 5m"
-        exit 1
+        record_junit "run_sonobuoy" "wait_e2e_running" "FAILED"
+        return ${rc}
     fi
+    record_junit "run_sonobuoy" "wait_e2e_running" "OK"
 
     # Use 1h timeout. A normal run on 2 CPUs takes 40-45min.
+    local -r stat_file="${SCENARIO_INFO_DIR}/${SCENARIO}/cncf_status.json"
     start=$(date +%s)
-    while [ "$(~/go/bin/sonobuoy status --json | jq -r '.status')" = "running" ] ; do
-        now=$(date +%s)
-        if [ $(( now - start )) -ge ${TIMEOUT_TEST} ]; then
-            echo "Tests running for 1h. Timing out"
+    while true ; do
+        ~/go/bin/sonobuoy status --json > "${stat_file}"
+        if [ "$(jq -r '.status' "${stat_file}")" != "running" ] ; then
             break
         fi
-        ~/go/bin/sonobuoy status --json | jq '.plugins[] | select(.plugin=="e2e") | .progress'
+
+        now=$(date +%s)
+        if [ $(( now - start )) -ge ${TIMEOUT_TEST} ]; then
+            rc=1
+            echo "Tests running for 1h. Timing out"
+            record_junit "run_sonobuoy" "wait_e2e_finished" "FAILED"
+            break
+        fi
+        # Print progress information
+        jq '.plugins[] | select(.plugin=="e2e") | .["result-counts"], .progress' "${stat_file}"
         sleep 60
     done
+    # If the timeout is exceeded, proceed to attempt collecting test results
+    if [ ${rc} -eq 0 ] ; then
+        record_junit "run_sonobuoy" "wait_e2e_finished" "OK"
+    fi
 
+    local results=true
     start=$(date +%s)
     # shellcheck disable=SC2046  # Jq is unable to process escaped quotes
     while [ -z $(~/go/bin/sonobuoy status --json | jq -r '."tar-info".name') ] ; do
         now=$(date +%s)
         if [ $(( now - start )) -ge ${TIMEOUT_RESULTS} ]; then
+            rc=1
+            results=false
             echo "Waited for results for 5m. Timing out"
+            record_junit "run_sonobuoy" "wait_e2e_results" "FAILED"
             break
         fi
         echo "Waiting for results availability"
         sleep 10
     done
-    RESULTS_DIR=$(mktemp -d -p /tmp)
-    ~/go/bin/sonobuoy retrieve "${RESULTS_DIR}" -f results.tar.gz
-    tar xf "${RESULTS_DIR}/results.tar.gz" -C "${RESULTS_DIR}"
-    cp "${RESULTS_DIR}/plugins/e2e/results/global/"{e2e.log,junit_01.xml} "${SCENARIO_INFO_DIR}/${SCENARIO}/"
-    rm -r "${RESULTS_DIR}"
 
-    if [ "$(~/go/bin/sonobuoy status --json | jq -r '.plugins[] | select(.plugin=="e2e") | .progress.failed')" != "null" ] ; then
-        echo "Test was finished with errors. Check junit"
-        exit 1
+    if ${results} ; then
+        record_junit "run_sonobuoy" "wait_e2e_results" "OK"
+
+        # Collect the results
+        local -r results_dir=$(mktemp -d -p /tmp)
+        ~/go/bin/sonobuoy retrieve "${results_dir}" -f results.tar.gz
+        tar xf "${results_dir}/results.tar.gz" -C "${results_dir}"
+        cp "${results_dir}/plugins/e2e/results/global/"{e2e.log,junit_01.xml} "${SCENARIO_INFO_DIR}/${SCENARIO}/"
+        rm -r "${results_dir}"
     fi
+
+    if [ ${rc} -eq 0 ] ; then
+        echo "Tests finished successfully"
+        record_junit "run_sonobuoy" "run_e2e_status" "OK"
+    else
+        echo "Tests finished with errors. Check junit"
+        record_junit "run_sonobuoy" "run_e2e_status" "FAILED"
+    fi
+    return ${rc}
 }
 
 scenario_create_vms() {
@@ -106,6 +169,10 @@ scenario_remove_vms() {
 }
 
 scenario_run_tests() {
-    prepare_hosts
-    run_sonobuoy
+    start_junit
+    trap "close_junit" EXIT
+
+    if prepare_hosts ; then
+        run_sonobuoy
+    fi
 }
