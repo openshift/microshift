df8353b79672ec9054c79fbef668fa01a2dbcfae
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-03-14 11:02:06 +0100

    USHIFT-2615: Replace registry.access.redhat.com with registry.redhat.io

--- scripts/auto-rebase/last_lvms_rebase.sh
+++ scripts/auto-rebase/last_lvms_rebase.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-./scripts/auto-rebase/rebase-lvms.sh to "registry.access.redhat.com/lvms4/lvms-operator-bundle:v4.15.0"
+./scripts/auto-rebase/rebase-lvms.sh to "registry.redhat.io/lvms4/lvms-operator-bundle:v4.15.0"
