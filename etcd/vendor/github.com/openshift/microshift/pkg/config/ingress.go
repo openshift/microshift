4492abd6d21d29177ba061e7fc709d24f7e6692a
ci-robot <ci-robot@openshift.io>
2024-03-15 05:15:28 +0000

    update etcd/vendor

--- etcd/vendor/github.com/openshift/microshift/pkg/config/ingress.go
+++ etcd/vendor/github.com/openshift/microshift/pkg/config/ingress.go
@@ -14,6 +14,16 @@ type IngressConfig struct {
 }
 
 type RouteAdmissionPolicy struct {
+	// Describes how host name claims across namespaces should be handled.
+	//
+	// Value must be one of:
+	//
+	// - Strict: Do not allow routes in different namespaces to claim the same host.
+	//
+	// - InterNamespaceAllowed: Allow routes to claim different paths of the same
+	//   host name across namespaces.
+	//
+	// If empty, the default is InterNamespaceAllowed.
 	// +kubebuilder:default="InterNamespaceAllowed"
 	NamespaceOwnership NamespaceOwnershipEnum `json:"namespaceOwnership"`
 }
