cf02d0c7c042ed11a3c76c51439cc7f14010ecb2
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2024-03-18 14:13:22 +0100

    serialize and load templating data for quicker iterations

--- test/pkg/compose/cmd.go
+++ test/pkg/compose/cmd.go
@@ -1,12 +1,15 @@
 package compose
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
 func NewComposeCmd() *cobra.Command {
+	templatingDataInput := ""
+
 	cmd := &cobra.Command{
 		Use:   "compose target",
 		Short: "",
@@ -18,13 +21,68 @@ func NewComposeCmd() *cobra.Command {
 			return fmt.Errorf("repo flag is nil")
 		}
 
-		td, err := NewTemplatingData(repoFlag.Value.String())
+		td, err := NewTemplatingData(repoFlag.Value.String(), templatingDataInput)
 		if err != nil {
 			return err
 		}
 		_ = td
 		return nil
 	}
+
+	cmd.PersistentFlags().StringVar(&templatingDataInput, "templating-data", "", "Provide path to partial templating data to skip querying remote repository.")
+
+	cmd.AddCommand(templatingDataSubCmd())
+
+	return cmd
+}
+
+func templatingDataSubCmd() *cobra.Command {
+	full := false
+
+	cmd := &cobra.Command{
+		Use:   "templating-data",
+		Short: "",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			repoFlag := cmd.Flag("repo")
+			if repoFlag == nil {
+				return fmt.Errorf("repo flag is nil")
+			}
+
+			td, err := NewTemplatingData(repoFlag.Value.String(), "")
+			if err != nil {
+				return err
+			}
+
+			// Only serialize entire templating data if requested.
+			if full {
+				b, err := json.MarshalIndent(td, "", "    ")
+				if err != nil {
+					return fmt.Errorf("failed to marshal templating data to json: %w", err)
+				}
+				fmt.Printf("%s", string(b))
+				return nil
+			}
+
+			// By default this will only include information that change less often (i.e. RHOCP and OpenShift mirror related) and take longer to obtain.
+			// Information obtained from local files is quick and can change more often.
+			reducedTD := make(map[string]interface{})
+			reducedTD["Current"] = td.Current
+			reducedTD["Previous"] = td.Previous
+			reducedTD["YMinus2"] = td.YMinus2
+			reducedTD["RHOCPMinorY"] = td.RHOCPMinorY
+			reducedTD["RHOCPMinorY1"] = td.RHOCPMinorY1
+			reducedTD["RHOCPMinorY2"] = td.RHOCPMinorY2
+			b, err := json.MarshalIndent(reducedTD, "", "    ")
+			if err != nil {
+				return fmt.Errorf("failed to marshal reduced templating data to json: %w", err)
+			}
+			fmt.Printf("%s", string(b))
+
+			return nil
+		},
+	}
+
+	cmd.Flags().BoolVar(&full, "full", false, "Obtain full templating data, including local RPM information (source, base, fake)")
 
 	return cmd
 }
