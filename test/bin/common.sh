b86f0fb74e48595a4a83f6508da5e9d5f71d734d
Gregory Giguashvili <85498834+ggiguash@users.noreply.github.com>
2024-04-02 18:13:44 +0300

    Update test/bin/common.sh
    Co-authored-by: Patryk Matuszak <pmtk@redhat.com>

--- test/bin/common.sh
+++ test/bin/common.sh
@@ -64,7 +64,7 @@ export BASE_REPO="${IMAGEDIR}/rpm-repos/microshift-base"
 # shellcheck disable=SC2034  # used elsewhere
 export CONTAINER_LIST="${IMAGEDIR}/container-images-list"
 
-# Location of container images in oci-dir formar for all the bootc images
+# Location of container images in oci-dir format for all the bootc images
 # shellcheck disable=SC2034  # used elsewhere
 export BOOTC_IMAGE_DIR="${IMAGEDIR}/bootc-images"
 
