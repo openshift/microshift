338ca7a22380358862de39219fa87e220438ef9f
Douglas Hensel <dhensel@redhat.com>
2024-04-04 13:46:52 -0400

    Add VID check

--- scripts/image-builder/configure.sh
+++ scripts/image-builder/configure.sh
@@ -23,6 +23,13 @@ fi
      createrepo yum-utils selinux-policy-devel jq wget lorax rpm-build \
      containernetworking-plugins expect"
 
+VID=$(source /etc/os-release && echo "${VERSION_ID}")
+if { [[ "${VID}" == "9.4" ]] && grep -qE 'Red Hat Enterprise Linux.*Beta' /etc/redhat-release; }; then
+    # If the system 9.4 beta: switch 9.4 repos to use beta packages
+    # Otherwise (like non-beta 9.4): keep original contents of the file
+    sudo sed -i "s,dist/rhel9/9.4,beta/rhel9/9,g" /usr/share/osbuild-composer/repositories/rhel-9.4.json
+fi
+
 sudo systemctl enable osbuild-composer.socket --now
 sudo systemctl enable cockpit.socket --now
 sudo firewall-cmd --add-service=cockpit --permanent
