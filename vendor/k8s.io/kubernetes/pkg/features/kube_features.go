bca32c3d6d7f0576637c96e845eb01d7e9ff951d
ci-robot <ci-robot@openshift.io>
2024-04-26 05:26:01 +0000

    update microshift/vendor

--- vendor/k8s.io/kubernetes/pkg/features/kube_features.go
+++ vendor/k8s.io/kubernetes/pkg/features/kube_features.go
@@ -1215,6 +1215,8 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	genericfeatures.UnauthenticatedHTTP2DOSMitigation: {Default: true, PreRelease: featuregate.Beta},
 
+	genericfeatures.WatchFromStorageWithoutResourceVersion: {Default: false, PreRelease: featuregate.Beta},
+
 	// inherited features from apiextensions-apiserver, relisted here to get a conflict if it is changed
 	// unintentionally on either side:
 
