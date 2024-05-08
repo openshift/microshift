8d6f1f2a5bd0b18336abaaec7075a761c16e2ad3
Gregory Giguashvili <ggiguash@redhat.com>
2024-05-08 08:17:47 +0300

    Refactor bootc osconfig scenarios not to exceed 20m runtime

---
+++ test/scenarios-bootc/el94-src@osconfig3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Sourced from scenario.sh and uses functions defined there.
+
+scenario_create_vms() {
+    prepare_kickstart host1 kickstart-bootc.ks.template rhel94-bootc-source
+    launch_vm host1 rhel94-bootc "" "" "" "" "" "" "1"
+}
+
+scenario_remove_vms() {
+    remove_vm host1
+}
+
+scenario_run_tests() {
+    run_tests host1 \
+        suites/osconfig/clusterid.robot \
+        suites/osconfig/systemd-resolved.robot
+}
