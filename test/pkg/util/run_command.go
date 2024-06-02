895db8f65885c00b17e73050e01115293ca5105b
Patryk Matuszak <pmatusza@redhat.com>
2024-06-02 15:23:24 +0200

    Helper func to run external commands

---
+++ test/pkg/util/run_command.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"bytes"
+	"context"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+
+	"k8s.io/klog/v2"
+)
+
+func RunCommand(c ...string) (string, string, error) {
+	return RunCommandWithContext(context.Background(), c...)
+}
+
+func RunCommandWithContext(ctx context.Context, c ...string) (string, string, error) {
+	cmd := exec.CommandContext(ctx, c[0], c[1:]...)
+
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+
+	klog.InfoS("Running command", "cmd", cmd)
+	start := time.Now()
+	err := cmd.Run()
+	dur := time.Since(start)
+
+	out := strings.Trim(outb.String(), "\n")
+	serr := errb.String()
+	klog.InfoS("Command complete", "duration", dur, "cmd", cmd, "stdout", redactOutput(out), "stderr", redactOutput(serr), "err", err)
+
+	return out, serr, err
+}
+
+// redactOutput overwrites sensitive data when logging command outputs
+func redactOutput(output string) string {
+	rx := regexp.MustCompile("gpgkeys.*")
+	return rx.ReplaceAllString(output, "gpgkeys = REDACTED")
+}
