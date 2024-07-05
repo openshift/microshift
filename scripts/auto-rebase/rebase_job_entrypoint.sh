6877e3e3ca3100163732387a5213f4e5be08fcc3
Jakob Möller <jmoller@redhat.com>
2024-07-05 13:13:55 +0200

    chore: rework rebase to update entry point and use fallback

--- scripts/auto-rebase/rebase_job_entrypoint.sh
+++ scripts/auto-rebase/rebase_job_entrypoint.sh
@@ -5,6 +5,26 @@ set -o errexit
 set -o pipefail
 set -x
 
+check_semver_suffix() {
+    local version=$1
+
+    # Check if the version is not empty
+    if [[ -z "$version" ]]; then
+        echo "false"
+        return 1
+    fi
+
+    # Check if the version string contains a numeric suffix of the form -xx
+    if [[ $version =~ -[0-9]+$ ]]; then
+        echo "true"
+        return 0
+    else
+        echo "false"
+        return 1
+    fi
+}
+
+
 echo "Environment:"
 printenv
 
@@ -41,14 +61,24 @@ ARM64_RELEASE=${pullspec_release_arm64} \
 #  For example, ocp-release at 4.13 will more often than not
 #  correspond to 4.12 LVMS, until the official 4.13 release when both
 #  components will be 4.13.
-release_lvms="v4.15.0"
+release_lvms="v4.17.0-18"
 
 # Since LVMS is not part of the release payload, it is not kept in
 # CI. Use the latest z-stream that coincides with the release
 # payload's X.Y version
 pullspec_release_lvms="registry.redhat.io/lvms4/lvms-operator-bundle:${release_lvms}"
+# A unreleased candidate doesnt exist in the official registry, so fallback to the lvms_dev namespace, which contains
+# the latest lvms release candidate replicated from CPaaS into quay
+pullspec_release_lvms_fallback="quay.io/lvms_dev/lvms4-lvms-operator-bundle:${release_lvms}"
 
-./scripts/auto-rebase/rebase-lvms.sh to "${pullspec_release_lvms}"
+# You can also pass the version as a command-line argument
+lvms_is_candidate_build=$(check_semver_suffix "$version")
+if [ "$lvms_is_candidate_build" == "true" ]; then
+    ./scripts/auto-rebase/rebase-lvms.sh to "${pullspec_release_lvms}"
+else
+    ./scripts/auto-rebase/rebase-lvms.sh to "${pullspec_release_lvms_fallback}"
+fi
+
 
 if [[ "${JOB_TYPE}" == "presubmit" ]]; then
     # Verify the assets after the rebase to make sure
