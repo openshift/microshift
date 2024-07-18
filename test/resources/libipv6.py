cd3752ce5e8443b1101ddada0915619c1bac5260
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-07-18 12:21:24 +0200

    USHIFT-3483: Add ipv6 helpers to test framework

---
+++ test/resources/libipv6.py
@@ -0,0 +1,34 @@
+import ipaddress
+import urllib
+import yaml
+import socket
+from robot.libraries.BuiltIn import BuiltIn
+
+_log = BuiltIn().log
+
+def is_ipv6(addr: str) -> bool:
+    """
+    Determine if the provided address is IPv6. If addr is a hostname then resolve it
+    to check if the resulting IP address is IPv6.
+    """
+    try:
+        parsed_addr = ipaddress.ip_network(addr)
+        return parsed_addr.version == 6
+    except ValueError:
+        pass
+    try:
+        socket.getaddrinfo(addr, None, socket.AF_INET6)
+    except socket.gaierror:
+        return False
+    return True
+
+def must_be_ipv6(addr: str) -> None:
+    BuiltIn().should_be_true(is_ipv6(addr))
+
+def add_brackets_if_ipv6(ip: str) -> str:
+    """
+    Add square brackets to the given IP if its ipv6 for later use in other tools (e.g. curl)
+    """
+    if is_ipv6(ip):
+        return f"[{ip}]"
+    return ip
