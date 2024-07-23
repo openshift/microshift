f354c79064c06fc0b4c56938e6d98dda6683a026
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-07-23 10:32:55 +0200

    USHIFT-3001: Force mirror usage in ipv6 tests

--- test/scenarios/el94-src@ipv6.sh
+++ test/scenarios/el94-src@ipv6.sh
@@ -9,6 +9,10 @@ VM_BRIDGE_IP="$(get_vm_bridge_ip "${VM_IPV6_NETWORK}")"
 WEB_SERVER_URL="http://[${VM_BRIDGE_IP}]:${WEB_SERVER_PORT}"
 # shellcheck disable=SC2034  # used elsewhere
 BOOTC_REGISTRY_URL="${VM_BRIDGE_IP}:5000"
+# Redefine registry mirror. The redhat.registry.io does not resolve to
+# ipv6 and the mirror is mandatory.
+# shellcheck disable=SC2034  # used elsewhere
+ENABLE_REGISTRY_MIRROR=true
 
 scenario_create_vms() {
     prepare_kickstart host1 kickstart.ks.template rhel-9.4-microshift-source
