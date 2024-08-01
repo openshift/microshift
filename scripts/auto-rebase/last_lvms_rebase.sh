ebf976b93beffeedcfe71ccf1c65dec49dc8d527
ehila <ehila@redhat.com>
2024-08-01 09:50:00 -0400

    update last_lvms_rebase.sh

--- scripts/auto-rebase/last_lvms_rebase.sh
+++ scripts/auto-rebase/last_lvms_rebase.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-./scripts/auto-rebase/rebase-lvms.sh to "quay.io/lvms_dev/lvms4-lvms-operator-bundle:v4.17.0-18"
+./scripts/auto-rebase/rebase-lvms.sh to "quay.io/lvms_dev/lvms4-lvms-operator-bundle:v4.17.0-27"
