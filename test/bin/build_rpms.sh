85cf5c30a5e49554185c2be95c6735ed7e1ba968
vanhalenar <thalenar@gmail.com>
2024-08-07 09:45:33 +0000

    Added back recreation of dnf cache

--- test/bin/build_rpms.sh
+++ test/bin/build_rpms.sh
@@ -109,7 +109,11 @@ create_local_repo() {
     make_repo "${LOCAL_REPO}" "${RPM_SOURCE}"
     make_repo "${NEXT_REPO}" "${NEXT_RPM_SOURCE}"
     make_repo "${BASE_REPO}" "${BASE_RPM_SOURCE}"
+    
+    # Force recreation of dnf cache after rebuilding the repositories
+    sudo dnf clean all
 }
+
 
 build_rpms
 
