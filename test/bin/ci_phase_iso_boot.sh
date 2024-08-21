cdda04193910c4a44f0a2bdd8068888cd13aa6cc
vanhalenar <thalenar@gmail.com>
2024-08-21 11:05:21 +0200

    Create empty scenario-info directory in ci_phase_iso_boot.sh

--- test/bin/ci_phase_iso_boot.sh
+++ test/bin/ci_phase_iso_boot.sh
@@ -5,6 +5,13 @@
 set -xeuo pipefail
 export PS4='+ $(date "+%T.%N") ${BASH_SOURCE#$HOME/}:$LINENO \011'
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck source=test/bin/common.sh
+source "${SCRIPTDIR}/common.sh"
+
+# create empty scenario-info directory
+mkdir "${SCENARIO_INFO_DIR}"
+
 echo "This script's functionality has been moved. The script will be removed once the necessary changes are implemented in the release repository."
 
 exit 0
