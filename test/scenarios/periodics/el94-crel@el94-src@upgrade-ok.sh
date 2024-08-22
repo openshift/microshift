e8c4d8c1883d33690e753b73e20e2c3f160a541c
Patryk Matuszak <pmatusza@redhat.com>
2024-08-22 17:29:38 +0200

    update test/

--- test/scenarios/periodics/el94-crel@el94-src@upgrade-ok.sh
+++ test/scenarios/periodics/el94-crel@el94-src@upgrade-ok.sh
@@ -2,16 +2,30 @@
 
 # Sourced from scenario.sh and uses functions defined there.
 
+start_commit=rhel-9.4-microshift-crel
+
 scenario_create_vms() {
-    prepare_kickstart host1 kickstart.ks.template rhel-9.4-microshift-crel
+    if ! does_commit_exist "${start_commit}"; then
+        echo "Commit '${start_commit}' not found in ostree repo - skipping test"
+        return 0
+    fi
+    prepare_kickstart host1 kickstart.ks.template "${start_commit}"
     launch_vm host1 rhel-9.4
 }
 
 scenario_remove_vms() {
+    if ! does_commit_exist "${start_commit}"; then
+        echo "Commit '${start_commit}' not found in ostree repo - skipping test"
+        return 0
+    fi
     remove_vm host1
 }
 
 scenario_run_tests() {
+    if ! does_commit_exist "${start_commit}"; then
+        echo "Commit '${start_commit}' not found in ostree repo - skipping test"
+        return 0
+    fi
     run_tests host1 \
               --variable "TARGET_REF:rhel-9.4-microshift-source" \
               suites/upgrade/upgrade-successful.robot
--- test/scenarios/presubmits/el94-yminus2@el94-src@upgrade-ok.sh
+++ test/scenarios/presubmits/el94-yminus2@el94-src@upgrade-ok.sh
@@ -3,8 +3,8 @@
 # Sourced from scenario.sh and uses functions defined there.
 
 scenario_create_vms() {
-    prepare_kickstart host1 kickstart.ks.template "rhel-9.2-microshift-4.${YMINUS2_MINOR_VERSION}"
-    launch_vm host1 rhel-9.2
+    prepare_kickstart host1 kickstart.ks.template "rhel-9.4-microshift-4.${YMINUS2_MINOR_VERSION}"
+    launch_vm host1 rhel-9.4
 }
 
 scenario_remove_vms() {
