6945dbe2ee6a47497b0b132f77047ab30ab1a96c
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2024-10-07 21:20:19 +0530

    USHIFT-4343: Add test scenario for flannel CNI plugin

---
+++ test/scenarios-bootc/periodics/cos9-src@flannel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Sourced from scenario.sh and uses functions defined there.
+
+scenario_create_vms() {
+    prepare_kickstart host1 kickstart-bootc.ks.template cos9-bootc-source
+    launch_vm --boot_blueprint centos9-bootc --bootc
+
+    # Open the firewall ports. Other scenarios get this behavior by
+    # embedding settings in the blueprint, but there is no blueprint
+    # for this scenario. We need do this step before running the RF
+    # suite so that suite can assume it can reach all of the same
+    # ports as for any other test.
+    configure_vm_firewall host1
+}
+
+scenario_remove_vms() {
+    remove_vm host1
+}
+
+scenario_run_tests() {
+    run_tests host1 suites/standard1/networking-smoke.robot
+}
