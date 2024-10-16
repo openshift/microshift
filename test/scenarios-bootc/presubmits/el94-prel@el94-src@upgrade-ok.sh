e539d15aa3bc4995b2ff5d04f39d5973f64d79ec
Gregory Giguashvili <ggiguash@redhat.com>
2024-10-16 09:37:56 +0300

    Add el94-prel@el94-src@upgrade-ok.sh test

---
+++ test/scenarios-bootc/presubmits/el94-prel@el94-src@upgrade-ok.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Sourced from scenario.sh and uses functions defined there.
+
+scenario_create_vms() {
+    prepare_kickstart host1 kickstart-bootc.ks.template rhel94-bootc-prel
+    launch_vm --boot_blueprint rhel94-bootc --bootc
+}
+
+scenario_remove_vms() {
+    remove_vm host1
+}
+
+scenario_run_tests() {
+    run_tests host1 \
+        --variable "TARGET_REF:rhel94-bootc-source" \
+        --variable "BOOTC_REGISTRY:${BOOTC_REGISTRY_URL}" \
+        suites/upgrade/upgrade-successful.robot
+}
