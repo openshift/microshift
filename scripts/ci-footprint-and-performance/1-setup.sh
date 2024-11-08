78a9641854c42a0c2a42d49a89035ab73d8f9e33
Patryk Matuszak <pmatusza@redhat.com>
2024-11-08 10:23:18 +0100

    Retry setup steps for footprint-and-performance

--- scripts/ci-footprint-and-performance/1-setup.sh
+++ scripts/ci-footprint-and-performance/1-setup.sh
@@ -6,7 +6,16 @@ export PS4='\n+ $(date "+%T.%N") ${BASH_SOURCE}:$LINENO \011'
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for f in "${SCRIPTDIR}"/setup/[0-9]*.sh; do
-    if ! "${f}"; then
+    success=false
+    for _ in $(seq 1 3); do
+        if "${f}"; then
+            success=true
+            break
+        fi
+        sleep 5
+    done
+
+    if ! "${success}"; then
         echo "ERROR: ${f} failed"
         exit 1
     fi
