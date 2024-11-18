3492d88c78a5de6cfd97f317e1d140ba55e13135
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-11-18 09:48:49 +0100

    USHIFT-4895: Add ipv6 bootc scenario

---
+++ test/scenarios-bootc/presubmits/el94-src@ipv6.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Sourced from scenario.sh and uses functions defined there.
+
+# Redefine network-related settings to use the dedicated IPv6 network bridge
+# shellcheck disable=SC2034  # used elsewhere
+VM_BRIDGE_IP="$(get_vm_bridge_ip "${VM_IPV6_NETWORK}")"
+# shellcheck disable=SC2034  # used elsewhere
+WEB_SERVER_URL="http://[${VM_BRIDGE_IP}]:${WEB_SERVER_PORT}"
+# shellcheck disable=SC2034  # used elsewhere
+MIRROR_REGISTRY_URL="$(hostname):${MIRROR_REGISTRY_PORT}"
+
+scenario_create_vms() {
+    prepare_kickstart host1 kickstart-bootc.ks.template rhel94-bootc-source
+    launch_vm --boot_blueprint rhel94-bootc --network "${VM_IPV6_NETWORK}" --bootc
+}
+
+scenario_remove_vms() {
+    remove_vm host1
+}
+
+scenario_run_tests() {
+    run_tests host1 suites/ipv6/singlestack.robot
+}
