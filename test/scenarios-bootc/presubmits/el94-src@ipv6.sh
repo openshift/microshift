ef6e02aeb15439be48424573b23809f4cbd91659
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-11-20 17:30:58 +0100

    USHIFT-4895: Add comments to scenario about skopeo

--- test/scenarios-bootc/presubmits/el94-src@ipv6.sh
+++ test/scenarios-bootc/presubmits/el94-src@ipv6.sh
@@ -7,6 +7,10 @@
 VM_BRIDGE_IP="$(get_vm_bridge_ip "${VM_IPV6_NETWORK}")"
 # shellcheck disable=SC2034  # used elsewhere
 WEB_SERVER_URL="http://[${VM_BRIDGE_IP}]:${WEB_SERVER_PORT}"
+# Using `hostname` here instead of a raw ip because skopeo only allows either
+# ipv4 or fqdn's, but not ipv6. Since the registry is hosted on the ipv6
+# network gateway in the host, we need to use a combination of the hostname
+# plus /etc/hosts resolution (which is taken care of by kickstart).
 # shellcheck disable=SC2034  # used elsewhere
 MIRROR_REGISTRY_URL="$(hostname):${MIRROR_REGISTRY_PORT}"
 
