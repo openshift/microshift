ab0a5fdb439add9028b6282e5d0c7e9a5c91be05
Patryk Matuszak <pmatusza@redhat.com>
2024-11-29 15:05:52 +0100

    Drop timeout calculation & greenboot integration
    Use --timeout option instead

--- pkg/cmd/healthcheck.go
+++ pkg/cmd/healthcheck.go
@@ -2,20 +2,25 @@ package cmd
 
 import (
 	"context"
+	"time"
 
 	"github.com/openshift/microshift/pkg/healthcheck"
 	"github.com/spf13/cobra"
 )
 
 func NewHealthcheckCommand() *cobra.Command {
+	var timeout time.Duration
+
 	cmd := &cobra.Command{
 		Use:   "healthcheck",
 		Short: "Verify health of the MicroShift",
 
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return healthcheck.MicroShiftHealthcheck(context.Background())
+			return healthcheck.MicroShiftHealthcheck(context.Background(), timeout)
 		},
 	}
+
+	cmd.Flags().DurationVar(&timeout, "timeout", 300*time.Second, "The maximum duration of each stage of the healthcheck.")
 
 	return cmd
 }
--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -2,11 +2,10 @@ package healthcheck
 
 import (
 	"context"
+	"time"
 )
 
-func MicroShiftHealthcheck(ctx context.Context) error {
-	timeout := getGreenbootTimeoutDuration()
-
+func MicroShiftHealthcheck(ctx context.Context, timeout time.Duration) error {
 	if enabled, err := microshiftServiceShouldBeOk(ctx, timeout); err != nil {
 		return err
 	} else if !enabled {
