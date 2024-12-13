be732ff60881d1952c9a8a23764c5b88f72a9d2d
Patryk Matuszak <pmatusza@redhat.com>
2024-12-13 10:42:55 +0100

    Move AllErrGroup to separate file

--- pkg/healthcheck/workloads.go
+++ pkg/healthcheck/workloads.go
@@ -1,12 +1,10 @@
 package healthcheck
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/openshift/microshift/pkg/config"
@@ -220,33 +218,4 @@ func logPodsAndEvents() {
 	}
 
 	klog.Infof("DEBUG INFORMATION\n%s", output.String())
-}
-
-// AllErrGroup is a helper to wait for all goroutines and get all errors that occurred.
-// It's based on sync.WaitGroup (which doesn't capture any errors) and errgroup.Group (which only captures the first error).
-type AllErrGroup struct {
-	wg   sync.WaitGroup
-	mu   sync.Mutex
-	errs []error
-
-	amount int
-}
-
-func (g *AllErrGroup) Go(f func() error) {
-	g.wg.Add(1)
-	g.amount += 1
-	go func() {
-		defer g.wg.Done()
-		if err := f(); err != nil {
-			g.mu.Lock()
-			defer g.mu.Unlock()
-			g.errs = append(g.errs, err)
-		}
-	}()
-}
-
-func (g *AllErrGroup) Wait() error {
-	klog.V(2).Infof("Waiting for %d goroutines", g.amount)
-	g.wg.Wait()
-	return errors.Join(g.errs...)
 }
