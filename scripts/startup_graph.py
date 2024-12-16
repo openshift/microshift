d39d5a15f5f7e4e7707ed9a47f7da1685bc060b9
vanhalenar <thalenar@gmail.com>
2024-12-16 14:14:55 +0100

    display each service deps with button

--- scripts/startup_graph.py
+++ scripts/startup_graph.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import plotly.express as px
 import datetime
+import numpy as np
 
 def format_time_ns(nanoseconds):
     """
@@ -54,29 +55,248 @@ fig.update_yaxes(autorange="reversed")
 fig.update_layout(coloraxis_showscale=False)
 
 
+def dep_line(x0, y0, x1, y1, x_offset=0, y_offset=0):
+    
+    x0_1 = x0 + x_offset
+    x1_1 = x1 - x_offset
+
+    return [dict(
+        type="line",
+        x0=x0,
+        x1=x0_1,
+        y0=y0,  
+        y1=y0,
+        line=dict(color="Red", width=2),
+        xref="x",
+        yref="y" 
+    ), 
+    dict(
+        type="line",
+        x0=x0_1,
+        x1=x1_1,
+        y0=y0,  
+        y1=y1,
+        y1shift=y_offset,
+        line=dict(color="Red", width=2),
+        xref="x",
+        yref="y"
+    ),
+    dict(
+        type="line",
+        x0=x1_1,
+        x1=x1,
+        y0=y1,  
+        y1=y1,
+        y0shift=y_offset,
+        y1shift=y_offset,
+        line=dict(color="Red", width=2),
+        xref="x",
+        yref="y" 
+    )]
+
 # Convert numerical x-coordinates to datetime
-def draw_dep(fig, serv1, serv2):
+def draw_dep(fig, serv1, serv2, y_offset=0):
     num1 = df.loc[df["name"] == serv1].index[0]
     num2 = df.loc[df["name"] == serv2].index[0]
 
+    x_offset = datetime.timedelta(milliseconds=200)
+
+    y_offset = y_offset
 
     x0 = fig.data[0].base[num1] + datetime.timedelta(milliseconds=(fig.data[0].x[num1]))
-    x1 = fig.data[0].base[num2] #+ datetime.timedelta(seconds=(fig.data[0].x[6]/1000000000))
+    y0 = fig.data[0].y[num1]
 
-    # Add line shape using the corrected datetime values
-    fig.add_shape(
-        type="line",
-        x0=x0,
-        x1=x1,
-        y0=fig.data[0].y[num1],  # Assuming y is correct
-        y1=fig.data[0].y[num2],
-        line=dict(color="Red", width=2),
-        xref="x",
-        yref="y"  # Match the axis types
-    )
+    x1 = fig.data[0].base[num2]
+    y1 = fig.data[0].y[num2]
+
+    shapes = dep_line(x0=x0, y0=y0, x1=x1, y1=y1, x_offset=x_offset, y_offset=y_offset)
+
+    return shapes
+
+    #fig.update_layout(
+    #    shapes=shapes
+    #)
+
+    # x0_1 = x0 + x_offset
+    # x1_1 = x1 - x_offset
+
+    # #Add line shape using the corrected datetime values
+    # fig.add_shape(
+    #     type="line",
+    #     x0=x0,
+    #     x1=x0_1,
+    #     y0=y0,  
+    #     y1=y0,
+    #     line=dict(color="Red", width=2),
+    #     xref="x",
+    #     yref="y"  
+    # )
+
+    # fig.add_shape(
+    #     type="line",
+    #     x0=x0_1,
+    #     x1=x1_1,
+    #     y0=y0,  
+    #     y1=y1,
+    #     y1shift=y_offset,
+    #     line=dict(color="Red", width=2),
+    #     xref="x",
+    #     yref="y"  
+    # )
+
+    # fig.add_shape(
+    #     type="line",
+    #     x0=x1_1,
+    #     x1=x1,
+    #     y0=y1,  
+    #     y1=y1,
+    #     y0shift=y_offset,
+    #     y1shift=y_offset,
+    #     line=dict(color="Red", width=2),
+    #     xref="x",
+    #     yref="y"  
+    # )
+    
+    # x_mid = x0 + (x1-x0)/2
+    # print(type(x_mid))
+    #y_mid = y1 - y0
+    
+    #path = f"""
+    #M {x0},{y0} C {x_mid},{y0} {x_mid},{y1} {x1},{y1}
+    #"""
+
+    # path = f""" M {x0},{y0} L {x1},{y1}"""
+
+    # print(path)
+    
+    # fig.add_shape(
+    #     type="path",
+    #     path=path,
+    #     line=dict(color="Red", width=2),
+    #     #xref="x",
+    #     #yref="y"  
+    # )
+
+    # fig.add_shape(
+    #     type="path",
+    #     path="M 2,2 L 5,5",
+    #     line=dict(color="Red", width=2),
+    #     #xref="x",
+    #     #yref="y"  
+    # )
+
+    # fig.add_shape(
+    #     type="line",
+    #     x0=x0,
+    #     x1=x_mid,
+    #     y0=y0,  
+    #     y1=y0,
+    #     line=dict(color="Red", width=2),
+    #     xref="x",
+    #     yref="y"  
+    # )
+
+    # fig.add_shape(
+    #     type="line",
+    #     x0=x_mid,
+    #     x1=x_mid,
+    #     y0=y0,  
+    #     y1=y1,
+    #     line=dict(color="Red", width=2),
+    #     xref="x",
+    #     yref="y"  
+    # )
+
+    # fig.add_shape(
+    #     type="line",
+    #     x0=x_mid,
+    #     x1=x1,
+    #     y0=y1,  
+    #     y1=y1,
+    #     line=dict(color="Red", width=2),
+    #     xref="x",
+    #     yref="y"  
+    # )
+
+
+
+    #fig.add_annotation(
+    #    x=x1,
+    #    y=y1,
+    #    ax=x0,
+    #    ay=y0,
+    #    xref="x",
+    #    yref="y",
+    #    axref="x",
+    #    ayref="y",
+    #    showarrow=True,
+    #    arrowhead=2,
+    #    arrowsize=1,
+    #    arrowwidth=2,
+    #    arrowcolor="Red",
+    #    visible=True
+    #)
     return fig
 
-fig = draw_dep(fig, "etcd", "kube-apiserver")
+#fig = draw_dep(fig, "kube-apiserver", "infrastructure-services-manager")
 
+shapes = []
+
+for service in services:
+    for dependency in service["dependencies"]:
+        shapes.append(draw_dep(fig, dependency, service["name"]))
+
+new_shapes = []
+
+for shape in shapes:
+    for line in shape:
+        new_shapes.append(line)
+
+def make_buttons():
+    buttons = []
+    for service in services:
+        deps = []
+        num_deps = len(service["dependencies"])
+        print(num_deps)
+        if (num_deps > 1):
+            y_offsets=np.linspace(-0.25, 0.25, num_deps)
+        else:
+            y_offsets=np.zeros(num_deps)
+        for i, dependency in enumerate(reversed(service["dependencies"])):
+            deps.extend(draw_dep(fig, dependency, service["name"], y_offsets[i]))
+        buttons.append(dict(
+            label=service["name"],
+            method="relayout",
+            args=["shapes", deps],
+        ))
+
+    return buttons
+
+buttons = make_buttons()
+buttons.insert(0, dict(
+    label="None",
+    method="relayout",
+    args=["shapes", []]
+))
+
+fig.update_layout(
+    xaxis=dict(type="date", range=[pd.to_datetime(microshift_start), pd.to_datetime(microshift_ready)]),
+    updatemenus=[
+        dict(
+            type="buttons",
+            buttons=buttons,
+                # dict(
+                #     label="None",
+                #     method="relayout",
+                #     args=["shapes", []]
+                # ),
+                # dict(
+                #     label="All",
+                #     method="relayout",
+                #     args=["shapes", new_shapes]
+                # )
+        )
+    ]
+)
 
 fig.show()
