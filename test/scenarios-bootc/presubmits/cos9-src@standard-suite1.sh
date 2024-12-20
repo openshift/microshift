c1bd572b1ba33f90437e06a52c4021d43783ee5a
Gregory Giguashvili <ggiguash@redhat.com>
2024-12-20 12:14:16 +0200

    Use offline images for standard suites to speed up service startup

--- test/scenarios-bootc/presubmits/cos9-src@standard-suite1.sh
+++ test/scenarios-bootc/presubmits/cos9-src@standard-suite1.sh
@@ -3,7 +3,9 @@
 # Sourced from scenario.sh and uses functions defined there.
 
 scenario_create_vms() {
-    prepare_kickstart host1 kickstart-bootc.ks.template cos9-bootc-source
+    # Use a layer with preloaded container images to speed up MicroShift startup
+    # for this long running suite
+    prepare_kickstart host1 kickstart-bootc.ks.template cos9-bootc-source-isolated
     launch_vm --boot_blueprint centos9-bootc
 }
 
--- test/scenarios-bootc/presubmits/el95-src@standard-suite1.sh
+++ test/scenarios-bootc/presubmits/el95-src@standard-suite1.sh
@@ -3,7 +3,9 @@
 # Sourced from scenario.sh and uses functions defined there.
 
 scenario_create_vms() {
-    prepare_kickstart host1 kickstart-bootc.ks.template rhel95-bootc-source
+    # Use a layer with preloaded container images to speed up MicroShift startup
+    # for this long running suite
+    prepare_kickstart host1 kickstart-bootc.ks.template rhel95-bootc-source-isolated
     launch_vm --boot_blueprint rhel95-bootc
 }
 
