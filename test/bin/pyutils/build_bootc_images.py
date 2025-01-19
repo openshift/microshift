35770788aa2a460ac0836f3b7f0af41a50ff6712
Gregory Giguashvili <ggiguash@redhat.com>
2025-01-19 09:52:14 +0200

    Add authfile argument to skopeo commands

--- test/bin/pyutils/build_bootc_images.py
+++ test/bin/pyutils/build_bootc_images.py
@@ -383,6 +383,7 @@ def process_container_encapsulate(groupdir, containerfile, dry_run):
         try:
             dst_ref_cmd = [
                 "skopeo", "inspect",
+                "--authfile", PULL_SECRET,
                 f"docker://{ce_targetimg}",
                 "2>/dev/null", "|",
                 "jq", "-r", "'.Labels[\"ostree.commit\"]'"
@@ -428,6 +429,7 @@ def process_container_encapsulate(groupdir, containerfile, dry_run):
             # necessary for subsequent builds that depend on this container image
             copy_args = [
                 "sudo", "skopeo", "copy",
+                "--authfile", PULL_SECRET,
                 f"docker://{ce_targetimg}",
                 f"containers-storage:{ce_localimg}"
             ]
