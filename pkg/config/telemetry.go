956ef2ae1c7e607f7cdada9345b66aec16ea16c4
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-01-29 11:58:20 +0100

    USHIFT-5401: Add telemetry configuration parameters

---
+++ pkg/config/telemetry.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+)
+
+const (
+	StatusEnabled   TelemetryStatusEnum = "Enabled"
+	StatusDisabled  TelemetryStatusEnum = "Disabled"
+	defaultEndpoint                     = "https://infogw.api.openshift.com"
+)
+
+type TelemetryStatusEnum string
+
+type Telemetry struct {
+	// Telemetry status, which can be Enabled or Disabled. Defaults to Enabled.
+	// +kubebuilder:default=Enabled
+	Status TelemetryStatusEnum `json:"status"`
+
+	// Endpoint where to send telemetry data.
+	Endpoint string `json:"endpoint"`
+}
+
+func telemetryDefaults() Telemetry {
+	return Telemetry{
+		Status:   StatusEnabled,
+		Endpoint: defaultEndpoint,
+	}
+}
+
+func (t *Telemetry) validate() error {
+	if t.Status != StatusEnabled && t.Status != StatusDisabled {
+		return fmt.Errorf("invalid telemetry status: %s", t.Status)
+	}
+	return nil
+}
