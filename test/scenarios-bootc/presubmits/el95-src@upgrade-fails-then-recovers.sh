f3f8851ced0d36db46a9ef4bac5200431170cc2c
Gregory Giguashvili <ggiguash@redhat.com>
2025-02-25 18:15:04 +0200

    Shorten fail-and-recover test names

--- test/scenarios-bootc/presubmits/el95-src@upgrade-fails-then-recovers.sh
+++ test/scenarios-bootc/presubmits/el95-src@upgrade-fails-then-recovers.sh
@@ -15,5 +15,5 @@ scenario_run_tests() {
     run_tests host1 \
         --variable "TARGET_REF:rhel95-bootc-source-fake-next-minor" \
         --variable "BOOTC_REGISTRY:${MIRROR_REGISTRY_URL}" \
-        suites/upgrade/upgrade-fails-on-1st-boot-but-recovers-on-2nd.robot
+        suites/upgrade/upgrade-fails-then-recovers.robot
 }
