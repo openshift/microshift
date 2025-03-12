0ef026c72cb55d8fa1ef8f5cdcea9e1a54998390
Gregory Giguashvili <ggiguash@redhat.com>
2025-03-12 09:55:58 +0200

    Fix expected OS version in the standard suite

--- test/scenarios-bootc/presubmits/el96-src@standard-suite1.sh
+++ test/scenarios-bootc/presubmits/el96-src@standard-suite1.sh
@@ -13,7 +13,7 @@ scenario_remove_vms() {
 
 scenario_run_tests() {
     run_tests host1 \
-        --variable "EXPECTED_OS_VERSION:9.5" \
+        --variable "EXPECTED_OS_VERSION:9.6" \
         suites/standard1/
     # When SELinux is working on RHEL 9.6 bootc systems add following suite:
     # suites/selinux/validate-selinux-policy.robot
