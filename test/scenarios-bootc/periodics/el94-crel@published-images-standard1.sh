28205204d854b1e6cf1b594c291e176064ffb39a
Gregory Giguashvili <ggiguash@redhat.com>
2025-03-14 08:08:32 +0200

    Enable SELinux tests in standard suite for bootc images

--- test/scenarios-bootc/periodics/el94-crel@published-images-standard1.sh
+++ test/scenarios-bootc/periodics/el94-crel@published-images-standard1.sh
@@ -58,7 +58,5 @@ scenario_run_tests() {
     run_tests host1 \
         --variable "EXPECTED_OS_VERSION:9.4" \
         --variable "IMAGE_SIGSTORE_ENABLED:True" \
-        suites/standard1/
-    # When SELinux is working on RHEL 9.6 bootc systems add following suite:
-    # suites/selinux/validate-selinux-policy.robot
+        suites/standard1/ suites/selinux/validate-selinux-policy.robot
 }
