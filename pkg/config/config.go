ebb67c65675d324121ceb3330650a6d016cef9da
Patryk Matuszak <pmatusza@redhat.com>
2025-03-18 15:41:00 +0100

    Update MicroShift configuration with .network.multusStatus

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -215,6 +215,10 @@ func (c *Config) incorporateUserSettings(u *Config) {
 		c.Network.DNS = u.Network.DNS
 	}
 
+	if u.Network.MultusStatus != "" {
+		c.Network.MultusStatus = u.Network.MultusStatus
+	}
+
 	if u.Etcd.MemoryLimitMB != 0 {
 		c.Etcd.MemoryLimitMB = u.Etcd.MemoryLimitMB
 	}
