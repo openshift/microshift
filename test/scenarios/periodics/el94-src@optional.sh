66b1a34728a18281761e2f3d737b0d77ea49b857
Jon Cope <jcope@redhat.com>
2025-03-19 17:12:35 -0500

    attempting to set the configuration of the the opentelemetry-collector.yaml during scenario creation
    Signed-off-by: Jon Cope <jcope@redhat.com>

--- test/scenarios/periodics/el94-src@optional.sh
+++ test/scenarios/periodics/el94-src@optional.sh
@@ -11,6 +11,16 @@ scenario_create_vms() {
     prepare_kickstart host1 kickstart.ks.template rhel-9.4-microshift-source-optionals
     # Two nics - one for macvlan, another for ipvlan (they cannot enslave the same interface)
     launch_vm  --network "${VM_MULTUS_NETWORK},${VM_MULTUS_NETWORK}"
+    local tempfile=$(mktemp)
+
+    copy_file_from_vm "host1" /etc/microshift/opentelemetry-collector.yaml "$tempfile"
+    # add the debug exporter and set all pipelines to use only the debug exporter. This is necessary, otherwise
+    # the opentelemetry-collector will fill logs with errors about being unable to reach the backend endpoint. Handling
+    # the configuration here should keep the microshift-observability.service stable throughout the test.
+    yq -i '.service.pipelines |= with_entries(.value.exporters |= [ "debug" ]) | .exporters["debug"] = {}' "$tempfile"
+    copy_file_to_vm host1 "$tempfile" /etc/microshift/opentelemetry-collector.yaml
+    run_command_on_vm host1 "sudo systemctl restart microshift-observability.service"
+    run_command_on_vm host1 "sudo systemctl is-active microshift-observability.service"
 }
 
 scenario_remove_vms() {
