d560db21b40a5037d46529eaa5f4560e8f446050
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-04-08 09:35:40 +0200

    USHIFT-5561: Add bootc periodic scenario

---
+++ test/scenarios-bootc/periodics/el96-src@telemetry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Sourced from scenario.sh and uses functions defined there.
+
+scenario_create_vms() {
+    prepare_kickstart host1 kickstart-bootc.ks.template rhel96-bootc-source
+    launch_vm --boot_blueprint rhel96-bootc
+}
+
+scenario_remove_vms() {
+    remove_vm host1
+}
+
+scenario_run_tests() {
+    ${SCRIPTDIR}/manage_prometheus.sh start
+    run_tests host1 \
+        --variable "PROXY_HOST:$(hostname)" \
+        --variable "PROXY_PORT:9001" \
+        --variable "PROMETHEUS_HOST:$(hostname)" \
+        --variable "PROMETHEUS_PORT:9090" \
+        suites/telemetry/telemetry.robot
+    ${SCRIPTDIR}/manage_prometheus.sh stop
+}
