2b0239e7620dccbfe9ecfe63c8c21768b374e394
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-04-10 16:38:13 +0200

    USHIFT-5561: Final tweaks to work in aws

--- test/scenarios-bootc/periodics/el96-src@telemetry.sh
+++ test/scenarios-bootc/periodics/el96-src@telemetry.sh
@@ -2,6 +2,8 @@
 
 # Sourced from scenario.sh and uses functions defined there.
 
+VM_BRIDGE_IP="$(get_vm_bridge_ip default)"
+
 scenario_create_vms() {
     prepare_kickstart host1 kickstart-bootc.ks.template rhel96-bootc-source
     launch_vm --boot_blueprint rhel96-bootc
@@ -13,9 +15,9 @@ scenario_remove_vms() {
 
 scenario_run_tests() {
     run_tests host1 \
-        --variable "PROXY_HOST:$(hostname)" \
+        --variable "PROXY_HOST:${VM_BRIDGE_IP}" \
         --variable "PROXY_PORT:9001" \
         --variable "PROMETHEUS_HOST:$(hostname)" \
-        --variable "PROMETHEUS_PORT:9091" \
+        --variable "PROMETHEUS_PORT:9092" \
         suites/telemetry/telemetry.robot
 }
