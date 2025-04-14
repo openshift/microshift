5aee1acc0f37d34b5fd36e0fe6093cec74dc5d00
Gregory Giguashvili <ggiguash@redhat.com>
2025-04-14 08:29:23 +0300

    Work around OS version check in standard suite

--- test/scenarios-bootc/presubmits/el96-src@standard-suite1.sh
+++ test/scenarios-bootc/presubmits/el96-src@standard-suite1.sh
@@ -11,8 +11,13 @@ scenario_remove_vms() {
     remove_vm host1
 }
 
+# Note:
+# This is a workaround for the problem described in https://issues.redhat.com/browse/USHIFT-5584.
+# Revert to the rhel-9.6 base image when it is in GA.
+#
+#       --variable "EXPECTED_OS_VERSION:9.6" \
 scenario_run_tests() {
     run_tests host1 \
-        --variable "EXPECTED_OS_VERSION:9.6" \
+        --variable "EXPECTED_OS_VERSION:9" \
         suites/standard1/ suites/selinux/validate-selinux-policy.robot
 }
