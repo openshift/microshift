83bb5852560e1045d8a3b255bc2af5a87aeee1b7
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-04-30 08:35:28 +0200

    update etcd/vendor

--- etcd/vendor/go.etcd.io/etcd/server/v3/etcdserver/api/membership/cluster.go
+++ etcd/vendor/go.etcd.io/etcd/server/v3/etcdserver/api/membership/cluster.go
@@ -697,6 +697,10 @@ func (c *RaftCluster) IsReadyToPromoteMember(id uint64) bool {
 	return true
 }
 
+func (c *RaftCluster) MembersFromStore() (map[types.ID]*Member, map[types.ID]bool) {
+	return membersFromStore(c.lg, c.v2store)
+}
+
 func membersFromStore(lg *zap.Logger, st v2store.Store) (map[types.ID]*Member, map[types.ID]bool) {
 	members := make(map[types.ID]*Member)
 	removed := make(map[types.ID]bool)
@@ -731,6 +735,10 @@ func membersFromStore(lg *zap.Logger, st v2store.Store) (map[types.ID]*Member, m
 		removed[MustParseMemberIDFromKey(lg, n.Key)] = true
 	}
 	return members, removed
+}
+
+func (c *RaftCluster) MembersFromBackend() (map[types.ID]*Member, map[types.ID]bool) {
+	return membersFromBackend(c.lg, c.be)
 }
 
 func membersFromBackend(lg *zap.Logger, be backend.Backend) (map[types.ID]*Member, map[types.ID]bool) {
@@ -904,6 +912,7 @@ func (c *RaftCluster) IsMemberExist(id types.ID) bool {
 	c.Lock()
 	defer c.Unlock()
 	_, ok := c.members[id]
+	// gofail: var afterIsMemberExist struct{}
 	return ok
 }
 
