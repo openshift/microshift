12bcc64b34f9f31a4c9b8ccb764325699f39ac4b
ci-robot <ci-robot@openshift.io>
2025-05-01 04:11:49 +0000

    Update AI Model Serving for MicroShift

--- scripts/auto-rebase/last_rebase_ai_model_serving.sh
+++ scripts/auto-rebase/last_rebase_ai_model_serving.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-./scripts/auto-rebase/rebase_ai_model_serving.sh to "registry.redhat.io/rhoai/odh-operator-bundle:v2.19"
+./scripts/auto-rebase/rebase_ai_model_serving.sh to "registry.redhat.io/rhoai/odh-operator-bundle:v2.18"
