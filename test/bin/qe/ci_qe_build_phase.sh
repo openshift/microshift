8a4797c0eb607fc7794b60f908daccfd2c9aea39
Alejandro Gullón <agullon@redhat.com>
2025-05-14 12:28:11 +0200

    test

--- test/bin/qe/ci_qe_build_phase.sh
+++ test/bin/qe/ci_qe_build_phase.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-set -eux
+set -euo pipefail
 
 # --- Helper function for logging ---
 log() {
@@ -21,8 +21,12 @@ ssh_build_vm() {
 MICROSHIFT_REPO_OWNER=${1:-openshift}
 MICROSHIFT_REPO_BRANCH="${2:-main}"
 RPM_ORIGIN="${3:-all}"
+MICROSHIFT_BREW_VERSION="${4:-""}" # By default it will fetch last brew available from this version
 
-CONFIG_VM=false
+CONFIG_VM_FLAG=false
+BUILD_RPMS_FLAG=true
+BUILD_BOOTC_IMAGES_FLAG=false
+
 VM_NAME="microshift-dev"
 REPO_NAME="microshift-${MICROSHIFT_REPO_BRANCH}-repo"
 
@@ -45,7 +49,7 @@ fi
 
 # Configuring VM
 VM_IPADDR=$("${HOME}/${REPO_NAME}/scripts/devenv-builder/manage-vm.sh" ip -n "${VM_NAME}")
-if ${CONFIG_VM}; then
+if ${CONFIG_VM_FLAG}; then
     log "VM '${VM_NAME}' local IP address: ${VM_IPADDR}"
     ssh-copy-id -f "microshift@${VM_IPADDR}"
     scp "${HOME}/${REPO_NAME}/scripts/devenv-builder/configure-vm.sh" "microshift@${VM_IPADDR}:~/configure-vm.sh"
@@ -53,7 +57,7 @@ if ${CONFIG_VM}; then
     ssh_build_vm "${HOME}/configure-vm.sh --no-build ${HOME}/.pull-secret.json"
 fi
 
-# Build rpms
+# Git clone/fetch microshift repo into the build VM
 ssh_build_vm "sudo rm -rf ${HOME}/.cache ${HOME}/.local"
 if ( ! ssh_build_vm "test -d ${HOME}/${REPO_NAME}" ); then
     log "Cloning MicroShift repository..."
@@ -65,12 +69,28 @@ else
     ssh_build_vm git -C "${HOME}/${REPO_NAME}" pull
 fi
 
-if ( ! ssh_build_vm "test -d ${HOME}/${REPO_NAME}/_output/test-images/rpm-repos/" ); then
-    ssh_build_vm "bash -x ${HOME}/${REPO_NAME}/test/bin/build_rpms.sh ${RPM_ORIGIN}"
+# Build rpms
+# if ( ! ssh_build_vm "test -d ${HOME}/${REPO_NAME}/_output/test-images/rpm-repos/" ); then
+if ${BUILD_RPMS_FLAG}; then
+    ssh_build_vm "bash -x ${HOME}/${REPO_NAME}/test/bin/build_rpms.sh ${RPM_ORIGIN} ${MICROSHIFT_BREW_VERSION}"
     log "RPM packages available: ${HOME}/${REPO_NAME}/_output/test-images/rpm-repos/"
 else
     log "RPM packages available: ${HOME}/${REPO_NAME}/_output/test-images/rpm-repos/"
     ssh_build_vm "ls -lrth ${HOME}/${REPO_NAME}/_output/test-images/rpm-repos/"
+fi
+
+# Build bootc images
+if ${BUILD_BOOTC_IMAGES_FLAG}; then
+    ssh_build_vm "make -C "${HOME}/${REPO_NAME}" verify-containers"
+
+    ssh_build_vm "bash -x ${HOME}/${REPO_NAME}/bin/build_bootc_images.sh -l ./image-blueprints-bootc/layer1-base"
+    # ssh_build_vm "bash -x ${HOME}/${REPO_NAME}/bin/build_bootc_images.sh -l ./image-blueprints-bootc/layer2-presubmit"
+    # ssh_build_vm "bash -x ${HOME}/${REPO_NAME}/bin/build_bootc_images.sh -l ./image-blueprints-bootc/layer3-periodic"
+
+    log "BootC images available: ${HOME}/${REPO_NAME}/_output/test-images/______"
+else
+    log "BootC images available: ${HOME}/${REPO_NAME}/_output/test-images/______"
+    ssh_build_vm "ls -lrth ${HOME}/${REPO_NAME}/_output/test-images/______"
 fi
 
 # Copy RPM packages into QE microshift build versions
@@ -78,5 +98,3 @@ MICROSHIFT_VERSION=$(ssh_build_vm ls "${HOME}/${REPO_NAME}/_output/test-images/r
 log "MICROSHIFT_VERSION=${MICROSHIFT_VERSION}"
 mkdir -p "${HOME}/libvirt/images/microshift_builds/${MICROSHIFT_VERSION}/"
 scp -qr "microshift@${VM_IPADDR}:${HOME}/${REPO_NAME}/_output/test-images/rpm-repos/" "${HOME}/libvirt/images/microshift_builds/${MICROSHIFT_VERSION}/rpm-repos/"
-
-
