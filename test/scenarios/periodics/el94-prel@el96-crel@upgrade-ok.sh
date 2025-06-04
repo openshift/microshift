5d8f13157412af6dc6739ae2e61b8df73728ace8
Patryk Matuszak <pmatusza@redhat.com>
2025-06-04 11:11:42 +0200

    Bump ostree tests to RHEL 9.6

---
+++ test/scenarios/periodics/el94-prel@el96-crel@upgrade-ok.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Sourced from scenario.sh and uses functions defined there.
+
+target_commit=rhel-9.6-microshift-crel
+
+scenario_create_vms() {
+    if ! does_commit_exist "${target_commit}"; then
+        echo "Commit '${target_commit}' not found in ostree repo - skipping test"
+        return 0
+    fi
+
+    prepare_kickstart host1 kickstart.ks.template "rhel-9.4-microshift-4.${PREVIOUS_MINOR_VERSION}"
+    launch_vm
+}
+
+scenario_remove_vms() {
+    if ! does_commit_exist "${target_commit}"; then
+        echo "Commit '${target_commit}' not found in ostree repo - skipping test"
+        return 0
+    fi
+    remove_vm host1
+}
+
+scenario_run_tests() {
+    if ! does_commit_exist "${target_commit}"; then
+        echo "Commit '${target_commit}' not found in ostree repo - skipping test"
+        return 0
+    fi
+    run_tests host1 \
+              --variable "TARGET_REF:${target_commit}" \
+              suites/upgrade/upgrade-successful.robot
+}
---
+++ test/scenarios/presubmits/el96-src@standard-suite1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Sourced from scenario.sh and uses functions defined there.
+
+scenario_create_vms() {
+    prepare_kickstart host1 kickstart.ks.template rhel-9.6-microshift-source
+    launch_vm
+}
+
+scenario_remove_vms() {
+    remove_vm host1
+}
+
+scenario_run_tests() {
+    run_tests host1 \
+        --variable "EXPECTED_OS_VERSION:9.6" \
+        suites/standard1/ suites/selinux/validate-selinux-policy.robot
+}
