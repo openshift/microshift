a52a0b9dad1c12a122fb20a81bef751246aa7e78
Patryk Matuszak <pmatusza@redhat.com>
2025-06-06 08:17:39 +0200

    Fix initial ref for upgrade-ostree2bootc test

--- test/scenarios-bootc/periodics/el96-prel@el96-src@upgrade-ostree2bootc-ok.sh
+++ test/scenarios-bootc/periodics/el96-prel@el96-src@upgrade-ostree2bootc-ok.sh
@@ -5,7 +5,7 @@
 scenario_create_vms() {
     # The y-1 ostree image will be fetched from the cache as it is not built
     # as part of the bootc image build procedure
-    prepare_kickstart host1 kickstart.ks.template "rhel-9.4-microshift-4.${PREVIOUS_MINOR_VERSION}"
+    prepare_kickstart host1 kickstart.ks.template "rhel-9.6-microshift-4.${PREVIOUS_MINOR_VERSION}"
     launch_vm 
 }
 
