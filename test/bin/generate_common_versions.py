b18456d1f86dddc6044c146bcc018e3ce66b9ec8
vanhalenar <thalenar@redhat.com>
2025-06-09 08:57:55 +0000

    minor fixes, comments

--- test/bin/generate_common_versions.py
+++ test/bin/generate_common_versions.py
@@ -4,18 +4,64 @@ import os
 
 ARCH = os.uname().machine
 
-MINOR_VERSION = 19
-PREVIOUS_MINOR_VERSION = MINOR_VERSION - 1
-YMINUS2_MINOR_VERSION = MINOR_VERSION - 2
-FAKE_NEXT_MINOR_VERSION = MINOR_VERSION + 1
+MINOR = 20
 
-def get_candidate_repo(dev_preview=False):
+
+def get_candidate_repo(minor, dev_preview=False):
+    """
+    :param minor: the minor version, e.g. 19 for 4.19
+    :param dev_preview: if True, returns the engineering candidate repo, otherwise returns the release candidate repo
+
+    ``get_candidate_repo`` returns the URL of the EC/RC repository for the specified minor version.
+    """
     if dev_preview:
-        return f"https://mirror.openshift.com/pub/openshift-v4/{ARCH}/microshift/ocp-dev-preview/latest-4.{MINOR_VERSION}/el9/os"
+        return f"https://mirror.openshift.com/pub/openshift-v4/{ARCH}/microshift/ocp-dev-preview/latest-4.{minor}/el9/os"
     else:
-        return f"https://mirror.openshift.com/pub/openshift-v4/{ARCH}/microshift/ocp/latest-4.{MINOR_VERSION}/el9/os"
+        return f"https://mirror.openshift.com/pub/openshift-v4/{ARCH}/microshift/ocp/latest-4.{minor}/el9/os"
+
+
+def get_beta_repo(minor):
+    """
+    :param minor: the minor version, e.g. 19. for 4.19
+
+    ``get_beta_repo`` returns the URL of the beta repository for the specified minor version.
+    """
+    for i in range(minor, minor-3, -1):
+        url = f"https://mirror.openshift.com/pub/openshift-v4/{ARCH}/dependencies/rpms/4.{i}-el9-beta"
+        if repo_exists(url) and provides_deps(url):
+            return f'"{url}"'
+
+    return None
+
+
+def provides_deps(repo_url):
+    """
+    :param repo_url: the URL of the repository to check
+
+    ``provides_deps`` checks if the repository provides the cri-o package.
+    """
+    try:
+        temp = f"this,{repo_url}"
+        _ = subprocess.run(
+                ['dnf', 'repoquery', 'cri-o', '--quiet',
+                 '--queryformat', '%{version}-%{release}', '--disablerepo', '\'*\'', '--repofrompath', temp],
+                capture_output=True,
+                text=True,
+                check=True
+            )
+
+        return True
+
+    except subprocess.CalledProcessError:
+        return False
+
 
 def dnf_repo_is_enabled(repo_name):
+    """
+    :param repo_name: the name of the repository to check
+
+    ``dnf_repo_is_enabled`` checks if the repository is enabled on this system.
+    """
     try:
         result = subprocess.run(
             ["dnf", "repolist"],
@@ -30,6 +76,11 @@ def dnf_repo_is_enabled(repo_name):
 
 
 def repo_exists(repo_url):
+    """
+    :param repo_url: the URL of a repository
+
+    ``repo_exists`` checks if a URL points to a valid repository.
+    """
     url = repo_url + "/repodata/repomd.xml"
     r = requests.get(url)
     if r.status_code == 404:
@@ -37,79 +88,131 @@ def repo_exists(repo_url):
     else:
         return True
 
-""" def get_current_release_from_sub_repos(minor=MINOR_VERSION):
-    try:
-        repo = f"rhocp-4.{minor}-for-rhel-9-{ARCH}-rpms"
-        result = subprocess.run(
-                ['sudo', 'dnf', 'repoquery', 'microshift', '--quiet',
-                '--queryformat', '%{version}-%{release}', '--repo', repo],
-                capture_output=True,
-                text=True,
-                check=True
-            )
 
-        versions = result.stdout.strip().splitlines()
-        if not versions:
-            return None
+def get_sub_repo(minor):
+    """
+    :param minor: the minor version, e.g. 19 for 4.19
 
-        sort_proc = subprocess.run(
-            ['sort', '--version-sort'],
-            input='\n'.join(versions),
-            capture_output=True,
-            text=True,
-            check=True
-        )
-
-        sorted_versions = sort_proc.stdout.strip().splitlines()
-        if sorted_versions:
-            return repo, sorted_versions[-1] 
-        else:
-            return None, None
-    except subprocess.CalledProcessError:
-        return None, None """
-
-def get_sub_repo(minor=MINOR_VERSION):
+    ``get_sub_repo`` returns the name of the subscription repository for the specified
+    minor version, if the repository provides the microshift package, otherwise returns None.
+    """
     try:
         repo = f"rhocp-4.{minor}-for-rhel-9-{ARCH}-rpms"
         _ = subprocess.run(
                 ['sudo', 'dnf', 'repoquery', 'microshift', '--quiet',
-                '--queryformat', '%{version}-%{release}', '--repo', repo],
+                 '--queryformat', '%{version}-%{release}', '--repo', repo],
                 capture_output=True,
                 text=True,
                 check=True
             )
 
         return repo
-        
+
     except subprocess.CalledProcessError:
         return None
 
 
+def get_release_repo(minor):
+    """
+    :param minor: the minor version, e.g. 19 for 4.19
 
-def get_current_release_repo():
-    repo = get_sub_repo()
+    ``get_release_repo`` gets the repository for the specified minor version. It first
+    tries the subscription repository, if it does not provide the microshift package,
+    goes on to try the candidate repositories (EC, then RC). returns the name of the
+    subscription repo, the candidate repo URL, or None.
+    """
+    repo = get_sub_repo(minor)
     if repo is not None:
-        return repo
-    rc = get_candidate_repo(dev_preview=False)
+        return f'"{repo}"'
+
+    rc = get_candidate_repo(minor, dev_preview=False)
     if repo_exists(rc):
-        return rc
-    
-    ec = get_candidate_repo(dev_preview=True)
+        return f'"{rc}"'
+
+    ec = get_candidate_repo(minor, dev_preview=True)
     if repo_exists(ec):
-        return ec
+        return f'"{ec}"'
 
-    return None
-    
-def get_current_release_version(repo):
-    if 
+    return '""'
 
 
+def get_release_version(repo):
+    """
+    :param repo: the name or the URL of the repo, enclosed in double quotes
+
+    ``get_release_version`` returns a string to be used as a bash variable,
+    with a call to the right function depending on whether the `repo` param
+    is a URL or not. if neither, returns empty double quotes.
+    """
+    if repo.startswith("\"rhocp"):
+        return f'"$(get_vrel_from_rhsm {repo})"'
+    elif repo.startswith("\"https"):
+        return f'"$(get_vrel_from_beta {repo})"'
+    else:
+        return '""'
 
 
-CURRENT_RELEASE_REPO = get_current_release_repo()
-CURRENT_RELEASE_VERSION = get_current_release_version(CURRENT_RELEASE_REPO)
+minor_version = MINOR
+previous_minor_version = minor_version - 1
+yminus2_minor_version = minor_version - 2
+fake_next_minor_version = minor_version + 1
 
-print(CURRENT_RELEASE_REPO)
+current_release_repo = get_release_repo(minor_version)
+current_release_version = get_release_version(current_release_repo)
 
+previous_release_repo = get_release_repo(previous_minor_version)
+previous_release_version = get_release_version(previous_release_repo)
 
+yminus2_release_repo = get_release_repo(yminus2_minor_version)
+yminus2_release_version = get_release_version(yminus2_release_repo)
 
+rhocp_minor_y = minor_version if get_sub_repo(minor_version) is not None else '""'
+rhocp_minor_y_beta = get_beta_repo(minor_version)
+
+rhocp_minor_y1 = previous_minor_version
+rhocp_minor_y1_beta = get_beta_repo(previous_minor_version)
+
+rhocp_minor_y2 = yminus2_minor_version
+
+cncf_sonobuoy_version = "v0.57.3"
+
+print(f"export MINOR_VERSION={minor_version}")
+print(f"export PREVIOUS_MINOR_VERSION={previous_minor_version}")
+print(f"export YMINUS2_MINOR_VERSION={yminus2_minor_version}")
+print(f"export FAKE_NEXT_MINOR_VERSION={fake_next_minor_version}")
+print("")
+
+print(f"CURRENT_RELEASE_REPO={current_release_repo}")
+print(f"CURRENT_RELEASE_VERSION={current_release_version}")
+print("export CURRENT_RELEASE_REPO")
+print("export CURRENT_RELEASE_VERSION")
+print("")
+
+print(f"PREVIOUS_RELEASE_REPO={previous_release_repo}")
+print(f"PREVIOUS_RELEASE_VERSION={previous_release_version}")
+print("export PREVIOUS_RELEASE_REPO")
+print("export PREVIOUS_RELEASE_VERSION")
+print("")
+
+print(f"YMINUS2_RELEASE_REPO={yminus2_release_repo}")
+print(f"YMINUS2_RELEASE_VERSION={yminus2_release_version}")
+print("export YMINUS2_RELEASE_REPO")
+print("export YMINUS2_RELEASE_VERSION")
+print("")
+
+print(f"RHOCP_MINOR_Y={rhocp_minor_y}")
+print(f"RHOCP_MINOR_Y_BETA={rhocp_minor_y_beta}")
+print("export RHOCP_MINOR_Y")
+print("export RHOCP_MINOR_Y_BETA")
+print("")
+
+print(f"RHOCP_MINOR_Y1={rhocp_minor_y1}")
+print(f"RHOCP_MINOR_Y1_BETA={rhocp_minor_y1_beta}")
+print("export RHOCP_MINOR_Y1")
+print("export RHOCP_MINOR_Y1_BETA")
+print("")
+
+print(f"export RHOCP_MINOR_Y2={rhocp_minor_y2}")
+print("")
+
+print(f"export CNCF_SONOBUOY_VERSION={cncf_sonobuoy_version}")
