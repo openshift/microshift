d00db073e95ec41d16ba446da00fb292f884bd95
ci-robot <ci-robot@openshift.io>
2025-06-19 04:11:42 +0000

    Update Go version

--- scripts/devenv-builder/configure-vm.sh
+++ scripts/devenv-builder/configure-vm.sh
@@ -178,7 +178,7 @@ if ${INSTALL_BUILD_DEPS} || ${BUILD_AND_RUN}; then
     [[ -d /run/systemd/system ]] &&  sudo systemctl enable --now cockpit.socket
 fi
 
-GO_VER=1.23.6
+GO_VER=1.24.3
 GO_ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
 GO_INSTALL_DIR="/usr/local/go${GO_VER}"
 if ${INSTALL_BUILD_DEPS} && [ ! -d "${GO_INSTALL_DIR}" ]; then
