6b59cb91b30ea0cdf10e3bcb24cad949626b638e
Patryk Matuszak <pmatusza@redhat.com>
2025-07-01 16:56:00 +0200

    Duplicate & tweak config for pretty print

--- pkg/config/gdp.go
+++ pkg/config/gdp.go
@@ -31,7 +31,16 @@ type GenericDevicePlugin struct {
 	Domain string `json:"domain"`
 
 	// Devices configuration
-	Devices []deviceplugin.DeviceSpec `json:"devices"`
+	Devices []DeviceSpec `json:"devices"`
+}
+
+// GetGDPDevices transforms the Devices to be of a type compatible with the Generic Device Plugin implementation.
+func (gdp *GenericDevicePlugin) GetGDPDevices() []deviceplugin.DeviceSpec {
+	deviceSpecs := make([]deviceplugin.DeviceSpec, 0, len(gdp.Devices))
+	for _, deviceSpec := range gdp.Devices {
+		deviceSpecs = append(deviceSpecs, deviceSpec.toGDP())
+	}
+	return deviceSpecs
 }
 
 func (gdp GenericDevicePlugin) incorporateUserSettings(c *Config) {
@@ -89,67 +98,67 @@ func genericDevicePluginDefaults() GenericDevicePlugin {
 	return GenericDevicePlugin{
 		Status: "Disabled",
 		Domain: "device.microshift.io",
-		Devices: []deviceplugin.DeviceSpec{
+		Devices: []DeviceSpec{
 			{
 				Name: "serial",
-				Groups: []*deviceplugin.Group{
-					{Paths: []*deviceplugin.Path{{Path: "/dev/ttyUSB*"}}},
-					{Paths: []*deviceplugin.Path{{Path: "/dev/ttyACM*"}}},
-					{Paths: []*deviceplugin.Path{{Path: "/dev/tty.usb*"}}},
-					{Paths: []*deviceplugin.Path{{Path: "/dev/cu.*"}}},
-					{Paths: []*deviceplugin.Path{{Path: "/dev/cuaU*"}}},
-					{Paths: []*deviceplugin.Path{{Path: "/dev/rfcomm*"}}},
+				Groups: []*Group{
+					{Paths: []*Path{{Path: "/dev/ttyUSB*"}}},
+					{Paths: []*Path{{Path: "/dev/ttyACM*"}}},
+					{Paths: []*Path{{Path: "/dev/tty.usb*"}}},
+					{Paths: []*Path{{Path: "/dev/cu.*"}}},
+					{Paths: []*Path{{Path: "/dev/cuaU*"}}},
+					{Paths: []*Path{{Path: "/dev/rfcomm*"}}},
 				},
 			},
 			{
 				Name: "video",
-				Groups: []*deviceplugin.Group{
+				Groups: []*Group{
 					{
-						Paths: []*deviceplugin.Path{{Path: "/dev/video0"}},
+						Paths: []*Path{{Path: "/dev/video0"}},
 					},
 				},
 			},
 			{
 				Name: "fuse",
-				Groups: []*deviceplugin.Group{
+				Groups: []*Group{
 					{
-						Paths: []*deviceplugin.Path{{Path: "/dev/fuse"}},
+						Paths: []*Path{{Path: "/dev/fuse"}},
 						Count: 10,
 					},
 				},
 			},
 			{
 				Name: "audio",
-				Groups: []*deviceplugin.Group{
+				Groups: []*Group{
 					{
-						Paths: []*deviceplugin.Path{{Path: "/dev/snd"}},
+						Paths: []*Path{{Path: "/dev/snd"}},
 						Count: 10,
 					},
 				},
 			},
 			{
 				Name: "capture",
-				Groups: []*deviceplugin.Group{
+				Groups: []*Group{
 					{
-						Paths: []*deviceplugin.Path{
+						Paths: []*Path{
 							{Path: "/dev/snd/controlC0"},
 							{Path: "/dev/snd/pcmC0D0c"},
 						},
 					},
 					{
-						Paths: []*deviceplugin.Path{
+						Paths: []*Path{
 							{Path: "/dev/snd/controlC1", MountPath: "/dev/snd/controlC0"},
 							{Path: "/dev/snd/pcmC1D0c", MountPath: "/dev/snd/pcmC0D0c"},
 						},
 					},
 					{
-						Paths: []*deviceplugin.Path{
+						Paths: []*Path{
 							{Path: "/dev/snd/controlC2", MountPath: "/dev/snd/controlC0"},
 							{Path: "/dev/snd/pcmC2D0c", MountPath: "/dev/snd/pcmC0D0c"},
 						},
 					},
 					{
-						Paths: []*deviceplugin.Path{
+						Paths: []*Path{
 							{Path: "/dev/snd/controlC3", MountPath: "/dev/snd/controlC0"},
 							{Path: "/dev/snd/pcmC3D0c", MountPath: "/dev/snd/pcmC0D0c"},
 						},
@@ -157,5 +166,99 @@ func genericDevicePluginDefaults() GenericDevicePlugin {
 				},
 			},
 		},
+	}
+}
+
+// Following DeviceSpec, Group, and Path are copied from Generic Device Plugin code and
+// changed to look prettier when priting a configuration using 'show-config' command.
+// Changes:
+// - 'omitempty' tag was added to Group's Paths and USBSpecs
+// - 'omitempty' tag was added to Path's Type
+
+// DeviceSpec defines a device that should be discovered and scheduled.
+// DeviceSpec allows multiple host devices to be selected and scheduled fungibly under the same name.
+// Furthermore, host devices can be composed into groups of device nodes that should be scheduled
+// as an atomic unit.
+type DeviceSpec struct {
+	// Name is a unique string representing the kind of device this specification describes.
+	Name string `json:"name"`
+	// Groups is a list of groups of devices that should be scheduled under the same name.
+	Groups []*Group `json:"groups"`
+}
+
+func (d DeviceSpec) toGDP() deviceplugin.DeviceSpec {
+	groups := make([]*deviceplugin.Group, len(d.Groups))
+	for i, group := range d.Groups {
+		groups[i] = group.toGDP()
+	}
+	return deviceplugin.DeviceSpec{
+		Name:   d.Name,
+		Groups: groups,
+	}
+}
+
+// Group represents a set of devices that should be grouped and mounted into a container together as one single meta-device.
+type Group struct {
+	// Paths is the list of devices of which the device group consists.
+	// Paths can be globs, in which case each device matched by the path will be schedulable `Count` times.
+	// When the paths have differing cardinalities, that is, the globs match different numbers of devices,
+	// the cardinality of each path is capped at the lowest cardinality.
+	Paths []*Path `json:"paths,omitempty"`
+	// USBSpecs is the list of USB specifications that this device group consists of.
+	USBSpecs []*deviceplugin.USBSpec `json:"usb,omitempty"`
+	// Count specifies how many times this group can be mounted concurrently.
+	// When unspecified, Count defaults to 1.
+	Count uint `json:"count,omitempty"`
+}
+
+func (g *Group) toGDP() *deviceplugin.Group {
+	paths := make([]*deviceplugin.Path, 0, len(g.Paths))
+	for _, path := range g.Paths {
+		paths = append(paths, path.toGDP())
+	}
+	return &deviceplugin.Group{
+		Paths:    paths,
+		USBSpecs: g.USBSpecs,
+		Count:    g.Count,
+	}
+}
+
+// Path represents a file path that should be discovered.
+type Path struct {
+	// Path is the file path of a device in the host.
+	Path string `json:"path"`
+	// MountPath is the file path at which the host device should be mounted within the container.
+	// When unspecified, MountPath defaults to the Path.
+	MountPath string `json:"mountPath,omitempty"`
+	// Permissions is the file-system permissions given to the mounted device.
+	// Permissions apply only to mounts of type `Device`.
+	// This can be one or more of:
+	// * r - allows the container to read from the specified device.
+	// * w - allows the container to write to the specified device.
+	// * m - allows the container to create device files that do not yet exist.
+	// When unspecified, Permissions defaults to mrw.
+	Permissions string `json:"permissions,omitempty"`
+	// ReadOnly specifies whether the path should be mounted read-only.
+	// ReadOnly applies only to mounts of type `Mount`.
+	ReadOnly bool `json:"readOnly,omitempty"`
+	// Type describes what type of file-system node this Path represents and thus how it should be mounted.
+	// When unspecified, Type defaults to Device.
+	Type deviceplugin.PathType `json:"type,omitempty"`
+	// Limit specifies up to how many times this device can be used in the group concurrently when other devices
+	// in the group yield more matches.
+	// For example, if one path in the group matches 5 devices and another matches 1 device but has a limit of 10,
+	// then the group will provide 5 pairs of devices.
+	// When unspecified, Limit defaults to 1.
+	Limit uint `json:"limit,omitempty"`
+}
+
+func (p *Path) toGDP() *deviceplugin.Path {
+	return &deviceplugin.Path{
+		Path:        p.Path,
+		MountPath:   p.MountPath,
+		Permissions: p.Permissions,
+		ReadOnly:    p.ReadOnly,
+		Type:        p.Type,
+		Limit:       p.Limit,
 	}
 }
