61c3733e0d1ed77192cebeae3705fea30c51429d
Patryk Matuszak <pmatusza@redhat.com>
2025-07-04 15:37:36 +0200

    Rename MicroShift 4.14 & 4.15 CRI-O dropins

--- scripts/ci-nvidia-device-plugin/setup/03-nvidia-container-toolkit.sh
+++ scripts/ci-nvidia-device-plugin/setup/03-nvidia-container-toolkit.sh
@@ -19,4 +19,13 @@ sudo nvidia-ctk runtime configure --runtime=crio --set-as-default --config=/etc/
 # Update runtimes from ["docker-runc", "runc", "crun"] to ["crun", "docker-runc", "runc"]
 sudo sed -i 's/^runtimes =.*$/runtimes = ["crun", "docker-runc", "runc"]/g' /etc/nvidia-container-runtime/config.toml
 
+# MicroShift 4.14 and 4.15 don't use numerical prefixes.
+# Rename the files, so the 99-nvidia.conf overwrites them.
+if [ -f /etc/crio/crio.conf.d/microshift.conf ]; then
+    sudo mv /etc/crio/crio.conf.d/microshift.conf /etc/crio/crio.conf.d/10-microshift.conf
+fi
+if [ -f /etc/crio/crio.conf.d/microshift-ovn.conf ]; then
+    sudo mv /etc/crio/crio.conf.d/microshift-ovn.conf /etc/crio/crio.conf.d/11-microshift-ovn.conf
+fi
+
 sudo systemctl restart crio
