31598b2e314f6e325fb4a00f04056530ef8ca9e0
Patryk Matuszak <pmatusza@redhat.com>
2025-07-22 12:02:21 +0200

    Enable UserNamespacesSupport feature gate

--- pkg/controllers/kube-apiserver.go
+++ pkg/controllers/kube-apiserver.go
@@ -217,6 +217,7 @@ func (s *KubeAPIServer) configure(cfg *config.Config) error {
 			"enable-admission-plugins":              {},
 			"send-retry-after-while-not-ready-once": {"true"},
 			"shutdown-delay-duration":               {"5s"},
+			"feature-gates":                         {"UserNamespacesSupport=true", "UserNamespacesPodSecurityStandards=true"},
 		},
 		GenericAPIServerConfig: configv1.GenericAPIServerConfig{
 			AdmissionConfig: configv1.AdmissionConfig{
