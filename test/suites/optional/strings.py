22dd231e400aa87c74cb13ddd1396e469880624b
Kasturi Narra <knarra@knarra-thinkpadx1carbon7th.remote.csb>
2025-08-04 17:45:27 +0530

    Adding new test case for robot framework

--- test/suites/optional/strings.py
+++ test/suites/optional/strings.py
@@ -8,6 +8,17 @@ genericDevicePlugin:
              - path: /dev/ttyPipeB0
 '''
 
+GDP_CONFIG_DROPIN_WITH_MOUNT = '''
+genericDevicePlugin:
+  status: Enabled
+  devices:
+     - name: fakeserial
+       groups:
+         - paths:
+             - path: /dev/ttyPipeB0
+               mountPath: /dev/myrenamedserial
+'''
+
 CONFIGMAP_PREAMBLE = '''
 apiVersion: v1
 kind: ConfigMap
