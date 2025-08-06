7c5b3fd461a0a3e1e6b6431be5ce3435c6c6cd24
Kasturi Narra <knarra@knarra-thinkpadx1carbon7th.remote.csb>
2025-08-06 19:11:09 +0530

    Add test for GDP device allocation

--- test/suites/optional/strings.py
+++ test/suites/optional/strings.py
@@ -8,6 +8,82 @@ genericDevicePlugin:
              - path: /dev/ttyPipeB0
 '''
 
+GDP_CONFIG_SERIAL_GLOB = '''
+genericDevicePlugin:
+  status: Enabled
+  devices:
+     - name: fakeserial
+       groups:
+         - paths:
+             - path: /dev/ttyUSB*
+'''
+
+POD_SERIAL_2_DEVICES = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  name: serial-test-pod
+spec:
+  containers:
+  - name: serialdevice-app-container
+    image: registry.access.redhat.com/ubi9/ubi:9.6
+    command: ["sleep", "infinity"]
+    resources:
+      limits:
+        device.microshift.io/fakeserial: "2"
+    securityContext:
+      allowPrivilegeEscalation: false
+      capabilities:
+        drop: ["ALL"]
+      runAsNonRoot: true
+      seccompProfile:
+        type: "RuntimeDefault"
+'''
+
+POD_SERIAL_1_DEVICE = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  name: serial-test-pod1
+spec:
+  containers:
+  - name: serialdevice-app-container
+    image: registry.access.redhat.com/ubi9/ubi:9.6
+    command: ["sleep", "infinity"]
+    resources:
+      limits:
+        device.microshift.io/fakeserial: "1"
+    securityContext:
+      allowPrivilegeEscalation: false
+      capabilities:
+        drop: ["ALL"]
+      runAsNonRoot: true
+      seccompProfile:
+        type: "RuntimeDefault"
+'''
+
+POD_SERIAL_2_DEVICES_SECOND = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  name: serial-test-pod2
+spec:
+  containers:
+  - name: serialdevice-app-container
+    image: registry.access.redhat.com/ubi9/ubi:9.6
+    command: ["sleep", "infinity"]
+    resources:
+      limits:
+        device.microshift.io/fakeserial: "2"
+    securityContext:
+      allowPrivilegeEscalation: false
+      capabilities:
+        drop: ["ALL"]
+      runAsNonRoot: true
+      seccompProfile:
+        type: "RuntimeDefault"
+'''
+
 CONFIGMAP_PREAMBLE = '''
 apiVersion: v1
 kind: ConfigMap
