a662e0b6bf173cd32a04f59c3505fae0c7f62052
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-09-15 00:40:52 +0200

    Update el94 tests to el96

--- test/scenarios/releases/el96-yminus2@el96-lrel@standard2.sh
+++ test/scenarios/releases/el96-yminus2@el96-lrel@standard2.sh
@@ -7,7 +7,7 @@
 export TEST_RANDOMIZATION=none
 
 scenario_create_vms() {
-    prepare_kickstart host1 kickstart.ks.template "rhel-9.4-microshift-brew-optionals-4.${YMINUS2_MINOR_VERSION}-zstream"
+    prepare_kickstart host1 kickstart.ks.template "rhel-9.6-microshift-brew-optionals-4.${YMINUS2_MINOR_VERSION}-zstream"
     launch_vm 
 }
 
@@ -16,9 +16,8 @@ scenario_remove_vms() {
 }
 
 scenario_run_tests() {
-    run_tests host1 \
+        run_tests host1 \
         --variable "TARGET_REF:rhel-9.6-microshift-brew-optionals-4.${MINOR_VERSION}-${LATEST_RELEASE_TYPE}" \
-        --variable "EXPECTED_OS_VERSION:9.6" \
         suites/upgrade/upgrade-successful.robot \
-        suites/standard1/
+        suites/standard2/
 }
