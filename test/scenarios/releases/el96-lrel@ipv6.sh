ae54ea6ac04f01546884d57a6e76ba3812c262a1
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-09-18 10:41:36 +0200

    NO-ISSUE: skip ostree releases tests if images are not available

--- test/scenarios/releases/el96-lrel@ipv6.sh
+++ test/scenarios/releases/el96-lrel@ipv6.sh
@@ -10,16 +10,33 @@ WEB_SERVER_URL="http://[${VM_BRIDGE_IP}]:${WEB_SERVER_PORT}"
 # shellcheck disable=SC2034  # used elsewhere
 MIRROR_REGISTRY_URL="${VM_BRIDGE_IP}:${MIRROR_REGISTRY_PORT}"
 
+start_image="rhel-9.6-microshift-brew-optionals-4.${MINOR_VERSION}-${LATEST_RELEASE_TYPE}"
+
 scenario_create_vms() {
     # Enable IPv6 single stack in kickstart
-    prepare_kickstart host1 kickstart.ks.template "rhel-9.6-microshift-brew-optionals-4.${MINOR_VERSION}-${LATEST_RELEASE_TYPE}" false true
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
+    prepare_kickstart host1 kickstart.ks.template "${start_image}" false true
     launch_vm  --network "${VM_IPV6_NETWORK}"
 }
 
 scenario_remove_vms() {
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
     remove_vm host1
 }
 
 scenario_run_tests() {
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
     run_tests host1 suites/ipv6/singlestack.robot
 }
