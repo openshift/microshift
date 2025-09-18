0df114b39e777594e740d1998811f27dc472e999
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-09-18 08:15:27 +0200

    NO-ISSUE: Skip bootc releases tests if images are not available

--- test/scenarios-bootc/releases/el96-lrel@ipv6.sh
+++ test/scenarios-bootc/releases/el96-lrel@ipv6.sh
@@ -14,16 +14,33 @@ WEB_SERVER_URL="http://[${VM_BRIDGE_IP}]:${WEB_SERVER_PORT}"
 # shellcheck disable=SC2034  # used elsewhere
 MIRROR_REGISTRY_URL="$(hostname):${MIRROR_REGISTRY_PORT}/microshift"
 
+start_image="rhel96-bootc-brew-${LATEST_RELEASE_TYPE}-with-optional"
+
 scenario_create_vms() {
     # Enable IPv6 single stack in kickstart
-    prepare_kickstart host1 kickstart-bootc.ks.template "rhel96-bootc-brew-${LATEST_RELEASE_TYPE}-with-optional" false true
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
+    prepare_kickstart host1 kickstart-bootc.ks.template "${start_image}" false true
     launch_vm --boot_blueprint rhel96-bootc --network "${VM_IPV6_NETWORK}"
 }
 
 scenario_remove_vms() {
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
     remove_vm host1
 }
 
 scenario_run_tests() {
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
     run_tests host1 suites/ipv6/singlestack.robot
 }
