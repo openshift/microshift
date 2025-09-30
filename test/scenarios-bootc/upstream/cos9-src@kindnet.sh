4e8d427c1e8ddd934d4fd8ee1ac3d870366c804c
Gregory Giguashvili <ggiguash@redhat.com>
2025-09-30 16:04:37 +0300

    Fix kindnet crio configuration for RHEL 9 plugins

--- test/scenarios-bootc/upstream/cos9-src@kindnet.sh
+++ test/scenarios-bootc/upstream/cos9-src@kindnet.sh
@@ -5,13 +5,6 @@
 scenario_create_vms() {
     prepare_kickstart host1 kickstart-bootc.ks.template cos9-bootc-source-kindnet
     launch_vm --boot_blueprint centos9-bootc
-
-    # Open the firewall ports. Other scenarios get this behavior by
-    # embedding settings in the blueprint, but there is no blueprint
-    # for this scenario. We need do this step before running the RF
-    # suite so that suite can assume it can reach all of the same
-    # ports as for any other test.
-    configure_vm_firewall host1
 }
 
 scenario_remove_vms() {
