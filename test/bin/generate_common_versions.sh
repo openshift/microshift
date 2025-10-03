ccf6c34760c071248a4011c2f78819cf958e7578
vanhalenar <thalenar@redhat.com>
2025-10-03 12:05:00 +0000

    Add usage function to generate_common_versions.sh

--- test/bin/generate_common_versions.sh
+++ test/bin/generate_common_versions.sh
@@ -5,4 +5,17 @@ set -euo pipefail
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SCRIPTDIR
 
+usage() {
+    cat - <<EOF
+generate_common_versions.sh minor
+
+    minor -- MicroShift minor version, e.g. "21"
+EOF
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 1
+fi
+
 python3 "${SCRIPTDIR}/pyutils/generate_common_versions.py" "$@" > "${SCRIPTDIR}/common_versions.sh"
