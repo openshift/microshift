706836765335c29bcfa19f2702ad3ccf5c0230a9
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-10-03 23:52:54 +0200

    USHIFT-6191: update ostree scenario

--- test/scenarios/periodics/el96-src@cncf-conformance.sh
+++ test/scenarios/periodics/el96-src@cncf-conformance.sh
@@ -7,6 +7,13 @@ KUBECONFIG="${SCENARIO_INFO_DIR}/${SCENARIO}/kubeconfig"
 TIMEOUT_TEST=7400
 TIMEOUT_RESULTS=600
 
+collect_sonobuoy_debug_info() {
+    ~/go/bin/sonobuoy logs > "${SCENARIO_INFO_DIR}/${SCENARIO}/sonobuoy-logs.txt" || true
+    oc get all -n sonobuoy -o wide > "${SCENARIO_INFO_DIR}/${SCENARIO}/sonobuoy-resources.txt" || true
+    oc describe all -n sonobuoy > "${SCENARIO_INFO_DIR}/${SCENARIO}/sonobuoy-resources-describe.txt" || true
+    oc get events -n sonobuoy --sort-by=.metadata.creationTimestamp > "${SCENARIO_INFO_DIR}/${SCENARIO}/sonobuoy-events.txt" || true
+}
+
 prepare_hosts() {
     local -r primary_host_ip=$(cat "${SCENARIO_INFO_DIR}/${SCENARIO}/vms/host1/ip")
     local -r secondary_host_ip=$(cat "${SCENARIO_INFO_DIR}/${SCENARIO}/vms/host2/ip")
@@ -69,7 +76,8 @@ run_sonobuoy() {
     # Initiate test startup
     go install "github.com/vmware-tanzu/sonobuoy@${CNCF_SONOBUOY_VERSION}"
     ~/go/bin/sonobuoy run \
-	--sonobuoy-image docker.io/sonobuoy/sonobuoy:${CNCF_SONOBUOY_VERSION} \
+	    --sonobuoy-image docker.io/sonobuoy/sonobuoy:${CNCF_SONOBUOY_VERSION} \
+        --systemd-logs-image docker.io/sonobuoy/systemd-logs:${CNCF_SYSTEMD_LOGS_VERSION} \
         --mode=certified-conformance \
         --plugin-env=e2e.E2E_SKIP=".*Services should be able to switch session affinity for NodePort service.*" \
         --dns-namespace=openshift-dns \
@@ -91,6 +99,8 @@ run_sonobuoy() {
     done
     if [ ${rc} -ne 0 ] ; then
         echo "Failed to start tests after 5m"
+        # Retrieve sonobuoy info in case the tests have not started yet. This covers scheduling/container creation issues.
+        collect_sonobuoy_debug_info
         ~/go/bin/sonobuoy status --json
         record_junit "run_sonobuoy" "wait_e2e_running" "FAILED"
         return ${rc}
@@ -111,6 +121,7 @@ run_sonobuoy() {
         if [ $(( now - start )) -ge ${TIMEOUT_TEST} ]; then
             rc=1
             echo "Tests running for ${TIMEOUT_TEST}s. Timing out"
+            collect_sonobuoy_debug_info
             record_junit "run_sonobuoy" "wait_e2e_finished" "FAILED"
             break
         fi
