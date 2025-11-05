3ba8310a0986b99937683643b06c286c2b55724c
Alejandro Gullon <agullon@redhat.com>
2025-11-05 10:50:10 +0100

    backport changes from 2 PRs: 5476 and 5679

--- test/scenarios-bootc/periodics/el96-crel@brew-standard1.sh
+++ test/scenarios-bootc/periodics/el96-crel@brew-standard1.sh
@@ -2,16 +2,30 @@
 
 # Sourced from scenario.sh and uses functions defined there.
 
+start_image=rhel96-bootc-brew
+
 scenario_create_vms() {
-    prepare_kickstart host1 kickstart-bootc.ks.template rhel96-bootc-brew
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+    prepare_kickstart host1 kickstart-bootc.ks.template "${start_image}"
     launch_vm --boot_blueprint rhel96-bootc
 }
 
 scenario_remove_vms() {
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
     remove_vm host1
 }
 
 scenario_run_tests() {
+    if ! does_image_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
     run_tests host1 \
         --variable "EXPECTED_OS_VERSION:9.6" \
         suites/standard1/ suites/selinux/validate-selinux-policy.robot
