784b6158313fe6e634c033d543a72d5d8805b4f7
copejon <jcope@redhat.com>
2025-11-10 15:14:10 -0600

    Add API Server Configuration Flow Documentation
    This commit introduces a comprehensive document detailing the data flow of the API server configuration, tracing the path from the `apiServer` field in the MicroShift configuration file to its ingestion by the kube-apiserver. It includes a UML sequence diagram illustrating the configuration steps, key data structures, and the transformation of configuration values into command-line flags. Additionally, it updates the configuration structure to support feature gates, enhancing the API server's customization capabilities.

--- pkg/config/feature_gates_test.go
+++ pkg/config/feature_gates_test.go
@@ -6,45 +6,61 @@ func Test_validateFeatureSets(t *testing.T) {
 	tests := []struct {
 		name string // description of this test case
 		// Named input parameters for target function.
-		cfg     *FeatureGates
+		c       *Config
 		wantErr bool
 	}{
 		{
 			name: "unrecognized featureSet should return an error",
-			cfg: &FeatureGates{
-				FeatureSet: "unrecognized",
+			c: &Config{
+				ApiServer: ApiServer{
+					FeatureGates: FeatureGates{
+						FeatureSet: "unrecognized",
+					},
+				},
 			},
 			wantErr: true,
 		},
 		{
 			name: "featureSet is CustomNoUpgrade and customNoUpgrade.[Enabled || Disabled] is empty should not return an error",
-			cfg: &FeatureGates{
-				FeatureSet: FeatureSetCustomNoUpgrade,
-				CustomNoUpgrade: CustomNoUpgrade{
-					Enabled:  []string{},
-					Disabled: []string{},
+			c: &Config{
+				ApiServer: ApiServer{
+					FeatureGates: FeatureGates{
+						FeatureSet: FeatureSetCustomNoUpgrade,
+						CustomNoUpgrade: CustomNoUpgrade{
+							Enabled:  []string{},
+							Disabled: []string{},
+						},
+					},
 				},
 			},
 			wantErr: false,
 		},
 		{
 			name: "customNoUpgrade.[Enabled || Disabled] can only be used when featureSet is CustomNoUpgrade",
-			cfg: &FeatureGates{
-				FeatureSet: "",
-				CustomNoUpgrade: CustomNoUpgrade{
-					Enabled:  []string{"feature1"},
-					Disabled: []string{"feature2"},
+			c: &Config{
+				ApiServer: ApiServer{
+					FeatureGates: FeatureGates{
+						FeatureSet: "",
+						CustomNoUpgrade: CustomNoUpgrade{
+							Enabled:  []string{"feature1"},
+							Disabled: []string{"feature2"},
+						},
+					},
 				},
 			},
 			wantErr: true,
 		},
 		{
 			name: "customNoUpgrade.[Enabled && Disabled] cannot have the same feature gate",
-			cfg: &FeatureGates{
-				FeatureSet: FeatureSetCustomNoUpgrade,
-				CustomNoUpgrade: CustomNoUpgrade{
-					Enabled:  []string{"feature1"},
-					Disabled: []string{"feature1"},
+			c: &Config{
+				ApiServer: ApiServer{
+					FeatureGates: FeatureGates{
+						FeatureSet: FeatureSetCustomNoUpgrade,
+						CustomNoUpgrade: CustomNoUpgrade{
+							Enabled:  []string{"feature1"},
+							Disabled: []string{"feature1"},
+						},
+					},
 				},
 			},
 			wantErr: true,
@@ -53,7 +69,7 @@ func Test_validateFeatureSets(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotErr := validateFeatureSets(tt.cfg)
+			gotErr := validateFeatureGates(tt.c)
 			if gotErr != nil {
 				if !tt.wantErr {
 					t.Errorf("validateFeatureSets() failed: %v", gotErr)
@@ -71,14 +87,14 @@ func Test_validateCustomNoUpgrade(t *testing.T) {
 	tests := []struct {
 		name string // description of this test case
 		// Named input parameters for target function.
-		cfg     *FeatureGates
+		c       *Config
 		wantErr bool
 	}{
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotErr := validateCustomNoUpgrade(tt.cfg)
+			gotErr := validateFeatureGates(tt.c)
 			if gotErr != nil {
 				if !tt.wantErr {
 					t.Errorf("validateCustomNoUpgrade() failed: %v", gotErr)
