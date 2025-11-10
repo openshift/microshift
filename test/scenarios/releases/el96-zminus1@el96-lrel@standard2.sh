558f325a3c2c4315db56d0ce3920e192d9e0c89c
Alejandro Gullon <agullon@redhat.com>
2025-11-10 10:48:12 +0100

    add new test scenarios for Z-1

---
+++ test/scenarios/releases/el96-zminus1@el96-lrel@standard2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Sourced from scenario.sh and uses functions defined there.
+
+# NOTE: Unlike most suites, these tests rely on being run IN ORDER to
+# ensure MicroShift is upgraded before running standard suite tests
+export TEST_RANDOMIZATION=none
+
+start_image="rhel-9.6-microshift-brew-optionals-4.${MINOR_VERSION}-z1"
+
+scenario_create_vms() {
+    if ! does_commit_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
+    prepare_kickstart host1 kickstart.ks.template "${start_image}"
+    launch_vm
+}
+
+scenario_remove_vms() {
+    if ! does_commit_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
+    remove_vm host1
+}
+
+scenario_run_tests() {
+    if ! does_commit_exist "${start_image}"; then
+        echo "Image '${start_image}' not found - skipping test"
+        return 0
+    fi
+
+    run_tests host1 \
+        --variable "TARGET_REF:rhel-9.6-microshift-brew-optionals-4.${MINOR_VERSION}-${LATEST_RELEASE_TYPE}" \
+        suites/upgrade/upgrade-successful.robot \
+        suites/standard2/
+}
