13e6e85f4711f85794d16a165ddfb43e570b6c37
vanhalenar <thalenar@redhat.com>
2025-11-24 12:07:36 +0000

    Add entry for healthcheck, fix RF test naming

--- test/bin/common.sh
+++ test/bin/common.sh
@@ -42,9 +42,6 @@ export VM_ISOLATED_NETWORK="isolated"
 # Libvirt network for Multus tests
 export VM_MULTUS_NETWORK="multus"
 
-# Libvirt network for SR-IOV tests
-export SRIOV_NETWORK="sriov"
-
 # Libvirt network for IPv6 tests
 export VM_IPV6_NETWORK="ipv6"
 
@@ -391,7 +388,6 @@ MICROSHIFT_Y1_OPTIONAL_RPMS_LIST=(
 MICROSHIFT_OPTIONAL_RPMS_LIST=(
     "${MICROSHIFT_Y1_OPTIONAL_RPMS_LIST[@]}"
     microshift-sriov
-    microshift-sriov-release-info
 )
 MICROSHIFT_Y2_X86_64_RPMS_LIST=(
     microshift-ai-model-serving
