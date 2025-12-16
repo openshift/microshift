e326c0d1e1a248e63d437f9d5093f4b7c5e95fc0
Alejandro Gullón <agullon@redhat.com>
2025-12-16 08:56:43 +0100

    update openshift-test commit repo

--- test/bin/scenario.sh
+++ test/bin/scenario.sh
@@ -1197,9 +1197,6 @@ run_ginkgo_tests() {
         echo "Running Ginkgo tests with MicroShift filter..."
     fi
 
-    echo "----------------Remove 62738 test case---------------"
-    sed -i "/62738/d" "${case_selected}"
-    echo "-----------------------------------------------------"
     echo "------------------Selected test cases------------------"
     cat "${case_selected}"
     echo "-----------------------------------------------------"
