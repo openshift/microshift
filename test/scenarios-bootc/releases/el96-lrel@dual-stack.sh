0546c7e5795f8d0a8ead2a3d493174ca2f70c940
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2025-12-19 10:41:18 +0100

    USHIFT-6379: Update scenarios

--- test/scenarios-bootc/releases/el96-lrel@dual-stack.sh
+++ test/scenarios-bootc/releases/el96-lrel@dual-stack.sh
@@ -19,14 +19,5 @@ scenario_remove_vms() {
 
 scenario_run_tests() {
     exit_if_image_not_found "${start_image}"
-
-    local -r vmname=$(full_vm_name host1)
-    # Valid IP addresses are the first two entries returned by manage-vm script.
-    local -r vm_ip1=$("${ROOTDIR}/scripts/devenv-builder/manage-vm.sh" ip -n "${vmname}" | head -1)
-    local -r vm_ip2=$("${ROOTDIR}/scripts/devenv-builder/manage-vm.sh" ip -n "${vmname}" | head -2 | tail -1)
-
-    run_tests host1 \
-        --variable "USHIFT_HOST_IP1:${vm_ip1}" \
-        --variable "USHIFT_HOST_IP2:${vm_ip2}" \
-        suites/ipv6/dualstack.robot
+    run_tests host1 suites/ipv6/dualstack.robot
 }
