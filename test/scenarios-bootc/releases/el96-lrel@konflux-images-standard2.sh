f6cd6ef832aceb287e4df84bc32ebf8554ef73ab
Alejandro Gullon <agullon@redhat.com>
2025-12-19 11:04:38 +0100

    fix check

--- test/scenarios-bootc/releases/el96-lrel@konflux-images-standard2.sh
+++ test/scenarios-bootc/releases/el96-lrel@konflux-images-standard2.sh
@@ -8,8 +8,9 @@
 IMAGE_SIGSTORE_ENABLED=true
 
 scenario_create_vms() {
-    if [ -z "${LATEST_RELEASE_IMAGE_URL}" ] || [[ "${LATEST_RELEASE_IMAGE_URL}" == "" ]] ; then
+    if [ -z "${LATEST_RELEASE_IMAGE_URL}" ]; then
         echo "ERROR: Scenario requires a valid LATEST_RELEASE_IMAGE_URL, but got '${LATEST_RELEASE_IMAGE_URL}'"
+        record_junit "scenario_create_vms" "build_vm_image_not_found" "FAILED"
         exit 1
     fi
     prepare_kickstart host1 kickstart-bootc.ks.template "${LATEST_RELEASE_IMAGE_URL}"
