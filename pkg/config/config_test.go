f89c78357a99b453c5f6997a41824779e4b54cf3
Jon Cope <jcope@redhat.com>
2026-01-23 10:58:08 -0600

    Apply suggestion from @agullon
    Co-authored-by: Alejandro Gullón <agullon@redhat.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -874,6 +874,17 @@ func TestValidate(t *testing.T) {
 			expectErr: true,
 		},
 		{
+			name: "feature-gates-custom-no-upgrade-with-empty-enabled-and-disabled-lists",
+			config: func() *Config {
+				c := mkDefaultConfig()
+				c.ApiServer.FeatureGates.FeatureSet = "CustomNoUpgrade"
+				c.ApiServer.FeatureGates.CustomNoUpgrade.Enabled = []string{}
+				c.ApiServer.FeatureGates.CustomNoUpgrade.Disabled = []string{}
+				return c
+			}(),
+			expectErr: false,
+		},
+		{
 			name: "feature-gates-preview-feature-sets-not-supported",
 			config: func() *Config {
 				c := mkDefaultConfig()
