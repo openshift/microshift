e233a8b4997a14ec912a70a640fd54a1eeb3a161
vanhalenar <thalenar@redhat.com>
2026-01-26 09:33:56 +0000

    Bump RHEL version in more files

--- test/scenarios-bootc/releases/el102-lrel@standard2.sh
+++ test/scenarios-bootc/releases/el102-lrel@standard2.sh
@@ -2,7 +2,7 @@
 
 # Sourced from scenario.sh and uses functions defined there.
 
-start_image="rhel100-bootc-brew-${LATEST_RELEASE_TYPE}-with-optional"
+start_image="rhel102-bootc-brew-${LATEST_RELEASE_TYPE}-with-optional"
 
 scenario_create_vms() {
     if ! does_image_exist "${start_image}"; then
@@ -11,7 +11,7 @@ scenario_create_vms() {
     fi
 
     prepare_kickstart host1 kickstart-bootc.ks.template "${start_image}"
-    launch_vm --boot_blueprint rhel100-bootc --vm_disksize 30 --vm_vcpus 4
+    launch_vm --boot_blueprint rhel102-bootc --vm_vcpus 4
 }
 
 scenario_remove_vms() {
@@ -29,8 +29,5 @@ scenario_run_tests() {
         return 0
     fi
 
-    # Wait for MicroShift to be ready
-    wait_for_microshift_to_be_ready host1
-
-    run_ginkgo_tests host1 "~Disruptive"
+    run_tests host1 suites/standard2
 }
