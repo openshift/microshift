029fe0f639b833d6a690e65635ce9dafe435cb6d
Gregory Giguashvili <ggiguash@redhat.com>
2026-03-08 13:07:14 +0200

    Temporarily disable gitops tests

--- test/scenarios-bootc/periodics/el98-src@gitops-telemetry-clusterid-systemd.sh
+++ test/scenarios-bootc/periodics/el98-src@gitops-telemetry-clusterid-systemd.sh
@@ -3,7 +3,9 @@
 # Sourced from scenario.sh and uses functions defined there.
 
 scenario_create_vms() {
-    prepare_kickstart host1 kickstart-bootc.ks.template rhel98-bootc-source-gitops
+    # TODO: Add rhel98-bootc-source-gitops when the RPM package is updated not to use
+    # non-existent calls from the greenboot functions include file
+    prepare_kickstart host1 kickstart-bootc.ks.template rhel98-bootc-source
     launch_vm --boot_blueprint rhel98-bootc
 }
 
@@ -12,11 +14,12 @@ scenario_remove_vms() {
 }
 
 scenario_run_tests() {
+    # TODO: Add suites/gitops/ when the RPM package is updated not to use
+    # non-existent calls from the greenboot functions include file
     run_tests host1 \
         --variable "PROXY_HOST:${VM_BRIDGE_IP}" \
         --variable "PROXY_PORT:9001" \
         --variable "PROMETHEUS_HOST:$(hostname)" \
-        suites/gitops/ \
         suites/telemetry/telemetry.robot \
         suites/osconfig/clusterid.robot \
         suites/osconfig/systemd-resolved.robot
