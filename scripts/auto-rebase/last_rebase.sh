a46cff663589abb808c32c3bf210294a291540ba
ci-robot <ci-robot@openshift.io>
2026-03-09 04:11:08 +0000

    update last_rebase.sh

--- scripts/auto-rebase/last_rebase.sh
+++ scripts/auto-rebase/last_rebase.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-./scripts/auto-rebase/rebase.sh to "registry.ci.openshift.org/ocp/release:4.20.0-0.nightly-2026-03-05-003951" "registry.ci.openshift.org/ocp-arm64/release-arm64:4.20.0-0.nightly-arm64-2026-03-05-022258"
+./scripts/auto-rebase/rebase.sh to "registry.ci.openshift.org/ocp/release:4.20.0-0.nightly-2026-03-05-003951" "registry.ci.openshift.org/ocp-arm64/release-arm64:4.20.0-0.nightly-arm64-2026-03-07-161702"
