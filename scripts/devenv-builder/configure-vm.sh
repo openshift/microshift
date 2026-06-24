bb07ec3d090c22cd456915570a3235cee3d14c6c
Patryk Matuszak <pmatusza@redhat.com>
2026-06-24 16:11:19 +0200

    Install gettext - build dep

--- scripts/devenv-builder/configure-vm.sh
+++ scripts/devenv-builder/configure-vm.sh
@@ -271,7 +271,7 @@ function install_build_deps() {
     if ${DNF_UPDATE}; then
         "${DNF_RETRY}" "update" "--nobest"
     fi
-    "${DNF_RETRY}" "install" "gcc git golang cockpit make jq selinux-policy-devel rpm-build jq bash-completion avahi-tools createrepo"
+    "${DNF_RETRY}" "install" "gcc git golang cockpit make jq selinux-policy-devel rpm-build jq bash-completion avahi-tools createrepo gettext"
 
     # run only if booted with systemd
     [[ -d /run/systemd/system ]] &&  sudo systemctl enable --now cockpit.socket
