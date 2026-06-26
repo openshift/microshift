e8b4f1ebbced68d1397a053e286a52c7c230ddff
Gregory Giguashvili <ggiguash@redhat.com>
2026-06-26 09:04:32 +0300

    update last_rebase_lvms.sh

--- scripts/auto-rebase/last_rebase_lvms.sh
+++ scripts/auto-rebase/last_rebase_lvms.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-./scripts/auto-rebase/rebase_lvms.sh to "registry.redhat.io/lvms4/lvms-operator-bundle:v4.21.0"
+./scripts/auto-rebase/rebase_lvms.sh to "registry.redhat.io/lvms4/lvms-operator-bundle:v4.19.2"
