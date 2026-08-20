92ef81a65f9811b648f178dbdafba65d8371e127
ci-robot <ci-robot@openshift.io>
2026-08-20 04:23:22 +0000

    update etcd/vendor

--- etcd/vendor/go.etcd.io/etcd/server/v3/storage/util.go
+++ etcd/vendor/go.etcd.io/etcd/server/v3/storage/util.go
@@ -39,6 +39,10 @@ func AssertNoV2StoreContent(lg *zap.Logger, st v2store.Store, deprecationStage c
 	if metaOnly {
 		return nil
 	}
+	if deprecationStage == config.V2Depr1WriteOnlySkipCheck {
+		lg.Warn("WARNING: --v2-deprecation=write-only-skip-check is enabled; v2 content detected, but validation check is being bypassed")
+		return nil
+	}
 	if deprecationStage.IsAtLeast(config.V2Depr1WriteOnly) {
 		return fmt.Errorf("detected disallowed custom content in v2store for stage --v2-deprecation=%s", deprecationStage)
 	}
@@ -129,6 +133,9 @@ func GetEffectiveNodeIDsFromWALEntries(lg *zap.Logger, snap *raftpb.Snapshot, en
 	ids := make(map[uint64]bool)
 	if snap != nil {
 		for _, id := range snap.Metadata.ConfState.Voters {
+			ids[id] = true
+		}
+		for _, id := range snap.Metadata.ConfState.Learners {
 			ids[id] = true
 		}
 	}
