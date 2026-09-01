ec1a64eef720822b3409c10462b5a3083c39663a
Evgeny Slutsky <eslutsky@redhat.com>
2026-09-01 08:37:40 +0200

    microshift 5.0 promoted to rc in common_versions
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- test/bin/common_versions.sh
+++ test/bin/common_versions.sh
@@ -109,7 +109,7 @@ export CURRENT_RELEASE_VERSION
 # For a release branch, the previous release repository should come from the
 # official 'rhocp' stream.# The previous release repository value should either
 # point to the OpenShift mirror URL or the 'rhocp' repository name.
-PREVIOUS_RELEASE_REPO="https://mirror.openshift.com/pub/openshift-v5/${UNAME_M}/microshift/ocp-dev-preview/latest-5.0/el9/os"
+PREVIOUS_RELEASE_REPO="https://mirror.openshift.com/pub/openshift-v5/${UNAME_M}/microshift/ocp/latest-5.0/el9/os"
 PREVIOUS_RELEASE_VERSION="$(get_vrel_from_beta "${PREVIOUS_RELEASE_REPO}")"
 export PREVIOUS_RELEASE_REPO
 export PREVIOUS_RELEASE_VERSION
