b481b08d5f6f6cc1f4ff08b17b2767c8592f4731
ehila <ehila@redhat.com>
2026-09-10 14:40:57 -0400

    feat: remove WalkChains function
    We no longer are using this function as our main collection of certs is moved over to the Inventory object.
    
    Signed-off-by: ehila <ehila@redhat.com>

--- pkg/util/cryptomaterial/certchains/certchains.go
+++ pkg/util/cryptomaterial/certchains/certchains.go
@@ -1,7 +1,6 @@
 package certchains
 
 import (
-	"crypto/x509"
 	"fmt"
 	"time"
 
@@ -58,50 +57,6 @@ func (cs *CertificateChains) Regenerate(certPath ...string) error {
 	}
 
 	return fmt.Errorf("no such signer: %s", certPath[0])
-}
-
-type CertWalkFunc func(certPath []string, c x509.Certificate) error
-
-// WalkChains traverses through the trust chain starting at `rootPath` and applies
-// `fn` on all the certificates in the chain tree
-func (cs *CertificateChains) WalkChains(rootPath []string, fn CertWalkFunc) error {
-	if len(rootPath) == 0 {
-		for _, signerName := range cs.GetSignerNames() {
-			if err := cs.WalkChains([]string{signerName}, fn); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
-	if signer := cs.GetSigner(rootPath...); signer != nil {
-		// the path points to a signer
-		if err := fn(rootPath, *signer.signerConfig.Config.Certs[0]); err != nil {
-			return fmt.Errorf("failed to execute walk function on %v: %v", rootPath, err)
-		}
-
-		nextNames := append(signer.GetSubCANames(), signer.GetCertNames()...)
-		for _, name := range nextNames {
-			if err := cs.WalkChains(append(rootPath, name), fn); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-	if len(rootPath) == 1 {
-		// the path is a single element but no such signer exists
-		return fmt.Errorf("%v is not a path to a signer", rootPath)
-	}
-	// the path points to a leaf certificate
-	signerPath := rootPath[:len(rootPath)-1]
-	if signer := cs.GetSigner(signerPath...); signer != nil {
-		cert := signer.signedCertificates[rootPath[len(rootPath)-1]]
-		if cert == nil {
-			return fmt.Errorf("the requested element does not exist")
-		}
-		return fn(rootPath, *cert.tlsConfig.Certs[0])
-	}
-	return fmt.Errorf("a non-leaf fragment of the path '%v' either is not a signer or it doesn't exist", rootPath)
 }
 
 func WhenToRotateAtEarliest(cs *CertificateChains) ([]string, time.Time, error) {
